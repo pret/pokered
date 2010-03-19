dd95618085e9979aded492c65d40050450d56f8e
IIMarckus <mail@iimarck.us>
2010-03-19 00:06:01 -0600

    add wild pokemon data, with a little code that parses it
    hg-commit-id: 725848b96314

--- pokered.asm
+++ pokered.asm
@@ -169,7 +169,1170 @@ SECTION "bank2",DATA,BANK[$2]
 INCBIN "baserom.gbc",$8000,$4000
 
 SECTION "bank3",DATA,BANK[$3]
-INCBIN "baserom.gbc",$C000,$4000
+INCBIN "baserom.gbc",$C000,$EB8
+
+; wild pokemon data: from 4EB8 to 55C7
+
+LoadWildData: ; 4EB8
+        ld hl,WildDataPointers
+        ld a,[$D35E] ; current map number
+
+	; get wild data for current map
+        ld c,a
+        ld b,0
+        add hl,bc
+        add hl,bc
+        ld a,[hli]
+        ld h,[hl]
+        ld l,a       ; hl now points to wild data for current map
+        ld a,[hli]
+        ld [$D887],a ; grass encounter rate
+        and a
+        jr z,.NoGrassData\@ ; if no grass data, skip to surfing data
+        push hl
+        ld de,$D888 ; otherwise, load grass data
+        ld bc,$0014
+        call $B5 ; copy data
+        pop hl
+        ld bc,$0014
+        add hl,bc
+.NoGrassData\@
+        ld a,[hli]
+        ld [$D8A4],a ; surfing encounter rate
+        and a
+        ret z        ; if no water data, we're done
+        ld de,$D8A5  ; otherwise, load surfing data
+        ld bc,$0014
+        jp $B5 ; copy data
+
+; XXX replace "WildMonster__" with real location names
+WildDataPointers: ; 4EEB
+        dw NoMons ; PalletTown
+        dw NoMons ; ViridianCity
+        dw NoMons ; PewterCity
+        dw NoMons ; CeruleanCity
+        dw NoMons ; LavenderTown
+        dw NoMons ; VermilionCity
+        dw NoMons ; CeladonCity
+        dw NoMons ; FuchsiaCity
+        dw NoMons ; CinnibarIsland
+        dw NoMons ; IndigoPlateau
+        dw NoMons ; SaffronCity
+        dw NoMons ; not a real map
+        dw Route1Mons
+        dw Route2Mons
+        dw Route3Mons
+        dw Route4Mons
+        dw Route5Mons
+        dw Route6Mons
+        dw Route7Mons
+        dw Route8Mons
+        dw Route9Mons
+        dw Route10Mons
+        dw Route11Mons
+        dw Route12Mons
+        dw Route13Mons
+        dw Route14Mons
+        dw Route15Mons
+        dw Route16Mons
+        dw Route17Mons
+        dw Route18Mons
+        dw WaterMons ; Route19
+        dw WaterMons ; Route20
+        dw Route21Mons
+        dw Route22Mons
+        dw Route23Mons
+        dw Route24Mons
+        dw Route25Mons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw ForestMons ; ViridianForest
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters06
+        dw WildMonsters07
+        dw WildMonsters08
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters16
+        dw PowerPlantMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters57
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters22
+        dw WildMonsters23
+        dw WildMonsters24
+        dw WildMonsters25
+        dw WildMonsters26
+        dw WildMonsters27
+        dw WildMonsters28
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters41
+        dw WildMonsters42
+        dw WildMonsters43
+        dw WildMonsters44
+        dw NoMons
+        dw NoMons
+        dw WildMonsters45
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters40
+        dw NoMons
+        dw WildMonsters55
+        dw NoMons
+        dw NoMons
+        dw WildMonsters58
+        dw WildMonsters56
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters46
+        dw WildMonsters47
+        dw WildMonsters48
+        dw WildMonsters36
+        dw WildMonsters37
+        dw WildMonsters38
+        dw WildMonsters35
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters51
+        dw WildMonsters52
+        dw WildMonsters50
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw WildMonsters17
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw NoMons
+        dw $FFFF
+
+; wild pokemon data is divided into two parts.
+; first part:  pokemon found in grass
+; second part: pokemon found while surfing
+; each part goes as follows:
+	; if first byte == 00, then
+		; no wild pokemon on this map
+	; if first byte != 00, then
+		; first byte is encounter rate
+		; followed by 20 bytes:
+		; level, species (ten times)
+
+NoMons: ; no wild pokemon
+	db 0
+	db 0
+
+Route1Mons:
+	db $19
+	db 3,PIDGEY
+	db 3,RATTATA
+	db 3,RATTATA
+	db 2,RATTATA
+	db 2,PIDGEY
+	db 3,PIDGEY
+	db 3,PIDGEY
+	db 4,RATTATA
+	db 4,PIDGEY
+	db 5,PIDGEY
+
+	db 0
+
+Route2Mons:
+	db $19
+	db 3,RATTATA
+	db 3,PIDGEY
+	db 4,PIDGEY
+	db 4,RATTATA
+	db 5,PIDGEY
+	db 3,WEEDLE
+	db 2,RATTATA
+	db 5,RATTATA
+	db 4,WEEDLE
+	db 5,WEEDLE
+
+	db 0
+
+Route22Mons:
+	db $19
+	db 3,RATTATA
+	db 3,NIDORAN_M
+	db 4,RATTATA
+	db 4,NIDORAN_M
+	db 2,RATTATA
+	db 2,NIDORAN_M
+	db 3,SPEAROW
+	db 5,SPEAROW
+	db 3,NIDORAN_F
+	db 4,NIDORAN_F
+
+	db 0
+
+ForestMons:
+	db $08
+	db 4,WEEDLE
+	db 5,KAKUNA
+	db 3,WEEDLE
+	db 5,WEEDLE
+	db 4,KAKUNA
+	db 6,KAKUNA
+	db 4,METAPOD
+	db 3,CATERPIE
+	db 3,PIKACHU
+	db 5,PIKACHU
+
+	db 0
+
+Route3Mons:
+	db $14
+	db 6,PIDGEY
+	db 5,SPEAROW
+	db 7,PIDGEY
+	db 6,SPEAROW
+	db 7,SPEAROW
+	db 8,PIDGEY
+	db 8,SPEAROW
+	db 3,JIGGLYPUFF
+	db 5,JIGGLYPUFF
+	db 7,JIGGLYPUFF
+
+	db 0
+
+WildMonsters06:
+	db $0A
+	db 8,ZUBAT
+	db 7,ZUBAT
+	db 9,ZUBAT
+	db 8,GEODUDE
+	db 6,ZUBAT
+	db 10,ZUBAT
+	db 10,GEODUDE
+	db 8,PARAS
+	db 11,ZUBAT
+	db 8,CLEFAIRY
+
+	db 0
+
+WildMonsters07:
+	db $0A
+	db 8,ZUBAT
+	db 7,ZUBAT
+	db 7,GEODUDE
+	db 8,GEODUDE
+	db 9,ZUBAT
+	db 10,PARAS
+	db 10,ZUBAT
+	db 11,ZUBAT
+	db 9,CLEFAIRY
+	db 9,GEODUDE
+
+	db 0
+
+WildMonsters08:
+	db $0A
+	db 9,ZUBAT
+	db 9,GEODUDE
+	db 10,ZUBAT
+	db 10,GEODUDE
+	db 11,ZUBAT
+	db 10,PARAS
+	db 12,PARAS
+	db 10,CLEFAIRY
+	db 12,ZUBAT
+	db 12,CLEFAIRY
+
+	db 0
+
+Route4Mons:
+	db $14
+	db 10,RATTATA
+	db 10,SPEAROW
+	db 8,RATTATA
+	db 6,EKANS
+	db 8,SPEAROW
+	db 10,EKANS
+	db 12,RATTATA
+	db 12,SPEAROW
+	db 8,EKANS
+	db 12,EKANS
+
+	db 0
+
+Route24Mons:
+	db $19
+	db 7,WEEDLE
+	db 8,KAKUNA
+	db 12,PIDGEY
+	db 12,ODDISH
+	db 13,ODDISH
+	db 10,ABRA
+	db 14,ODDISH
+	db 13,PIDGEY
+	db 8,ABRA
+	db 12,ABRA
+
+	db 0
+
+Route25Mons:
+	db $0F
+	db 8,WEEDLE
+	db 9,KAKUNA
+	db 13,PIDGEY
+	db 12,ODDISH
+	db 13,ODDISH
+	db 12,ABRA
+	db 14,ODDISH
+	db 10,ABRA
+	db 7,METAPOD
+	db 8,CATERPIE
+
+	db 0
+
+Route9Mons:
+	db $0F
+	db 16,RATTATA
+	db 16,SPEAROW
+	db 14,RATTATA
+	db 11,EKANS
+	db 13,SPEAROW
+	db 15,EKANS
+	db 17,RATTATA
+	db 17,SPEAROW
+	db 13,EKANS
+	db 17,EKANS
+
+	db 0
+
+Route5Mons:
+	db $0F
+	db 13,ODDISH
+	db 13,PIDGEY
+	db 15,PIDGEY
+	db 10,MANKEY
+	db 12,MANKEY
+	db 15,ODDISH
+	db 16,ODDISH
+	db 16,PIDGEY
+	db 14,MANKEY
+	db 16,MANKEY
+
+	db 0
+
+Route6Mons:
+	db $0F
+	db 13,ODDISH
+	db 13,PIDGEY
+	db 15,PIDGEY
+	db 10,MANKEY
+	db 12,MANKEY
+	db 15,ODDISH
+	db 16,ODDISH
+	db 16,PIDGEY
+	db 14,MANKEY
+	db 16,MANKEY
+
+	db 0
+
+Route11Mons:
+	db $0F
+	db 14,EKANS
+	db 15,SPEAROW
+	db 12,EKANS
+	db 9,DROWZEE
+	db 13,SPEAROW
+	db 13,DROWZEE
+	db 15,EKANS
+	db 17,SPEAROW
+	db 11,DROWZEE
+	db 15,DROWZEE
+
+	db 0
+
+WildMonsters16:
+	db $0F
+	db 16,ZUBAT
+	db 17,ZUBAT
+	db 17,GEODUDE
+	db 15,MACHOP
+	db 16,GEODUDE
+	db 18,ZUBAT
+	db 15,ZUBAT
+	db 17,MACHOP
+	db 13,ONIX
+	db 15,ONIX
+
+	db 0
+
+WildMonsters17:
+	db $0F
+	db 16,ZUBAT
+	db 17,ZUBAT
+	db 17,GEODUDE
+	db 15,MACHOP
+	db 16,GEODUDE
+	db 18,ZUBAT
+	db 17,MACHOP
+	db 17,ONIX
+	db 13,ONIX
+	db 18,GEODUDE
+
+	db 0
+
+Route10Mons:
+	db $0F
+	db 16,VOLTORB
+	db 16,SPEAROW
+	db 14,VOLTORB
+	db 11,EKANS
+	db 13,SPEAROW
+	db 15,EKANS
+	db 17,VOLTORB
+	db 17,SPEAROW
+	db 13,EKANS
+	db 17,EKANS
+
+	db 0
+
+Route12Mons:
+	db $0F
+	db 24,ODDISH
+	db 25,PIDGEY
+	db 23,PIDGEY
+	db 24,VENONAT
+	db 22,ODDISH
+	db 26,VENONAT
+	db 26,ODDISH
+	db 27,PIDGEY
+	db 28,GLOOM
+	db 30,GLOOM
+
+	db 0
+
+Route8Mons:
+	db $0F
+	db 18,PIDGEY
+	db 18,MANKEY
+	db 17,EKANS
+	db 16,GROWLITHE
+	db 20,PIDGEY
+	db 20,MANKEY
+	db 19,EKANS
+	db 17,GROWLITHE
+	db 15,GROWLITHE
+	db 18,GROWLITHE
+
+	db 0
+
+Route7Mons:
+	db $0F
+	db 19,PIDGEY
+	db 19,ODDISH
+	db 17,MANKEY
+	db 22,ODDISH
+	db 22,PIDGEY
+	db 18,MANKEY
+	db 18,GROWLITHE
+	db 20,GROWLITHE
+	db 19,MANKEY
+	db 20,MANKEY
+
+	db 0
+
+WildMonsters22:
+	db 0
+	db 0
+
+WildMonsters23:
+	db 0
+	db 0
+
+WildMonsters24:
+	db $0A
+	db 20,GASTLY
+	db 21,GASTLY
+	db 22,GASTLY
+	db 23,GASTLY
+	db 19,GASTLY
+	db 18,GASTLY
+	db 24,GASTLY
+	db 20,CUBONE
+	db 22,CUBONE
+	db 25,HAUNTER
+
+	db 0
+
+WildMonsters25:
+	db $0A
+	db 20,GASTLY
+	db 21,GASTLY
+	db 22,GASTLY
+	db 23,GASTLY
+	db 19,GASTLY
+	db 18,GASTLY
+	db 25,HAUNTER
+	db 20,CUBONE
+	db 22,CUBONE
+	db 24,GASTLY
+
+	db 0
+
+WildMonsters26:
+	db $0A
+	db 20,GASTLY
+	db 21,GASTLY
+	db 22,GASTLY
+	db 23,GASTLY
+	db 19,GASTLY
+	db 18,GASTLY
+	db 25,HAUNTER
+	db 20,CUBONE
+	db 22,CUBONE
+	db 24,GASTLY
+
+	db 0
+
+WildMonsters27:
+	db $0F
+	db 21,GASTLY
+	db 22,GASTLY
+	db 23,GASTLY
+	db 24,GASTLY
+	db 20,GASTLY
+	db 19,GASTLY
+	db 26,HAUNTER
+	db 22,CUBONE
+	db 24,CUBONE
+	db 28,HAUNTER
+
+	db 0
+
+WildMonsters28:
+	db $0F
+	db 21,GASTLY
+	db 22,GASTLY
+	db 23,GASTLY
+	db 24,GASTLY
+	db 20,GASTLY
+	db 28,HAUNTER
+	db 22,CUBONE
+	db 24,CUBONE
+	db 28,HAUNTER
+	db 30,HAUNTER
+
+	db 0
+
+Route13Mons:
+	db $14
+	db 24,ODDISH
+	db 25,PIDGEY
+	db 27,PIDGEY
+	db 24,VENONAT
+	db 22,ODDISH
+	db 26,VENONAT
+	db 26,ODDISH
+	db 25,DITTO
+	db 28,GLOOM
+	db 30,GLOOM
+
+	db 0
+
+Route14Mons:
+	db $0F
+	db 24,ODDISH
+	db 26,PIDGEY
+	db 23,DITTO
+	db 24,VENONAT
+	db 22,ODDISH
+	db 26,VENONAT
+	db 26,ODDISH
+	db 30,GLOOM
+	db 28,PIDGEOTTO
+	db 30,PIDGEOTTO
+
+	db 0
+
+Route15Mons:
+	db $0F
+	db 24,ODDISH
+	db 26,DITTO
+	db 23,PIDGEY
+	db 26,VENONAT
+	db 22,ODDISH
+	db 28,VENONAT
+	db 26,ODDISH
+	db 30,GLOOM
+	db 28,PIDGEOTTO
+	db 30,PIDGEOTTO
+
+	db 0
+
+Route16Mons:
+	db $19
+	db 20,SPEAROW
+	db 22,SPEAROW
+	db 18,RATTATA
+	db 20,DODUO
+	db 20,RATTATA
+	db 18,DODUO
+	db 22,DODUO
+	db 22,RATTATA
+	db 23,RATICATE
+	db 25,RATICATE
+
+	db 0
+
+Route17Mons:
+	db $19
+	db 20,SPEAROW
+	db 22,SPEAROW
+	db 25,RATICATE
+	db 24,DODUO
+	db 27,RATICATE
+	db 26,DODUO
+	db 28,DODUO
+	db 29,RATICATE
+	db 25,FEAROW
+	db 27,FEAROW
+
+	db 0
+
+Route18Mons:
+	db $19
+	db 20,SPEAROW
+	db 22,SPEAROW
+	db 25,RATICATE
+	db 24,DODUO
+	db 25,FEAROW
+	db 26,DODUO
+	db 28,DODUO
+	db 29,RATICATE
+	db 27,FEAROW
+	db 29,FEAROW
+
+	db 0
+
+WildMonsters35:
+	db $1E
+	db 22,NIDORAN_M
+	db 25,RHYHORN
+	db 22,VENONAT
+	db 24,EXEGGCUTE
+	db 31,NIDORINO
+	db 25,EXEGGCUTE
+	db 31,NIDORINA
+	db 30,PARASECT
+	db 23,SCYTHER
+	db 23,CHANSEY
+
+	db 0
+
+WildMonsters36:
+	db $1E
+	db 24,NIDORAN_M
+	db 26,DODUO
+	db 22,PARAS
+	db 25,EXEGGCUTE
+	db 33,NIDORINO
+	db 23,EXEGGCUTE
+	db 24,NIDORAN_F
+	db 25,PARASECT
+	db 25,KANGASKHAN
+	db 28,SCYTHER
+
+	db 0
+
+WildMonsters37:
+	db $1E
+	db 22,NIDORAN_M
+	db 26,RHYHORN
+	db 23,PARAS
+	db 25,EXEGGCUTE
+	db 30,NIDORINO
+	db 27,EXEGGCUTE
+	db 30,NIDORINA
+	db 32,VENOMOTH
+	db 26,CHANSEY
+	db 28,TAUROS
+
+	db 0
+
+WildMonsters38:
+	db $1E
+	db 25,NIDORAN_M
+	db 26,DODUO
+	db 23,VENONAT
+	db 24,EXEGGCUTE
+	db 33,NIDORINO
+	db 26,EXEGGCUTE
+	db 25,NIDORAN_F
+	db 31,VENOMOTH
+	db 26,TAUROS
+	db 28,KANGASKHAN
+
+	db 0
+
+WaterMons:
+	db 0
+
+	db 5
+	db 5,   TENTACOOL
+	db 10,  TENTACOOL
+	db 15,  TENTACOOL
+	db 5,   TENTACOOL
+	db 10,  TENTACOOL
+	db 15,  TENTACOOL
+	db 20,  TENTACOOL
+	db 30,  TENTACOOL
+	db 35,  TENTACOOL
+	db 40,  TENTACOOL
+
+WildMonsters40:
+	db $0F
+	db 30,SEEL
+	db 30,SLOWPOKE
+	db 30,SHELLDER
+	db 30,HORSEA
+	db 28,HORSEA
+	db 21,ZUBAT
+	db 29,GOLBAT
+	db 28,PSYDUCK
+	db 28,SHELLDER
+	db 38,GOLDUCK
+
+	db 0
+
+WildMonsters41:
+	db $0A
+	db 30,STARYU
+	db 30,HORSEA
+	db 32,SHELLDER
+	db 32,HORSEA
+	db 28,SLOWPOKE
+	db 30,SEEL
+	db 30,SLOWPOKE
+	db 28,SEEL
+	db 38,DEWGONG
+	db 37,SEADRA
+
+	db 0
+
+WildMonsters42:
+	db $0A
+	db 30,SEEL
+	db 30,SLOWPOKE
+	db 32,SEEL
+	db 32,SLOWPOKE
+	db 28,HORSEA
+	db 30,STARYU
+	db 30,HORSEA
+	db 28,SHELLDER
+	db 30,GOLBAT
+	db 37,SLOWBRO
+
+	db 0
+
+WildMonsters43:
+	db $0A
+	db 31,SLOWPOKE
+	db 31,SEEL
+	db 33,SLOWPOKE
+	db 33,SEEL
+	db 29,HORSEA
+	db 31,SHELLDER
+	db 31,HORSEA
+	db 29,SHELLDER
+	db 39,SEADRA
+	db 37,DEWGONG
+
+	db 0
+
+WildMonsters44:
+	db $0A
+	db 31,HORSEA
+	db 31,SHELLDER
+	db 33,HORSEA
+	db 33,SHELLDER
+	db 29,SLOWPOKE
+	db 31,SEEL
+	db 31,SLOWPOKE
+	db 29,SEEL
+	db 39,SLOWBRO
+	db 32,GOLBAT
+
+	db 0
+
+WildMonsters45:
+	db $0A
+	db 32,KOFFING
+	db 30,KOFFING
+	db 34,PONYTA
+	db 30,PONYTA
+	db 34,GROWLITHE
+	db 32,PONYTA
+	db 30,GRIMER
+	db 28,PONYTA
+	db 37,WEEZING
+	db 39,MUK
+
+	db 0
+
+WildMonsters46:
+	db $0A
+	db 32,GROWLITHE
+	db 34,KOFFING
+	db 34,KOFFING
+	db 30,PONYTA
+	db 30,KOFFING
+	db 32,PONYTA
+	db 30,GRIMER
+	db 28,PONYTA
+	db 39,WEEZING
+	db 37,MUK
+
+	db 0
+
+WildMonsters47:
+	db $0A
+	db 31,KOFFING
+	db 33,GROWLITHE
+	db 35,KOFFING
+	db 32,PONYTA
+	db 34,PONYTA
+	db 40,WEEZING
+	db 34,GRIMER
+	db 38,WEEZING
+	db 36,PONYTA
+	db 42,MUK
+
+	db 0
+
+WildMonsters48:
+	db $0A
+	db 33,KOFFING
+	db 31,KOFFING
+	db 35,GROWLITHE
+	db 32,PONYTA
+	db 31,KOFFING
+	db 40,WEEZING
+	db 34,PONYTA
+	db 35,GRIMER
+	db 42,WEEZING
+	db 42,MUK
+
+	db 0
+
+Route21Mons:
+	db $19
+	db 21,RATTATA
+	db 23,PIDGEY
+	db 30,RATICATE
+	db 23,RATTATA
+	db 21,PIDGEY
+	db 30,PIDGEOTTO
+	db 32,PIDGEOTTO
+	db 28,TANGELA
+	db 30,TANGELA
+	db 32,TANGELA
+
+	db 5
+	db 5,TENTACOOL
+	db 10,TENTACOOL
+	db 15,TENTACOOL
+	db 5,TENTACOOL
+	db 10,TENTACOOL
+	db 15,TENTACOOL
+	db 20,TENTACOOL
+	db 30,TENTACOOL
+	db 35,TENTACOOL
+	db 40,TENTACOOL
+
+WildMonsters50:
+	db $0A
+	db 46,GOLBAT
+	db 46,HYPNO
+	db 46,MAGNETON
+	db 49,DODRIO
+	db 49,VENOMOTH
+	db 52,ARBOK
+	db 49,KADABRA
+	db 52,PARASECT
+	db 53,RAICHU
+	db 53,DITTO
+
+	db 0
+
+WildMonsters51:
+	db $0F
+	db 51,DODRIO
+	db 51,VENOMOTH
+	db 51,KADABRA
+	db 52,RHYDON
+	db 52,MAROWAK
+	db 52,ELECTRODE
+	db 56,CHANSEY
+	db 54,WIGGLYTUFF
+	db 55,DITTO
+	db 60,DITTO
+
+	db 0
+
+WildMonsters52:
+	db $19
+	db 55,RHYDON
+	db 55,MAROWAK
+	db 55,ELECTRODE
+	db 64,CHANSEY
+	db 64,PARASECT
+	db 64,RAICHU
+	db 57,ARBOK
+	db 65,DITTO
+	db 63,DITTO
+	db 67,DITTO
+
+	db 0
+
+PowerPlantMons:
+	db $0A
+	db 21,VOLTORB
+	db 21,MAGNEMITE
+	db 20,PIKACHU
+	db 24,PIKACHU
+	db 23,MAGNEMITE
+	db 23,VOLTORB
+	db 32,MAGNETON
+	db 35,MAGNETON
+	db 33,ELECTABUZZ
+	db 36,ELECTABUZZ
+
+	db 0
+
+Route23Mons:
+	db $0A
+	db 26,EKANS
+	db 33,DITTO
+	db 26,SPEAROW
+	db 38,FEAROW
+	db 38,DITTO
+	db 38,FEAROW
+	db 41,ARBOK
+	db 43,DITTO
+	db 41,FEAROW
+	db 43,FEAROW
+
+	db 0
+
+WildMonsters55:
+	db $0A
+	db 22,MACHOP
+	db 24,GEODUDE
+	db 26,ZUBAT
+	db 36,ONIX
+	db 39,ONIX
+	db 42,ONIX
+	db 41,MACHOKE
+	db 40,GOLBAT
+	db 40,MAROWAK
+	db 43,GRAVELER
+
+	db 0
+
+WildMonsters56:
+	db $0F
+	db 24,MACHOP
+	db 26,GEODUDE
+	db 22,ZUBAT
+	db 42,ONIX
+	db 40,VENOMOTH
+	db 45,ONIX
+	db 43,GRAVELER
+	db 41,GOLBAT
+	db 42,MACHOKE
+	db 45,MACHOKE
+
+	db 0
+
+WildMonsters57:
+	db $0F
+	db 24,MACHOP
+	db 26,GEODUDE
+	db 22,ZUBAT
+	db 36,ONIX
+	db 39,ONIX
+	db 42,ONIX
+	db 41,GRAVELER
+	db 41,GOLBAT
+	db 42,MACHOKE
+	db 43,MAROWAK
+
+	db 0
+
+WildMonsters58:
+	db $14
+	db 18,DIGLETT
+	db 19,DIGLETT
+	db 17,DIGLETT
+	db 20,DIGLETT
+	db 16,DIGLETT
+	db 15,DIGLETT
+	db 21,DIGLETT
+	db 22,DIGLETT
+	db 29,DUGTRIO
+	db 31,DUGTRIO
+
+	db 0
+
+
+
+INCBIN "baserom.gbc",$D5C7,$10000 - $D5C7
 
 SECTION "bank4",DATA,BANK[$4]
 INCBIN "baserom.gbc",$10000,$4000
