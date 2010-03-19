9dc6221168cefee55771073a199813b22dbd91a6
IIMarckus <mail@iimarck.us>
2010-03-19 01:25:29 -0600

    add the attacks leaders and the elite 4 use
    hg-commit-id: 6d86aadf90a4

--- pokered.asm
+++ pokered.asm
@@ -1620,9 +1620,41 @@ SECTION "bankD",DATA,BANK[$D]
 INCBIN "baserom.gbc",$34000,$4000
 
 SECTION "bankE",DATA,BANK[$E]
-INCBIN "baserom.gbc",$38000,$1D3B
+INCBIN "baserom.gbc",$38000,$1D22
 
-; trainer data: from 5D3B to 652E
+; trainer data: from 5D22 to 652E
+
+LoneAttacks: ; 5D22
+; these are used for gym leaders.
+; this is not automatic! you have to write the move you want to $D05C first.
+; e.g., erika's script writes 4 to $D05C to get mega drain.
+
+; first byte:  pokemon in the trainer's party that gets the move
+; second byte: move
+; unterminated
+	db 1,BIDE
+	db 1,BUBBLEBEAM
+	db 2,THUNDERBOLT
+	db 2,MEGA_DRAIN
+	db 3,TOXIC
+	db 3,PSYWAVE
+	db 3,FIRE_BLAST
+	db 4,FISSURE
+
+TeamAttacks: ; 5D32
+; these are used for elite four.
+; this is automatic, based on trainer class.
+; don't be confused by LoneAttacks above, the two data structures are
+	; _completely_ unrelated.
+
+; first byte: trainer (all trainers in this class have this move)
+; second byte: move
+; ff-terminated
+	db LORELEI,BLIZZARD
+	db BRUNO,FISSURE
+	db AGATHA,TOXIC
+	db LANCE,BARRIER
+	db $FF
 
 TrainerDataPointers: ; 5D3B
 	dw YoungsterData,BugCatcherData,LassData,SailorData,JrTrainerMData
