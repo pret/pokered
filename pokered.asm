cd4c3f1d908d7a8681aff818a9c73c6e25e690c1
Sawakita <sawakitanoodles@gmail.com>
2012-01-12 20:59:03 +0100

    Add routine that ends text-scripts ($24D7).
    hg-commit-id: 89e0ea79b7ff

--- pokered.asm
+++ pokered.asm
@@ -1002,7 +1002,13 @@ MartInventories: ; 2442
 	db $FE,7,ULTRA_BALL,GREAT_BALL,FULL_RESTORE,MAX_POTION,FULL_HEAL
 	db REVIVE,MAX_REPEL,$FF
 
-INCBIN "baserom.gbc",$24d6,$24e5 - $24d6
+TextScriptEndingChar:
+        db "@"
+TextScriptEnd:
+        ld hl,TextScriptEndingChar
+        ret
+
+INCBIN "baserom.gbc",$24DB,$24E5 - $24DB
 
 VictoryRoad3Text10:
 VictoryRoad3Text9:
