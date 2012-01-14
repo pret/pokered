da2c784ae78800545e6f5080c26d8a05bba8996b
Bryan Bishop <kanzure@gmail.com>
2012-01-13 22:21:16 -0600

    romvisualizer - make gif animation of progress
    hg-commit-id: c2fbb929af69

---
+++ extras/romvisualizer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#author: Bryan Bishop <kanzure@gmail.com>
+#date: 2012-01-13
+import os
+
+changeset_numbers = range(266, 635)
+
+def take_snapshot_image(changeset_number):
+    "turn pokered.asm into an image at a certain version"
+
+    print "reverting pokered.asm to r" + str(changeset_number)
+
+    #revert the file
+    os.system("hg revert ../pokered.asm -r" + str(changeset_number))
+
+    print "generating the image.."
+
+    #draw the image
+    os.system("python romviz.py")
+
+    #move the file
+    os.system("mv test.png versions/" + str(changeset_number) + ".png")
+
+for changeset_number in changeset_numbers:
+    take_snapshot_image(changeset_number)
+
