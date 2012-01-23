b205d8a02ede876f6494e5f0861e5b78348fafdb
Bryan Bishop <kanzure@gmail.com>
2012-01-23 01:16:20 -0600

    remove silly script
    hg-commit-id: 8738e623fe29

--- extras/print_pc_items.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#author: Bryan Bishop <kanzure@gmail.com>
-#date: 2012-01-19
-#spit out some constants
-
-for x in range(1, 51):
-    print "W_BOXITEM%.2d       EQU $%s" % (x, ("%.2x" % (0xd539+(x*2))).upper())
-    print "W_BOXITEM%.2dQTY    EQU $%s" % (x, ("%.2x" % (0xd539+(x*2)+1)).upper())
-
