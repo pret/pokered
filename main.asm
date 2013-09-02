a38d122f8732bddf8a6550df467bfdd8f1cd6b0a
U-Fish-PC\Daniel <corrnondacqb@yahoo.com>
2013-09-02 15:46:07 -0400

    Fix typo: Unknown4445D -> Unknown_4045D

--- main.asm
+++ main.asm
@@ -66564,7 +66564,7 @@ HeightWeightText: ; 40448 (10:4448)
 	db "HT  ?",$60,"??",$61,$4E,"WT   ???lb@"
 
 ; XXX does anything point to this?
-Unknown4445D: ; 4045d (10:445d)
+Unknown_4045D: ; 4045d (10:445d)
 	db $54,$50
 
 ; horizontal line that divides the pokedex text description from the rest of the data
