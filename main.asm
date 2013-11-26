6b37623b1d431b450f0970f15039d4503c3c6d52
U-Fish-PC\Daniel <corrnondacqb@yahoo.com>
2013-11-26 12:11:44 -0500

    FlyWarp data

--- main.asm
+++ main.asm
@@ -6831,8 +6831,8 @@ DisplayTextID: ; 2920 (0:2920)
 	jp z,FuncTX_PokemonCenterPC
 	cp a,$f5   ; Vending Machine
 	jr nz,.notVendingMachine
-	ld b,BANK(Unknown_74ee0)
-	ld hl,Unknown_74ee0
+	ld b,BANK(Func_74ee0)
+	ld hl,Func_74ee0
 	call Bankswitch
 	jr AfterDisplayingTextID
 .notVendingMachine
@@ -6840,8 +6840,8 @@ DisplayTextID: ; 2920 (0:2920)
 	jp z,FuncTX_SlotMachine
 	cp a,$f6   ; cable connection NPC in Pokemon Center
 	jr nz,.notSpecialCase
-	ld hl, Unknown_71c5
-	ld b, BANK(Unknown_71c5)
+	ld hl, Func_71c5
+	ld b, BANK(Func_71c5)
 	call Bankswitch
 	jr AfterDisplayingTextID
 .notSpecialCase
@@ -7986,7 +7986,7 @@ Func_310e: ; 310e (0:310e)
 	add a
 	ld d, $0
 	ld e, a
-	ld hl, .unknown_3140
+	ld hl, .pointerTable_3140
 	add hl, de
 	ld a, [hli]
 	ld h, [hl]
@@ -8002,10 +8002,10 @@ Func_310e: ; 310e (0:310e)
 	ld [H_LOADEDROMBANK], a
 	ld [$2000], a
 	ret
-.unknown_3140 ; 0x3140
-	dw Unknown_1a442
-	dw Unknown_1a510
-	dw Unknown_1a57d
+.pointerTable_3140
+	dw PointerTable_1a442
+	dw PointerTable_1a510
+	dw PointerTable_1a57d
 .asm_3146
 	ld b, BANK(Func_1a3e0)
 	ld hl, Func_1a3e0
@@ -14022,7 +14022,7 @@ Func_5c0a: ; 5c0a (1:5c0a)
 	ld [$d358], a
 	ld hl, $d72e
 	set 6, [hl]
-	ld hl, Unknown_6b20 ; $6b20
+	ld hl, TextTerminator_6b20 ; $6b20
 	call PrintText
 	call SaveScreenTilesToBuffer1
 	ld hl, UnnamedText_5d43 ; $5d43
@@ -14875,20 +14875,20 @@ Func_62ff: ; 62ff (1:62ff)
 	ld a, [$d72d]
 	cp $ef
 	jr nz, .asm_6314
-	ld hl, Unknown_6428 ; $6428
+	ld hl, BattleCenterSpec1 ; $6428
 	ld a, [$FF00+$aa]
 	cp $2
 	jr z, .asm_6334
-	ld hl, Unknown_6430 ; $6430
+	ld hl, BattleCenterSpec2 ; $6430
 	jr .asm_6334
 .asm_6314
 	cp $f0
 	jr nz, .asm_6326
-	ld hl, Unknown_6438 ; $6438
+	ld hl, TradeCenterSpec1 ; $6438
 	ld a, [$FF00+$aa]
 	cp $2
 	jr z, .asm_6334
-	ld hl, Unknown_6440 ; $6440
+	ld hl, TradeCenterSpec2 ; $6440
 	jr .asm_6334
 .asm_6326
 	ld a, [$d732]
@@ -14928,7 +14928,7 @@ Func_62ff: ; 62ff (1:62ff)
 	ld [W_CURMAP], a ; $d35e
 	ld a, [$d71e]
 	ld c, a
-	ld hl, Unknown_63bf ; $63bf
+	ld hl, DungeonWarpList ; $63bf
 	ld de, $0
 	ld a, $6
 	ld [$d12f], a
@@ -14948,7 +14948,7 @@ Func_62ff: ; 62ff (1:62ff)
 	ld e, a
 	jr .asm_6376
 .asm_6388
-	ld hl, Unknown_63d8 ; $63d8
+	ld hl, DungeonWarpData ; $63d8
 	add hl, de
 	jr .asm_63a4
 .asm_638e
@@ -14987,32 +14987,63 @@ Func_62ff: ; 62ff (1:62ff)
 	ld [$d42f], a
 	ret
 
-Unknown_63bf: ; 63bf (1:63bf)
-INCBIN "baserom.gbc",$63bf,$63d8 - $63bf
+DungeonWarpList: ; 63bf (1:63bf)
+	db SEAFOAM_ISLANDS_2,$01
+	db SEAFOAM_ISLANDS_2,$02
+	db SEAFOAM_ISLANDS_3,$01
+	db SEAFOAM_ISLANDS_3,$02
+	db SEAFOAM_ISLANDS_4,$01
+	db SEAFOAM_ISLANDS_4,$02
+	db SEAFOAM_ISLANDS_5,$01
+	db SEAFOAM_ISLANDS_5,$02
+	db VICTORY_ROAD_2,$02
+	db MANSION_1,$01
+	db MANSION_1,$02
+	db MANSION_2,$03
+	db $FF
 
-Unknown_63d8: ; 63d8 (1:63d8)
-INCBIN "baserom.gbc",$63d8,$6420 - $63d8
+DungeonWarpData: ; 63d8 (1:63d8)
+	FLYWARP_DATA SEAFOAM_ISLANDS_2_WIDTH,7,18
+	FLYWARP_DATA SEAFOAM_ISLANDS_2_WIDTH,7,23
+	FLYWARP_DATA SEAFOAM_ISLANDS_3_WIDTH,7,19
+	FLYWARP_DATA SEAFOAM_ISLANDS_3_WIDTH,7,22
+	FLYWARP_DATA SEAFOAM_ISLANDS_4_WIDTH,7,18
+	FLYWARP_DATA SEAFOAM_ISLANDS_4_WIDTH,7,19
+	FLYWARP_DATA SEAFOAM_ISLANDS_5_WIDTH,14,4
+	FLYWARP_DATA SEAFOAM_ISLANDS_5_WIDTH,14,5
+	FLYWARP_DATA VICTORY_ROAD_2_WIDTH,16,22
+	FLYWARP_DATA MANSION_1_WIDTH,14,16
+	FLYWARP_DATA MANSION_1_WIDTH,14,16
+	FLYWARP_DATA MANSION_2_WIDTH,14,18
 
+;Format:
+;	db Map_id
+;	FLYWARP_DATA [Map Width][Y-pos][X-pos]
+;	db Tileset_id
 FirstMapSpec: ; 6420 (1:6420)
-	db REDS_HOUSE_2F ; RedsHouse2F
-; Original Format:
-;   [Event Displacement][Y-block][X-block][Y-sub_block][X-sub_block]
-; Macro Format:
-;   FLYWARP_DATA [Map Width][Y-pos][X-pos]
-	FLYWARP_DATA 4,6,3
-	db $04 ;Tileset_id
+	db REDS_HOUSE_2F
+	FLYWARP_DATA REDS_HOUSE_2F_WIDTH,6,3
+	db $04
 
-Unknown_6428: ; 6428 (1:6428)
-INCBIN "baserom.gbc",$6428,$6430 - $6428
+BattleCenterSpec1: ; 6428 (1:6428)
+	db BATTLE_CENTER
+	FLYWARP_DATA BATTLE_CENTER_WIDTH,4,3
+	db $15
 
-Unknown_6430: ; 6430 (1:6430)
-INCBIN "baserom.gbc",$6430,$6438 - $6430
+BattleCenterSpec2: ; 6430 (1:6430)
+	db BATTLE_CENTER
+	FLYWARP_DATA BATTLE_CENTER_WIDTH,4,6
+	db $15
 
-Unknown_6438: ; 6438 (1:6438)
-INCBIN "baserom.gbc",$6438,$6440 - $6438
+TradeCenterSpec1: ; 6438 (1:6438)
+	db TRADE_CENTER
+	FLYWARP_DATA TRADE_CENTER_WIDTH,4,3
+	db $15
 
-Unknown_6440: ; 6440 (1:6440)
-INCBIN "baserom.gbc",$6440,$6448 - $6440
+TradeCenterSpec2: ; 6440 (1:6440)
+	db TRADE_CENTER
+	FLYWARP_DATA TRADE_CENTER_WIDTH,4,6
+	db $15
 
 FlyWarpDataPtr: ; 6448 (1:6448)
 	db $00,0
@@ -15838,7 +15869,7 @@ DefaultNamesRivalList: ; 6b08 (1:6b08)
 	db "NEW NAME@RED@ASH@JACK@"
 ENDC
 
-Unknown_6b20: ; 6b20 (1:6b20)
+TextTerminator_6b20: ; 6b20 (1:6b20)
 	db "@"
 
 ; subtracts the amount the player paid from their money
@@ -16776,19 +16807,19 @@ PrintStartMenuItem: ; 71bb (1:71bb)
 	add hl,de
 	ret
 
-Unknown_71c5: ; 71c5 (1:71c5)
+Func_71c5: ; 71c5 (1:71c5)
 	ld hl, UnnamedText_72b8 ; $72b8
 	call PrintText
 	ld a, [$d74b]
 	bit 5, a
-	jp nz, Unknown_71e1
+	jp nz, Func_71e1
 	ld c, $3c
 	call DelayFrames
 	ld hl, UnnamedText_72d2 ; $72d2
 	call PrintText
 	jp Func_7298
 
-Unknown_71e1: ; 71e1 (1:71e1)
+Func_71e1: ; 71e1 (1:71e1)
 	ld a, $1
 	ld [$cc34], a
 	ld a, $5a
@@ -38768,7 +38799,7 @@ Func_1a41d: ; 1a41d (6:641d)
 	ld [$ccd3], a
 	ret
 
-Unknown_1a442: ; 1a442 (6:6442)
+PointerTable_1a442: ; 1a442 (6:6442)
 	dw Func_1a44c
 	dw Func_1a485
 	dw Func_1a4a1
@@ -38877,7 +38908,7 @@ Func_1a4f4: ; 1a4f4 (6:64f4)
 	res 7, [hl]
 	jp Func_314e
 
-Unknown_1a510: ; 1a510 (6:6510)
+PointerTable_1a510: ; 1a510 (6:6510)
 	dw Func_1a514
 	dw Func_1a56b
 
@@ -38934,7 +38965,7 @@ Func_1a56b: ; 1a56b (6:656b)
 	res 7, [hl]
 	jp Func_314e
 
-Unknown_1a57d: ; 1a57d (6:657d)
+PointerTable_1a57d: ; 1a57d (6:657d)
 	dw Func_1a581
 	dw Func_1a56b
 
@@ -107462,7 +107493,7 @@ CeruleanHouse2Object: ; 0x74ebe (size=34)
 	EVENT_DISP $4, $7, $2
 	EVENT_DISP $4, $7, $3
 
-Unknown_74ee0: ; 74ee0 (1d:4ee0)
+Func_74ee0: ; 74ee0 (1d:4ee0)
 	ld hl, UnnamedText_74f99
 	call PrintText
 	ld a, $13
