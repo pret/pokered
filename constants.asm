d4d4481df061dc250e2f85ea17103b873df8b49a
U-Fish-PC\Daniel <corrnondacqb@yahoo.com>
2013-12-11 02:00:10 -0500

    Move slot machine constants

--- constants.asm
+++ constants.asm
@@ -72,8 +72,6 @@ INCLUDE "constants/palette_constants.asm"
 INCLUDE "constants/map_constants.asm"
 INCLUDE "constants/map_dimensions.asm"
 
-INCLUDE "constants/slotmachine_constants.asm"
-
 ; Evolution types
 EV_LEVEL EQU 1
 EV_ITEM  EQU 2
