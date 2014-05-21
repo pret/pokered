ea3ba4cde3706b7c77efb705555ec0c86321cbe2
U-Fish-PC\Daniel <corrnondacqb@yahoo.com>
2014-05-21 19:50:57 -0400

    Pull out map header pointers and banks

---
+++ data/map_header_banks.asm
@@ -0,0 +1,250 @@
+; see also MapHeaderPointers
+MapHeaderBanks: ; c23d (3:423d)
+	db BANK(PalletTown_h) ;PALLET_TOWN
+	db BANK(ViridianCity_h) ; VIRIDIAN_CITY
+	db BANK(PewterCity_h) ; PEWTER_CITY
+	db BANK(CeruleanCity_h) ; CERULEAN_CITY
+	db BANK(LavenderTown_h) ; LAVENDER_TOWN
+	db BANK(VermilionCity_h) ; VERMILION_CITY
+	db BANK(CeladonCity_h) ; CELADON_CITY
+	db BANK(FuchsiaCity_h) ; FUCHSIA_CITY
+	db BANK(CinnabarIsland_h) ; CINNABAR_ISLAND
+	db BANK(IndigoPlateau_h) ; INDIGO_PLATEAU
+	db BANK(SaffronCity_h) ; SAFFRON_CITY
+	db $1 ; unused
+	db BANK(Route1_h) ; ROUTE_1
+	db BANK(Route2_h) ; ROUTE_2
+	db BANK(Route3_h) ; ROUTE_3
+	db BANK(Route4_h) ; ROUTE_4
+	db BANK(Route5_h) ; ROUTE_5
+	db BANK(Route6_h) ; ROUTE_6
+	db BANK(Route7_h) ; ROUTE_7
+	db BANK(Route8_h) ; ROUTE_8
+	db BANK(Route9_h) ; ROUTE_9
+	db BANK(Route10_h) ; ROUTE_10
+	db BANK(Route11_h) ; ROUTE_11
+	db BANK(Route12_h) ; ROUTE_12
+	db BANK(Route13_h) ; ROUTE_13
+	db BANK(Route14_h) ; ROUTE_14
+	db BANK(Route15_h) ; ROUTE_15
+	db BANK(Route16_h) ; ROUTE_16
+	db BANK(Route17_h) ; ROUTE_17
+	db BANK(Route18_h) ; ROUTE_18
+	db BANK(Route19_h) ; ROUTE_19
+	db BANK(Route20_h) ; ROUTE_20
+	db BANK(Route21_h) ; ROUTE_21
+	db BANK(Route22_h) ; ROUTE_22
+	db BANK(Route23_h) ; ROUTE_23
+	db BANK(Route24_h) ; ROUTE_24
+	db BANK(Route25_h) ; ROUTE_25
+	db BANK(RedsHouse1F_h)
+	db BANK(RedsHouse2F_h)
+	db BANK(BluesHouse_h)
+	db BANK(OaksLab_h)
+	db BANK(ViridianPokecenter_h)
+	db BANK(ViridianMart_h)
+	db BANK(School_h)
+	db BANK(ViridianHouse_h)
+	db BANK(ViridianGym_h)
+	db BANK(DiglettsCaveRoute2_h)
+	db BANK(ViridianForestExit_h)
+	db BANK(Route2House_h)
+	db BANK(Route2Gate_h)
+	db BANK(ViridianForestEntrance_h)
+	db BANK(ViridianForest_h)
+	db BANK(Museum1F_h)
+	db BANK(Museum2F_h)
+	db BANK(PewterGym_h)
+	db BANK(PewterHouse1_h)
+	db BANK(PewterMart_h)
+	db BANK(PewterHouse2_h)
+	db BANK(PewterPokecenter_h)
+	db BANK(MtMoon1_h)
+	db BANK(MtMoon2_h)
+	db BANK(MtMoon3_h)
+	db BANK(CeruleanHouseTrashed_h)
+	db BANK(CeruleanHouse1_h)
+	db BANK(CeruleanPokecenter_h)
+	db BANK(CeruleanGym_h)
+	db BANK(BikeShop_h)
+	db BANK(CeruleanMart_h)
+	db BANK(MtMoonPokecenter_h)
+	db BANK(CeruleanHouseTrashed_h)
+	db BANK(Route5Gate_h)
+	db BANK(UndergroundPathEntranceRoute5_h)
+	db BANK(DayCareM_h)
+	db BANK(Route6Gate_h)
+	db BANK(UndergroundPathEntranceRoute6_h)
+	db BANK(UndergroundPathEntranceRoute6_h) ;FREEZE
+	db BANK(Route7Gate_h)
+	db BANK(UndergroundPathEntranceRoute7_h)
+	db BANK(UndergroundPathEntranceRoute7Copy_h) ;FREEZE
+	db BANK(Route8Gate_h)
+	db BANK(UndergroundPathEntranceRoute8_h)
+	db BANK(RockTunnelPokecenter_h)
+	db BANK(RockTunnel1_h)
+	db BANK(PowerPlant_h)
+	db BANK(Route11Gate_h)
+	db BANK(DiglettsCaveEntranceRoute11_h)
+	db BANK(Route11GateUpstairs_h)
+	db BANK(Route12Gate_h)
+	db BANK(BillsHouse_h)
+	db BANK(VermilionPokecenter_h)
+	db BANK(FanClub_h)
+	db BANK(VermilionMart_h)
+	db BANK(VermilionGym_h)
+	db BANK(VermilionHouse1_h)
+	db BANK(VermilionDock_h)
+	db BANK(SSAnne1_h)
+	db BANK(SSAnne2_h)
+	db BANK(SSAnne3_h)
+	db BANK(SSAnne4_h)
+	db BANK(SSAnne5_h)
+	db BANK(SSAnne6_h)
+	db BANK(SSAnne7_h)
+	db BANK(SSAnne8_h)
+	db BANK(SSAnne9_h)
+	db BANK(SSAnne10_h)
+	db $1D ;unused
+	db $1D ;unused
+	db $1D ;unused
+	db BANK(VictoryRoad1_h)
+	db $1D ;unused
+	db $1D ;unused
+	db $1D ;unused
+	db $1D ;unused
+	db BANK(Lance_h)
+	db $1D ;unused
+	db $1D ;unused
+	db $1D ;unused
+	db $1D ;unused
+	db BANK(HallofFameRoom_h)
+	db BANK(UndergroundPathNS_h)
+	db BANK(Gary_h)
+	db BANK(UndergroundPathWE_h)
+	db BANK(CeladonMart1_h)
+	db BANK(CeladonMart2_h)
+	db BANK(CeladonMart3_h)
+	db BANK(CeladonMart4_h)
+	db BANK(CeladonMartRoof_h)
+	db BANK(CeladonMartElevator_h)
+	db BANK(CeladonMansion1_h)
+	db BANK(CeladonMansion2_h)
+	db BANK(CeladonMansion3_h)
+	db BANK(CeladonMansion4_h)
+	db BANK(CeladonMansion5_h)
+	db BANK(CeladonPokecenter_h)
+	db BANK(CeladonGym_h)
+	db BANK(CeladonGameCorner_h)
+	db BANK(CeladonMart5_h)
+	db BANK(CeladonPrizeRoom_h)
+	db BANK(CeladonDiner_h)
+	db BANK(CeladonHouse_h)
+	db BANK(CeladonHotel_h)
+	db BANK(LavenderPokecenter_h)
+	db BANK(PokemonTower1_h)
+	db BANK(PokemonTower2_h)
+	db BANK(PokemonTower3_h)
+	db BANK(PokemonTower4_h)
+	db BANK(PokemonTower5_h)
+	db BANK(PokemonTower6_h)
+	db BANK(PokemonTower7_h)
+	db BANK(LavenderHouse1_h)
+	db BANK(LavenderMart_h)
+	db BANK(LavenderHouse2_h)
+	db BANK(FuchsiaMart_h)
+	db BANK(FuchsiaHouse1_h)
+	db BANK(FuchsiaPokecenter_h)
+	db BANK(FuchsiaHouse2_h)
+	db BANK(SafariZoneEntrance_h)
+	db BANK(FuchsiaGym_h)
+	db BANK(FuchsiaMeetingRoom_h)
+	db BANK(SeafoamIslands2_h)
+	db BANK(SeafoamIslands3_h)
+	db BANK(SeafoamIslands4_h)
+	db BANK(SeafoamIslands5_h)
+	db BANK(VermilionHouse2_h)
+	db BANK(FuchsiaHouse3_h)
+	db BANK(Mansion1_h)
+	db BANK(CinnabarGym_h)
+	db BANK(Lab1_h)
+	db BANK(Lab2_h)
+	db BANK(Lab3_h)
+	db BANK(Lab4_h)
+	db BANK(CinnabarPokecenter_h)
+	db BANK(CinnabarMart_h)
+	db BANK(CinnabarMart_h)
+	db BANK(IndigoPlateauLobby_h)
+	db BANK(CopycatsHouse1F_h)
+	db BANK(CopycatsHouse2F_h)
+	db BANK(FightingDojo_h)
+	db BANK(SaffronGym_h)
+	db BANK(SaffronHouse1_h)
+	db BANK(SaffronMart_h)
+	db BANK(SilphCo1_h)
+	db BANK(SaffronPokecenter_h)
+	db BANK(SaffronHouse2_h)
+	db BANK(Route15Gate_h)
+	db BANK(Route15GateUpstairs_h)
+	db BANK(Route16Gate_h)
+	db BANK(Route16GateUpstairs_h)
+	db BANK(Route16House_h)
+	db BANK(Route12House_h)
+	db BANK(Route18Gate_h)
+	db BANK(Route18GateUpstairs_h)
+	db BANK(SeafoamIslands1_h)
+	db BANK(Route22Gate_h)
+	db BANK(VictoryRoad2_h)
+	db BANK(Route12GateUpstairs_h)
+	db BANK(VermilionHouse3_h)
+	db BANK(DiglettsCave_h)
+	db BANK(VictoryRoad3_h)
+	db BANK(RocketHideout1_h)
+	db BANK(RocketHideout2_h)
+	db BANK(RocketHideout3_h)
+	db BANK(RocketHideout4_h)
+	db BANK(RocketHideoutElevator_h)
+	db $01
+	db $01
+	db $01
+	db BANK(SilphCo2_h)
+	db BANK(SilphCo3_h)
+	db BANK(SilphCo4_h)
+	db BANK(SilphCo5_h)
+	db BANK(SilphCo6_h)
+	db BANK(SilphCo7_h)
+	db BANK(SilphCo8_h)
+	db BANK(Mansion2_h)
+	db BANK(Mansion3_h)
+	db BANK(Mansion4_h)
+	db BANK(SafariZoneEast_h)
+	db BANK(SafariZoneNorth_h)
+	db BANK(SafariZoneWest_h)
+	db BANK(SafariZoneCenter_h)
+	db BANK(SafariZoneRestHouse1_h)
+	db BANK(SafariZoneSecretHouse_h)
+	db BANK(SafariZoneRestHouse2_h)
+	db BANK(SafariZoneRestHouse3_h)
+	db BANK(SafariZoneRestHouse4_h)
+	db BANK(UnknownDungeon2_h)
+	db BANK(UnknownDungeon3_h)
+	db BANK(UnknownDungeon1_h)
+	db BANK(NameRater_h)
+	db BANK(CeruleanHouse2_h)
+	db $01
+	db BANK(RockTunnel2_h)
+	db BANK(SilphCo9_h)
+	db BANK(SilphCo10_h)
+	db BANK(SilphCo11_h)
+	db BANK(SilphCoElevator_h)
+	db $11
+	db $11
+	db BANK(BattleCenterM_h)
+	db BANK(TradeCenterM_h)
+	db $11
+	db $11
+	db $11
+	db $11
+	db BANK(Lorelei_h)
+	db BANK(Bruno_h)
+	db BANK(Agatha_h)
---
+++ data/map_header_pointers.asm
@@ -0,0 +1,250 @@
+; see also MapHeaderBanks
+MapHeaderPointers:: ; 01ae (0:01ae)
+	dw PalletTown_h
+	dw ViridianCity_h
+	dw PewterCity_h
+	dw CeruleanCity_h
+	dw LavenderTown_h
+	dw VermilionCity_h
+	dw CeladonCity_h
+	dw FuchsiaCity_h
+	dw CinnabarIsland_h
+	dw IndigoPlateau_h
+	dw SaffronCity_h
+	dw SaffronCity_h
+	dw Route1_h
+	dw Route2_h
+	dw Route3_h
+	dw Route4_h
+	dw Route5_h
+	dw Route6_h
+	dw Route7_h
+	dw Route8_h
+	dw Route9_h
+	dw Route10_h
+	dw Route11_h
+	dw Route12_h
+	dw Route13_h
+	dw Route14_h
+	dw Route15_h
+	dw Route16_h
+	dw Route17_h
+	dw Route18_h
+	dw Route19_h
+	dw Route20_h
+	dw Route21_h
+	dw Route22_h
+	dw Route23_h
+	dw Route24_h
+	dw Route25_h
+	dw RedsHouse1F_h
+	dw RedsHouse2F_h
+	dw BluesHouse_h
+	dw OaksLab_h ;id=40
+	dw ViridianPokecenter_h
+	dw ViridianMart_h
+	dw School_h
+	dw ViridianHouse_h
+	dw ViridianGym_h
+	dw DiglettsCaveRoute2_h
+	dw ViridianForestExit_h
+	dw Route2House_h
+	dw Route2Gate_h
+	dw ViridianForestEntrance_h ;id=50
+	dw ViridianForest_h
+	dw Museum1F_h
+	dw Museum2F_h
+	dw PewterGym_h
+	dw PewterHouse1_h
+	dw PewterMart_h
+	dw PewterHouse2_h
+	dw PewterPokecenter_h
+	dw MtMoon1_h
+	dw MtMoon2_h ;id=60
+	dw MtMoon3_h
+	dw CeruleanHouseTrashed_h
+	dw CeruleanHouse1_h
+	dw CeruleanPokecenter_h
+	dw CeruleanGym_h
+	dw BikeShop_h
+	dw CeruleanMart_h
+	dw MtMoonPokecenter_h
+	dw CeruleanHouseTrashed_h ; copy
+	dw Route5Gate_h
+	dw UndergroundPathEntranceRoute5_h
+	dw DayCareM_h
+	dw Route6Gate_h
+	dw UndergroundPathEntranceRoute6_h
+	dw UndergroundPathEntranceRoute6_h ; unused
+	dw Route7Gate_h
+	dw UndergroundPathEntranceRoute7_h
+	dw UndergroundPathEntranceRoute7Copy_h
+	dw Route8Gate_h
+	dw UndergroundPathEntranceRoute8_h ;id=80
+	dw RockTunnelPokecenter_h
+	dw RockTunnel1_h
+	dw PowerPlant_h
+	dw Route11Gate_h
+	dw DiglettsCaveEntranceRoute11_h
+	dw Route11GateUpstairs_h
+	dw Route12Gate_h
+	dw BillsHouse_h
+	dw VermilionPokecenter_h
+	dw FanClub_h ;id=90
+	dw VermilionMart_h
+	dw VermilionGym_h
+	dw VermilionHouse1_h
+	dw VermilionDock_h
+	dw SSAnne1_h
+	dw SSAnne2_h
+	dw SSAnne3_h
+	dw SSAnne4_h
+	dw SSAnne5_h
+	dw SSAnne6_h ;id=100
+	dw SSAnne7_h
+	dw SSAnne8_h
+	dw SSAnne9_h
+	dw SSAnne10_h
+	dw Lance_h ; unused
+	dw Lance_h ; unused
+	dw Lance_h ; unused
+	dw VictoryRoad1_h
+	dw Lance_h ; unused
+	dw Lance_h ; unused ;id=110
+	dw Lance_h ; unused
+	dw Lance_h ; unused
+	dw Lance_h
+	dw Lance_h ; unused
+	dw Lance_h ; unused
+	dw Lance_h ; unused
+	dw Lance_h ; unused
+	dw HallofFameRoom_h
+	dw UndergroundPathNS_h
+	dw Gary_h ;id=120
+	dw UndergroundPathWE_h
+	dw CeladonMart1_h
+	dw CeladonMart2_h
+	dw CeladonMart3_h
+	dw CeladonMart4_h
+	dw CeladonMartRoof_h
+	dw CeladonMartElevator_h
+	dw CeladonMansion1_h
+	dw CeladonMansion2_h
+	dw CeladonMansion3_h ;id=130
+	dw CeladonMansion4_h
+	dw CeladonMansion5_h
+	dw CeladonPokecenter_h
+	dw CeladonGym_h
+	dw CeladonGameCorner_h
+	dw CeladonMart5_h
+	dw CeladonPrizeRoom_h
+	dw CeladonDiner_h
+	dw CeladonHouse_h
+	dw CeladonHotel_h ;id=140
+	dw LavenderPokecenter_h
+	dw PokemonTower1_h
+	dw PokemonTower2_h
+	dw PokemonTower3_h
+	dw PokemonTower4_h
+	dw PokemonTower5_h
+	dw PokemonTower6_h
+	dw PokemonTower7_h
+	dw LavenderHouse1_h
+	dw LavenderMart_h ;id=150
+	dw LavenderHouse2_h
+	dw FuchsiaMart_h
+	dw FuchsiaHouse1_h
+	dw FuchsiaPokecenter_h
+	dw FuchsiaHouse2_h
+	dw SafariZoneEntrance_h
+	dw FuchsiaGym_h
+	dw FuchsiaMeetingRoom_h
+	dw SeafoamIslands2_h
+	dw SeafoamIslands3_h ;id=160
+	dw SeafoamIslands4_h
+	dw SeafoamIslands5_h
+	dw VermilionHouse2_h
+	dw FuchsiaHouse3_h
+	dw Mansion1_h
+	dw CinnabarGym_h
+	dw Lab1_h
+	dw Lab2_h
+	dw Lab3_h
+	dw Lab4_h ;id=170
+	dw CinnabarPokecenter_h
+	dw CinnabarMart_h
+	dw CinnabarMart_h ; unused
+	dw IndigoPlateauLobby_h
+	dw CopycatsHouse1F_h
+	dw CopycatsHouse2F_h
+	dw FightingDojo_h
+	dw SaffronGym_h
+	dw SaffronHouse1_h
+	dw SaffronMart_h ;id=180
+	dw SilphCo1_h
+	dw SaffronPokecenter_h
+	dw SaffronHouse2_h
+	dw Route15Gate_h
+	dw Route15GateUpstairs_h
+	dw Route16Gate_h
+	dw Route16GateUpstairs_h
+	dw Route16House_h
+	dw Route12House_h
+	dw Route18Gate_h ;id=190
+	dw Route18GateUpstairs_h
+	dw SeafoamIslands1_h
+	dw Route22Gate_h
+	dw VictoryRoad2_h
+	dw Route12GateUpstairs_h
+	dw VermilionHouse3_h
+	dw DiglettsCave_h
+	dw VictoryRoad3_h
+	dw RocketHideout1_h
+	dw RocketHideout2_h ;200
+	dw RocketHideout3_h
+	dw RocketHideout4_h
+	dw RocketHideoutElevator_h
+	dw RocketHideoutElevator_h ; unused
+	dw RocketHideoutElevator_h ; unused
+	dw RocketHideoutElevator_h ; unused
+	dw SilphCo2_h
+	dw SilphCo3_h
+	dw SilphCo4_h
+	dw SilphCo5_h ;210
+	dw SilphCo6_h
+	dw SilphCo7_h
+	dw SilphCo8_h
+	dw Mansion2_h
+	dw Mansion3_h
+	dw Mansion4_h
+	dw SafariZoneEast_h
+	dw SafariZoneNorth_h
+	dw SafariZoneWest_h
+	dw SafariZoneCenter_h ;220
+	dw SafariZoneRestHouse1_h
+	dw SafariZoneSecretHouse_h
+	dw SafariZoneRestHouse2_h
+	dw SafariZoneRestHouse3_h
+	dw SafariZoneRestHouse4_h
+	dw UnknownDungeon2_h
+	dw UnknownDungeon3_h
+	dw UnknownDungeon1_h
+	dw NameRater_h
+	dw CeruleanHouse2_h
+	dw Route16Gate_h ; unused
+	dw RockTunnel2_h
+	dw SilphCo9_h
+	dw SilphCo10_h
+	dw SilphCo11_h
+	dw SilphCoElevator_h
+	dw SilphCo2_h ; unused
+	dw SilphCo2_h ; unused
+	dw BattleCenterM_h
+	dw TradeCenterM_h
+	dw SilphCo2_h ; unused
+	dw SilphCo2_h ; unused
+	dw SilphCo2_h ; unused
+	dw SilphCo2_h ; unused
+	dw Lorelei_h
+	dw Bruno_h
+	dw Agatha_h ;247
