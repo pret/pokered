9ea5e580c990c536d28b11d5ef5de335854ee758
John Mothershed <jmothershed1@gmail.com>
2014-08-23 18:30:42 -0500

    More tackling of home.asm

--- home.asm
+++ home.asm
@@ -26,75 +26,10 @@ INCLUDE "home/utilities/wd0dcb.asm"
 INCLUDE "home/utilities/mon-sprite.asm"
 INCLUDE "home/utilities/mon-cry.asm"
 INCLUDE "home/utilities/party-menu.asm"
+INCLUDE "home/utilities/status-condition.asm"
+INCLUDE "home/utilities/level.asm"
 
-; prints a pokemon's status condition
-; INPUT:
-; de = address of status condition
-; hl = destination address
-PrintStatusCondition:: ; 14e1 (0:14e1)
-	push de
-	dec de
-	dec de ; de = address of current HP
-	ld a,[de]
-	ld b,a
-	dec de
-	ld a,[de]
-	or b ; is the pokemon's HP zero?
-	pop de
-	jr nz,PrintStatusConditionNotFainted
-; if the pokemon's HP is 0, print "FNT"
-	ld a,"F"
-	ld [hli],a
-	ld a,"N"
-	ld [hli],a
-	ld [hl],"T"
-	and a
-	ret
-PrintStatusConditionNotFainted ; 14f6
-	ld a,[H_LOADEDROMBANK]
-	push af
-	ld a,BANK(PrintStatusAilment)
-	ld [H_LOADEDROMBANK],a
-	ld [$2000],a
-	call PrintStatusAilment ; print status condition
-	pop bc
-	ld a,b
-	ld [H_LOADEDROMBANK],a
-	ld [$2000],a
-	ret
-
-; function to print pokemon level, leaving off the ":L" if the level is at least 100
-; INPUT:
-; hl = destination address
-; [wcfb9] = level
-PrintLevel:: ; 150b (0:150b)
-	ld a,$6e ; ":L" tile ID
-	ld [hli],a
-	ld c,2 ; number of digits
-	ld a,[wcfb9] ; level
-	cp a,100
-	jr c,PrintLevelCommon
-; if level at least 100, write over the ":L" tile
-	dec hl
-	inc c ; increment number of digits to 3
-	jr PrintLevelCommon
-
-; prints the level without leaving off ":L" regardless of level
-; INPUT:
-; hl = destination address
-; [wcfb9] = level
-PrintLevelFull:: ; 151b (0:151b)
-	ld a,$6e ; ":L" tile ID
-	ld [hli],a
-	ld c,3 ; number of digits
-	ld a,[wcfb9] ; level
-
-PrintLevelCommon:: ; 1523 (0:1523)
-	ld [wd11e],a
-	ld de,wd11e
-	ld b,$41 ; no leading zeroes, left-aligned, one byte
-	jp PrintNumber
-
+; Need to figure out what this does
 Func_152e:: ; 152e (0:152e)
 	ld hl,wd0dc
 	ld c,a
