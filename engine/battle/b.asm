6cb51a1c4efe543b153219677ba5a842d189ff98
U-Daniel-PC\Daniel <corrnondacqb@yahoo.com>
2015-02-26 23:27:51 -0500

    Label typo closes https://github.com/iimarckus/pokered/issues/81

--- engine/battle/b.asm
+++ engine/battle/b.asm
@@ -3,14 +3,14 @@ DisplayEffectiveness: ; 2fb7b (b:7b7b)
 	and a, $7F
 	cp a, $0A
 	ret z
-	ld hl, SupperEffectiveText
+	ld hl, SuperEffectiveText
 	jr nc, .done
 	ld hl, NotVeryEffectiveText
 .done
 	jp PrintText
 
-SupperEffectiveText: ; 2fb8e (b:7b8e)
-	TX_FAR _SupperEffectiveText
+SuperEffectiveText: ; 2fb8e (b:7b8e)
+	TX_FAR _SuperEffectiveText
 	db "@"
 
 NotVeryEffectiveText: ; 2fb93 (b:7b93)
