05195be15edac465a224e9fc1423b99daf25b8b2
Marcus Huderle <huderlem@gmail.com>
2016-02-15 10:17:54 -0800

    Update wild mon encounters to use 2-byte mon ids.

--- engine/battle/wild_encounters.asm
+++ engine/battle/wild_encounters.asm
@@ -75,9 +75,12 @@ TryDoWildEncounter: ; 13870 (4:7870)
 	add hl, bc
 	ld a, [hli]
 	ld [wCurEnemyLVL], a
-	ld a, [hl]
+	ld a, [hli]
 	ld [wcf91], a
 	ld [wEnemyMonSpecies2], a
+	ld a, [hl]
+	ld [wcf91 + 1], a
+	ld [wEnemyMonSpecies2 + 1], a
 	ld a, [wRepelRemainingSteps]
 	and a
 	jr z, .willEncounter
@@ -105,14 +108,14 @@ WildMonEncounterSlotChances: ; 13918 (4:7918)
 ; There are 10 slots for wild pokemon, and this is the table that defines how common each of
 ; those 10 slots is. A random number is generated and then the first byte of each pair in this
 ; table is compared against that random number. If the random number is less than or equal
-; to the first byte, then that slot is chosen.  The second byte is double the slot number.
+; to the first byte, then that slot is chosen.  The second byte is triple the slot number.
 	db $32, $00 ; 51/256 = 19.9% chance of slot 0
-	db $65, $02 ; 51/256 = 19.9% chance of slot 1
-	db $8C, $04 ; 39/256 = 15.2% chance of slot 2
-	db $A5, $06 ; 25/256 =  9.8% chance of slot 3
-	db $BE, $08 ; 25/256 =  9.8% chance of slot 4
-	db $D7, $0A ; 25/256 =  9.8% chance of slot 5
-	db $E4, $0C ; 13/256 =  5.1% chance of slot 6
-	db $F1, $0E ; 13/256 =  5.1% chance of slot 7
-	db $FC, $10 ; 11/256 =  4.3% chance of slot 8
-	db $FF, $12 ;  3/256 =  1.2% chance of slot 9
+	db $65, $03 ; 51/256 = 19.9% chance of slot 1
+	db $8C, $06 ; 39/256 = 15.2% chance of slot 2
+	db $A5, $09 ; 25/256 =  9.8% chance of slot 3
+	db $BE, $0C ; 25/256 =  9.8% chance of slot 4
+	db $D7, $0E ; 25/256 =  9.8% chance of slot 5
+	db $E4, $12 ; 13/256 =  5.1% chance of slot 6
+	db $F1, $15 ; 13/256 =  5.1% chance of slot 7
+	db $FC, $18 ; 11/256 =  4.3% chance of slot 8
+	db $FF, $1B ;  3/256 =  1.2% chance of slot 9
