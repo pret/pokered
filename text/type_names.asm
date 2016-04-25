36a512d19b2759fa1577968a5a15a39472809bbb
Me? <nomar5mikey@gmail.com>
2016-04-24 21:55:29 -0400

    Add Dark- and Steel-types

--- text/type_names.asm
+++ text/type_names.asm
@@ -6,21 +6,9 @@ TypeNames:
 	dw .Poison
 	dw .Ground
 	dw .Rock
-	dw .Bird
 	dw .Bug
 	dw .Ghost
-
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
-	dw .Normal
+	dw .Steel
 
 	dw .Fire
 	dw .Water
@@ -29,6 +17,7 @@ TypeNames:
 	dw .Psychic
 	dw .Ice
 	dw .Dragon
+	dw .Dark
 
 .Normal:   db "NORMAL@"
 .Fighting: db "FIGHTING@"
@@ -42,7 +31,8 @@ TypeNames:
 .Ice:      db "ICE@"
 .Ground:   db "GROUND@"
 .Rock:     db "ROCK@"
-.Bird:     db "BIRD@"
 .Bug:      db "BUG@"
 .Ghost:    db "GHOST@"
 .Dragon:   db "DRAGON@"
+.Dark:     db "DARK@"
+.Steel     db "STEEL@"
