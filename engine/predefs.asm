7c3a90bf0181127fab4cd98ad4a3173a2984f51e
YamaArashi <shadow962@live.com>
2017-01-07 11:13:52 -0800

    use actual predef label

--- engine/predefs.asm
+++ engine/predefs.asm
@@ -58,7 +58,7 @@ PredefPointers::
 	add_predef ScaleSpriteByTwo
 	add_predef LoadMonBackPic
 	add_predef CopyDownscaledMonTiles
-	add_predef LoadMissableObjects
+	dbw $03,JumpMoveEffect ; wrong bank
 	add_predef HealParty
 	add_predef MoveAnimation
 	add_predef DivideBCDPredef
