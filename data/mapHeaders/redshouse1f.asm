ee723161025844bf3606cd7e5c3b867db7a94160
Sam Nelson <snelson@cs.uoregon.edu>
2018-07-15 10:50:44 -0700

    make comments consistent throughout all mapHeader files

--- data/mapHeaders/redshouse1f.asm
+++ data/mapHeaders/redshouse1f.asm
@@ -1,6 +1,6 @@
 RedsHouse1F_h:
 	db REDS_HOUSE_1 ; tileset
-	db REDS_HOUSE_1F_HEIGHT, REDS_HOUSE_1F_WIDTH ; dimensions
-	dw RedsHouse1FBlocks, RedsHouse1FTextPointers, RedsHouse1FScript
-	db 0 ; no connections
-	dw RedsHouse1FObject
+	db REDS_HOUSE_1F_HEIGHT, REDS_HOUSE_1F_WIDTH ; dimensions (y, x)
+	dw RedsHouse1FBlocks, RedsHouse1FTextPointers, RedsHouse1FScript ; blocks, texts, scripts
+	db 0 ; connections
+	dw RedsHouse1FObject ; objects
