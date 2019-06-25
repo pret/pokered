b0a78c3319c5fa41e70274f7860b88c3081b996b
EgaoDaYo <51708131+EgaoDaYo@users.noreply.github.com>
2019-06-25 10:12:25 +0300

    Update Route2TradeHouse.asm

--- text/maps/Route2TradeHouse.asm
+++ text/maps/Route2TradeHouse.asm
@@ -1,6 +1,7 @@
 _Route2HouseText1::
-	text "A fainted #MON"
-	line "can't fight. But, "
-	cont "it can still use "
-	cont "moves like CUT!"
+	text "פוקימון מעולף"
+	line ".לא יכול להילחם"
+	cont "אבל עדיין יכול"
+	cont "להשתמש במתקפות"
+	cont "!כמו חתך"
 	done
