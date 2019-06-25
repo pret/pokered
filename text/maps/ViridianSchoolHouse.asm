c41b265134107e225785d949abb87d8cae8d223e
EgaoDaYo <51708131+EgaoDaYo@users.noreply.github.com>
2019-06-25 09:13:30 +0300

    Update ViridianSchoolHouse.asm

--- text/maps/ViridianSchoolHouse.asm
+++ text/maps/ViridianSchoolHouse.asm
@@ -1,13 +1,13 @@
 _SchoolText1::
-	text "Whew! I'm trying"
-	line "to memorize all"
-	cont "my notes."
+	text "פיו! אני מנסה"
+	line "לשנן את כל"
+	cont ".ההערות שלי"
 	done
 
 _SchoolText2::
-	text "Okay!"
+	text "!יופי"
 
-	para "Be sure to read"
-	line "the blackboard"
-	cont "carefully!"
+	para "תדאג לקרוא"
+	line "בתשומת לב"
+	cont "!מהלוח"
 	done
