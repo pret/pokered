1be474ecb10b938b1587c7a5d73750ec743274b6
Rangi <remy.oukaour+rangi42@gmail.com>
2020-06-27 17:02:08 -0400

    Replace .tilecoll files with tile ID db data

---
+++ data/collision_tile_ids.asm
@@ -0,0 +1,82 @@
+Underground_Coll::
+	db $0b, $0c, $13, $15, $18
+	db -1 ; end
+
+Overworld_Coll::
+	db $00, $10, $1b, $20, $21, $23, $2c, $2d, $2e, $30, $31, $33, $39, $3c, $3e, $52, $54, $58, $5b
+	db -1 ; end
+
+RedsHouse1_Coll::
+RedsHouse2_Coll::
+	db $01, $02, $03, $11, $12, $13, $14, $1c, $1a
+	db -1 ; end
+
+Mart_Coll::
+Pokecenter_Coll::
+	db $11, $1a, $1c, $3c, $5e
+	db -1 ; end
+
+Dojo_Coll::
+Gym_Coll::
+	db $11, $16, $19, $2b, $3c, $3d, $3f, $4a, $4c, $4d, $03
+	db -1 ; end
+
+Forest_Coll::
+	db $1e, $20, $2e, $30, $34, $37, $39, $3a, $40, $51, $52, $5a, $5c, $5e, $5f
+	db -1 ; end
+
+House_Coll::
+	db $01, $12, $14, $28, $32, $37, $44, $54, $5c
+	db -1 ; end
+
+ForestGate_Coll::
+Museum_Coll::
+Gate_Coll::
+	db $01, $12, $14, $1a, $1c, $37, $38, $3b, $3c, $5e
+	db -1 ; end
+
+Ship_Coll::
+	db $04, $0d, $17, $1d, $1e, $23, $34, $37, $39, $4a
+	db -1 ; end
+
+ShipPort_Coll::
+	db $0a, $1a, $32, $3b
+	db -1 ; end
+
+Cemetery_Coll::
+	db $01, $10, $13, $1b, $22, $42, $52
+	db -1 ; end
+
+Interior_Coll::
+	db $04, $0f, $15, $1f, $3b, $45, $47, $55, $56
+	db -1 ; end
+
+Cavern_Coll::
+	db $05, $15, $18, $1a, $20, $21, $22, $2a, $2d, $30
+	db -1 ; end
+
+	db -1 ; unused
+
+Lobby_Coll::
+	db $14, $17, $1a, $1c, $20, $38, $45
+	db -1 ; end
+
+Mansion_Coll::
+	db $01, $05, $11, $12, $14, $1a, $1c, $2c, $53
+	db -1 ; end
+
+Lab_Coll::
+	db $0c, $26, $16, $1e, $34, $37
+	db -1 ; end
+
+Club_Coll::
+	db $0f, $1a, $1f, $26, $28, $29, $2c, $2d, $2e, $2f, $41
+	db -1 ; end
+
+Facility_Coll::
+	db $01, $10, $11, $13, $1b, $20, $21, $22, $30, $31, $32, $42, $43, $48, $52, $55, $58, $5e
+	db -1 ; end
+
+Plateau_Coll::
+	db $1b, $23, $2c, $2d, $3b, $45
+	db -1 ; end
