2d562a673bbef2c8ecb7cfb9d5b1d080552dd465
Rangi <remy.oukaour+rangi42@gmail.com>
2020-06-27 22:24:13 -0400

    Factor out shared audio/notes.asm

--- audio/engine_1.asm
+++ audio/engine_1.asm
@@ -1723,15 +1723,4 @@ Audio1_HWChannelEnableMasks:
 	db HW_CH1_ENABLE_MASK, HW_CH2_ENABLE_MASK, HW_CH3_ENABLE_MASK, HW_CH4_ENABLE_MASK ; channels 4-7
 
 Audio1_Pitches:
-	dw $F82C ; C_
-	dw $F89D ; C#
-	dw $F907 ; D_
-	dw $F96B ; D#
-	dw $F9CA ; E_
-	dw $FA23 ; F_
-	dw $FA77 ; F#
-	dw $FAC7 ; G_
-	dw $FB12 ; G#
-	dw $FB58 ; A_
-	dw $FB9B ; A#
-	dw $FBDA ; B_
+INCLUDE "audio/notes.asm"
