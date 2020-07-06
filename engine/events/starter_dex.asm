f1133619526999de6ad1e0b5f36a7e69f4b8ef1f
Rangi <remy.oukaour+rangi42@gmail.com>
2020-07-06 19:50:36 -0400

    Use DEX_* constants for StarterDex

--- engine/events/starter_dex.asm
+++ engine/events/starter_dex.asm
@@ -1,9 +1,9 @@
 ; this function temporarily makes the starters (and Ivysaur) seen
 ; so that the full Pokedex information gets displayed in Oak's lab
 StarterDex:
-	ld a, %01001011 ; set starter flags
+	ld a, 1 << (DEX_BULBASAUR - 1) | 1 << (DEX_IVYSAUR - 1) | 1 << (DEX_CHARMANDER - 1) | 1 << (DEX_SQUIRTLE - 1)
 	ld [wPokedexOwned], a
 	predef ShowPokedexData
-	xor a ; unset starter flags
+	xor a
 	ld [wPokedexOwned], a
 	ret
