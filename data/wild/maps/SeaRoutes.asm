33ea73496a8145a95c5a35386bc3b8f581e0e72b
Rangi <remy.oukaour+rangi42@gmail.com>
2020-08-02 01:10:24 -0400

    Fix encounter rate comments

--- data/wild/maps/SeaRoutes.asm
+++ data/wild/maps/SeaRoutes.asm
@@ -1,7 +1,7 @@
 WaterMons:
-	db 0 ; water encounter rate
+	db 0 ; grass encounter rate
 
-	db 5 ; grass encounter rate
+	db 5 ; water encounter rate
 	db  5, TENTACOOL
 	db 10, TENTACOOL
 	db 15, TENTACOOL
