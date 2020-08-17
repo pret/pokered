f4514ff53ce74d13013c994cfd321aa711fc95c2
Rangi <remy.oukaour+rangi42@gmail.com>
2020-08-17 17:40:27 -0400

    Use map constants for SilphCoElevator warps

--- data/maps/objects/SilphCoElevator.asm
+++ data/maps/objects/SilphCoElevator.asm
@@ -2,8 +2,8 @@ SilphCoElevator_Object:
 	db $f ; border block
 
 	def_warps
-	warp 1, 3, 0, 237
-	warp 2, 3, 0, 237
+	warp 1, 3, 0, UNUSED_MAP_ED
+	warp 2, 3, 0, UNUSED_MAP_ED
 
 	def_signs
 	sign 3, 0, 1 ; SilphCoElevatorText1
