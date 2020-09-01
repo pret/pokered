df144ec5f35e2a8856ee5a91f031076560140eab
Rangi <remy.oukaour+rangi42@gmail.com>
2020-09-01 12:19:24 -0400

    Pan Docs: "When sending three or more data sets, data is continued in further packet(s). Unused bytes at the end of the last packet should be set to zero."

--- data/sgb/sgb_packets.asm
+++ data/sgb/sgb_packets.asm
@@ -13,35 +13,35 @@ ENDM
 PAL_SET: MACRO
 	db ($a << 3) + 1
 	dw \1, \2, \3, \4
-	ds 7
+	ds 7, 0
 ENDM
 
 PAL_TRN: MACRO
-	db ($b<< 3) + 1
-	ds 15
+	db ($b << 3) + 1
+	ds 15, 0
 ENDM
 
 MLT_REQ: MACRO
 	db ($11 << 3) + 1
 	db \1 - 1
-	ds 14
+	ds 14, 0
 ENDM
 
 CHR_TRN: MACRO
 	db ($13 << 3) + 1
 	db \1 + (\2 << 1)
-	ds 14
+	ds 14, 0
 ENDM
 
 PCT_TRN: MACRO
 	db ($14 << 3) + 1
-	ds 15
+	ds 15, 0
 ENDM
 
 MASK_EN: MACRO
 	db ($17 << 3) + 1
 	db \1
-	ds 14
+	ds 14, 0
 ENDM
 
 DATA_SND: MACRO
@@ -54,9 +54,11 @@ ENDM
 BlkPacket_WholeScreen:
 	ATTR_BLK 1
 	ATTR_BLK_DATA %011, 0,0,0, 00,00, 19,17
-	ds 8
+	ds 8, 0
 
-	db $03,$00,$00,$13,$11,$00,$00
+; unused
+	db $03, 00,00, 19,17, $00
+	db $00
 
 BlkPacket_Battle:
 	ATTR_BLK 5
@@ -66,34 +68,39 @@ BlkPacket_Battle:
 	ATTR_BLK_DATA %011, 2,2,0, 00,04, 08,11 ; player mon: pal 2
 	ATTR_BLK_DATA %011, 3,3,0, 11,00, 19,06 ; enemy mon : pal 3
 
-	db $03,$00,$00,$13,$0b,$00
-	db $03,$00,$0c,$13,$11,$02
-	db $03,$01,$00,$0a,$03,$01
-	db $03,$0a,$08,$13,$0a,$00
-	db $03,$00,$04,$08,$0b,$02
-	db $03,$0b,$00,$13,$07,$03
+; unused
+	db $03, 00,00, 19,11, $00
+	db $03, 00,12, 19,17, $02
+	db $03, 01,00, 10,03, $01
+	db $03, 10,08, 19,10, $00
+	db $03, 00,04, 08,11, $02
+	db $03, 11,00, 19,07, $03
 	db $00
 
 BlkPacket_StatusScreen:
 	ATTR_BLK 1
 	ATTR_BLK_DATA %111, 1,1,0, 01,00, 07,06 ; mon: pal 1, HP bar: pal 0
-	ds 8
+	ds 8, 0
 
-	db $02,$00,$00,$11,$00,$03
-	db $01,$00,$07,$06,$01,$03
-	db $01,$07,$13,$11,$00,$03
-	db $08,$00,$13,$06,$00,$00
+; unused
+	db $02, 00,00, 17,00
+	db $03, 01,00, 07,06, $01
+	db $03, 01,07, 19,17, $00
+	db $03, 08,00, 19,06, $00
+	db $00
 
 BlkPacket_Pokedex:
 	ATTR_BLK 1
 	ATTR_BLK_DATA %111, 1,1,0, 01,01, 08,08 ; mon: pal 1, everything else: pal 0
-	ds 8
+	ds 8, 0
 
-	db $02,$00,$00,$11,$00,$01
-	db $00,$01,$13,$00,$03,$01
-	db $01,$08,$08,$01,$03,$01
-	db $09,$08,$11,$00,$03,$09
-	db $01,$13,$11,$00,$00
+; unused
+	db $02, 00,00, 17,00
+	db $01, 00,01, 19,00
+	db $03, 01,01, 08,08, $01
+	db $03, 01,09, 08,17, $00
+	db $03, 09,01, 19,17, $00
+	db $00
 
 BlkPacket_Slots:
 	ATTR_BLK 5
@@ -103,11 +110,12 @@ BlkPacket_Slots:
 	ATTR_BLK_DATA %011, 0,0,0, 04,04, 15,09 ; slot reels: pal 0
 	ATTR_BLK_DATA %011, 0,0,0, 00,12, 19,17 ; message box: pal 0
 
-	db $03,$00,$00,$13,$0b,$01
-	db $03,$00,$04,$13,$09,$02
-	db $03,$00,$06,$13,$07,$03
-	db $03,$04,$04,$0f,$09,$00
-	db $03,$00,$0c,$13,$11,$00
+; unused
+	db $03, 00,00, 19,11, $01
+	db $03, 00,04, 19,09, $02
+	db $03, 00,06, 19,07, $03
+	db $03, 04,04, 15,09, $00
+	db $03, 00,12, 19,17, $00
 	db $00
 
 BlkPacket_Titlescreen:
@@ -115,11 +123,12 @@ BlkPacket_Titlescreen:
 	ATTR_BLK_DATA %011, 0,0,0, 00,00, 19,07 ; pokemon logo: pal 0
 	ATTR_BLK_DATA %010, 1,1,0, 00,08, 19,09 ; version text: pal 1
 	ATTR_BLK_DATA %011, 2,2,0, 00,10, 19,17 ; player, mon, copyright text: pal 2
-	ds 12
+	ds 12, 0
 
-	db $03,$00,$00,$13,$07,$00
-	db $03,$00,$08,$13,$09,$01
-	db $03,$00,$0a,$13,$11,$02
+; unused
+	db $03, 00,00, 19,07, $00
+	db $03, 00,08, 19,09, $01
+	db $03, 00,10, 19,17, $02
 	db $00
 
 BlkPacket_NidorinoIntro:
@@ -127,11 +136,12 @@ BlkPacket_NidorinoIntro:
 	ATTR_BLK_DATA %011, 1,1,0, 00,00, 19,03 ; upper black bar: pal 1
 	ATTR_BLK_DATA %011, 0,0,0, 00,04, 19,13 ; letterbox: pal 0
 	ATTR_BLK_DATA %011, 1,1,0, 00,14, 19,17 ; lower black bar: pal 1
-	ds 12
+	ds 12, 0
 
-	db $03,$00,$00,$13,$03,$01
-	db $03,$00,$04,$13,$0d,$00
-	db $03,$00,$0e,$13,$11,$01
+; unused
+	db $03, 00,00, 19,03, $01
+	db $03, 00,04, 19,13, $00
+	db $03, 00,14, 19,17, $01
 	db $00
 
 BlkPacket_PartyMenu:
@@ -143,18 +153,20 @@ BlkPacket_PartyMenu:
 	ATTR_BLK_DATA %010, 0,0,0, 05,07, 11,07 ; HP bar 3: pal set dynamically
 	ATTR_BLK_DATA %010, 0,0,0, 05,09, 11,09 ; HP bar 4: pal set dynamically
 	ATTR_BLK_DATA %010, 0,0,0, 05,11, 11,11 ; HP bar 5: pal set dynamically
-	ds 4
+	ds 4, 0
 
-	db $02,$00,$00,$11,$01,$03
-	db $01,$00,$02,$0c,$00,$03
-	db $01,$0d,$02,$11,$01,$03
-	db $03,$00,$13,$11,$01,$03
-	db $0c,$00,$12,$01,$00,$03
-	db $0c,$02,$12,$03,$00,$03
-	db $0c,$04,$12,$05,$00,$03
-	db $0c,$06,$12,$07,$00,$03
-	db $0c,$08,$12,$09,$00,$03
-	db $0c,$0a,$12,$0b,$00,$00
+; unused
+	db $02, 00,00, 17,01
+	db $03, 01,00, 02,12, $00
+	db $03, 01,13, 02,17, $01
+	db $03, 03,00, 19,17, $01
+	db $03, 12,00, 18,01, $00
+	db $03, 12,02, 18,03, $00
+	db $03, 12,04, 18,05, $00
+	db $03, 12,06, 18,07, $00
+	db $03, 12,08, 18,09, $00
+	db $03, 12,10, 18,11, $00
+	db $00
 
 BlkPacket_TrainerCard:
 	ATTR_BLK 10
@@ -168,18 +180,19 @@ BlkPacket_TrainerCard:
 	ATTR_BLK_DATA %010, 3,3,0, 07,15, 08,16 ; Marsh Badge
 	ATTR_BLK_DATA %010, 2,2,0, 11,15, 12,16 ; Volcano Badge
 	ATTR_BLK_DATA %010, 1,1,0, 15,15, 16,16 ; Earth Badge
-	ds 2
+	ds 2, 0
 
-	db $03,$03,$0c,$04,$0d,$00
-	db $03,$07,$0c,$08,$0d,$01
-	db $03,$0b,$0c,$0c,$0d,$03
-	db $03,$10,$0b,$11,$0c,$02
-	db $03,$0e,$0d,$0f,$0e,$01
-	db $03,$10,$0d,$11,$0e,$03
-	db $03,$03,$0f,$04,$10,$02
-	db $03,$07,$0f,$08,$10,$03
-	db $03,$0b,$0f,$0c,$10,$02
-	db $03,$0f,$0f,$10,$10,$01
+; unused
+	db $03, 03,12, 04,13, $00
+	db $03, 07,12, 08,13, $01
+	db $03, 11,12, 12,13, $03
+	db $03, 16,11, 17,12, $02
+	db $03, 14,13, 15,14, $01
+	db $03, 16,13, 17,14, $03
+	db $03, 03,15, 04,16, $02
+	db $03, 07,15, 08,16, $03
+	db $03, 11,15, 12,16, $02
+	db $03, 15,15, 16,16, $01
 	db $00
 
 BlkPacket_GameFreakIntro:
@@ -187,15 +200,16 @@ BlkPacket_GameFreakIntro:
 	ATTR_BLK_DATA %111, 1,1,0, 05,11, 07,13 ; falling stars (left): pal 1, GameFreak logo: pal 0
 	ATTR_BLK_DATA %010, 2,2,0, 08,11, 09,13 ; falling stars (middle): pal 2
 	ATTR_BLK_DATA %011, 3,3,0, 12,11, 14,13 ; falling stars (right): pal 3
-	ds 12
+	ds 12, 0
 
-	db $03,$00,$00,$13,$0a,$00
-	db $03,$00,$0b,$04,$0d,$00
-	db $03,$05,$0b,$07,$0d,$01
-	db $03,$08,$0b,$13,$0d,$00
-	db $03,$00,$0e,$13,$11,$00
-	db $03,$08,$0b,$09,$0d,$02
-	db $03,$0c,$0b,$0e,$0d,$03
+; unused
+	db $03, 00,00, 19,10, $00
+	db $03, 00,11, 04,13, $00
+	db $03, 05,11, 07,13, $01
+	db $03, 08,11, 19,13, $00
+	db $03, 00,14, 19,17, $00
+	db $03, 08,11, 09,13, $02
+	db $03, 12,11, 14,13, $03
 	db $00
 
 
@@ -229,7 +243,7 @@ DataSnd_72548: DATA_SND $85d, $0, 11
 	db  $8C                 ; cpx #$8c (2)
 	db  $D0, $F4            ; bne -$0c
 	db  $60                 ; rts
-	ds  7
+	ds  7, 0
 
 DataSnd_72558: DATA_SND $852, $0, 11
 	db  $A9, $E7            ; lda #$e7
