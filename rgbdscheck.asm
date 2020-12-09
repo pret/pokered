f16f53096b39ccb5b9ab2345faa3e49434e19336
Rangi <remy.oukaour+rangi42@gmail.com>
2020-12-09 15:11:06 -0500

    Update rgbdscheck.asm
    Hard-code the version string in the failure message so it won't be in hex

--- rgbdscheck.asm
+++ rgbdscheck.asm
@@ -1,12 +1,14 @@
-; pokered requires rgbds 0.4.1 or newer.
+; pokered requires rgbds 0.4.2 or newer.
 MAJOR EQU 0
 MINOR EQU 4
-PATCH EQU 1
+PATCH EQU 2
 
 IF !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
-	fail "pokered requires rgbds {MAJOR}.{MINOR}.{PATCH} or newer."
-ELIF (__RGBDS_MAJOR__ < MAJOR) || \
+	fail "pokered requires rgbds 0.4.2 or newer."
+ELSE
+IF (__RGBDS_MAJOR__ < MAJOR) || \
 	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ < MINOR) || \
 	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ == MINOR && __RGBDS_PATCH__ < PATCH)
-	fail "pokered requires rgbds {MAJOR}.{MINOR}.{PATCH} or newer."
+	fail "pokered requires rgbds 0.4.2 or newer."
+ENDC
 ENDC
