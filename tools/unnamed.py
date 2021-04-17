955e4eddf8fdfbb68dad3ada8712faf6fb44a417
Rangi <remy.oukaour+rangi42@gmail.com>
2021-04-17 19:16:12 -0400

    Actually use rgbds 0.5.0

--- tools/unnamed.py
+++ tools/unnamed.py
@@ -82,7 +82,7 @@ for objfile in objects:
     elif magic == b'RGB9':
         obj_ver = 10 + unpack_file("<I", f)[0]
 
-    if obj_ver not in [6, 10, 11, 12, 13, 15, 16]:
+    if obj_ver not in [6, 10, 11, 12, 13, 15, 16, 17]:
         print("Error: File '%s' is of an unknown format." % objfile, file=stderr)
         exit(1)
 
