27d0b146addee8185178388e0f7a94353d8409ba
SatoMew <SatoMew@users.noreply.github.com>
2022-04-01 04:49:08 +0100

    Add comment explaining the purpose of TrainerNamePointers (#352)

--- data/trainers/name_pointers.asm
+++ data/trainers/name_pointers.asm
@@ -1,5 +1,7 @@
 TrainerNamePointers:
-; what is the point of these?
+; These are only used for trainers' defeat speeches.
+; They were originally shortened variants of the trainer class names
+; in the Japanese versions, but are now redundant with TrainerNames.
 	dw .YoungsterName
 	dw .BugCatcherName
 	dw .LassName
