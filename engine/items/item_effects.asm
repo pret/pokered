b188e6a11de6b81890639b9c94f0d7f0625c5891
Rangi <35663410+Rangi42@users.noreply.github.com>
2022-06-17 22:24:23 -0400

    Apply suggestions from code review

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -225,7 +225,7 @@ ItemUseBall:
 	ld a, [wEnemyMonStatus]
 	and a
 	jr z, .skipAilmentValueSubtraction ; no ailments
-	and 1 << FRZ | SLP_MASK
+	and (1 << FRZ) | SLP_MASK
 	ld c, 12
 	jr z, .notFrozenOrAsleep
 	ld c, 25
@@ -380,7 +380,7 @@ ItemUseBall:
 	ld a, [wEnemyMonStatus]
 	and a
 	jr z, .skip5
-	and 1 << FRZ | SLP_MASK
+	and (1 << FRZ) | SLP_MASK
 	ld b, 5
 	jr z, .addAilmentValue
 	ld b, 10
@@ -1700,7 +1700,7 @@ ItemUsePokeflute:
 .inBattle
 	xor a
 	ld [wWereAnyMonsAsleep], a
-	ld b, ~SLP_MASK & $ff
+	ld b, ~SLP_MASK
 	ld hl, wPartyMon1Status
 	call WakeUpEntireParty
 	ld a, [wIsInBattle]
@@ -1752,7 +1752,7 @@ WakeUpEntireParty:
 .loop
 	ld a, [hl]
 	push af
-	and SLP_MASK ; is pokemon asleep?
+	and SLP_MASK
 	jr z, .notAsleep
 	ld a, 1
 	ld [wWereAnyMonsAsleep], a ; indicate that a pokemon had to be woken up
