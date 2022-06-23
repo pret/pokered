5de324fe33c1996b337b6284dcee0252f3544a3f
Vortyne <104168801+Vortyne@users.noreply.github.com>
2022-06-22 23:56:45 -0400

    Update TM prices to match current TMs in value

--- data/items/tm_prices.asm
+++ data/items/tm_prices.asm
@@ -2,53 +2,53 @@ TechnicalMachinePrices:
 ; In thousands (nybbles).
 	nybble_array TechnicalMachinePrices
 	nybble 3 ; TM01
-	nybble 2 ; TM02
+	nybble 3 ; TM02
 	nybble 2 ; TM03
-	nybble 1 ; TM04
+	nybble 3 ; TM04
 	nybble 3 ; TM05
-	nybble 4 ; TM06
-	nybble 2 ; TM07
-	nybble 4 ; TM08
-	nybble 3 ; TM09
-	nybble 4 ; TM10
+	nybble 1 ; TM06
+	nybble 5 ; TM07
+	nybble 2 ; TM08
+	nybble 5 ; TM09
+	nybble 5 ; TM10
 	nybble 2 ; TM11
-	nybble 1 ; TM12
-	nybble 4 ; TM13
-	nybble 5 ; TM14
-	nybble 5 ; TM15
-	nybble 5 ; TM16
-	nybble 3 ; TM17
-	nybble 2 ; TM18
-	nybble 3 ; TM19
+	nybble 2 ; TM12
+	nybble 5 ; TM13
+	nybble 8 ; TM14
+	nybble 8 ; TM15
+	nybble 3 ; TM16
+	nybble 8 ; TM17
+	nybble 3 ; TM18
+	nybble 2 ; TM19
 	nybble 2 ; TM20
 	nybble 5 ; TM21
-	nybble 5 ; TM22
-	nybble 5 ; TM23
-	nybble 2 ; TM24
-	nybble 5 ; TM25
-	nybble 4 ; TM26
+	nybble 8 ; TM22
+	nybble 4 ; TM23
+	nybble 5 ; TM24
+	nybble 8 ; TM25
+	nybble 5 ; TM26
 	nybble 5 ; TM27
-	nybble 2 ; TM28
-	nybble 4 ; TM29
-	nybble 1 ; TM30
-	nybble 2 ; TM31
-	nybble 1 ; TM32
+	nybble 4 ; TM28
+	nybble 5 ; TM29
+	nybble 4 ; TM30
+	nybble 1 ; TM31
+	nybble 6 ; TM32
 	nybble 1 ; TM33
 	nybble 2 ; TM34
 	nybble 4 ; TM35
-	nybble 2 ; TM36
-	nybble 2 ; TM37
+	nybble 3 ; TM36
+	nybble 8 ; TM37
 	nybble 5 ; TM38
-	nybble 2 ; TM39
-	nybble 4 ; TM40
-	nybble 2 ; TM41
-	nybble 2 ; TM42
-	nybble 5 ; TM43
-	nybble 2 ; TM44
+	nybble 4 ; TM39
+	nybble 3 ; TM40
+	nybble 5 ; TM41
+	nybble 3 ; TM42
+	nybble 8 ; TM43
+	nybble 1 ; TM44
 	nybble 2 ; TM45
-	nybble 4 ; TM46
-	nybble 3 ; TM47
-	nybble 4 ; TM48
-	nybble 4 ; TM49
-	nybble 2 ; TM50
+	nybble 2 ; TM46
+	nybble 5 ; TM47
+	nybble 5 ; TM48
+	nybble 3 ; TM49
+	nybble 6 ; TM50
 	end_nybble_array NUM_TMS
