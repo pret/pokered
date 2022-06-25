421b5e2c794c144db824b5fa381d5089639e7944
Vortyne <104168801+Vortyne@users.noreply.github.com>
2022-06-25 09:16:12 -0400

    more accurate item dupe behaviour

--- engine/pokemon/missingno_item_duplication.asm
+++ engine/pokemon/missingno_item_duplication.asm
@@ -2,8 +2,6 @@ MissingNoBattleStart::
 	ld a, [wItemDuplicationActive]
 	and a
 	ret z
-	ld hl, wBagItems + 11 ; sixth item in bag's ID
-	ld a, [hl]
-	add 128
-	ld [hl], a
+	ld hl, wBagItems + 11 ; sixth item in bag's quantity
+	set 7, [hl] ; adds 128 to the quantity, if you don't already have 128.
 	ret
