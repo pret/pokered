53e0cbf9f568f6e4efb32af5ce4f23fb7ae0e80e
MementoMartha <harleyrayquaza@gmail.com>
2022-08-31 16:43:33 +0100

    Learnsets, Sun Stones and Evos, oh my

--- data/items/use_party.asm
+++ data/items/use_party.asm
@@ -20,6 +20,7 @@ UsableItems_PartyMenu:
 	db CARBOS
 	db CALCIUM
 	db RARE_CANDY
+	db SUN_STONE
 	db LEAF_STONE
 	db FULL_HEAL
 	db REVIVE
--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -60,7 +60,7 @@ ItemUsePtrTable:
 	dw UnusableItem      ; DOME_FOSSIL
 	dw UnusableItem      ; HELIX_FOSSIL
 	dw UnusableItem      ; SECRET_KEY
-	dw UnusableItem
+	dw ItemUseEvoStone   ; SUN_STONE
 	dw UnusableItem      ; BIKE_VOUCHER
 	dw ItemUseXAccuracy  ; X_ACCURACY
 	dw ItemUseEvoStone   ; LEAF_STONE
