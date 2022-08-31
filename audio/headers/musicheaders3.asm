109eb664d5c62d5c8ba45612bb9303cccd8c8127
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2022-08-31 16:14:38 +0100

    add the headers..?
    this...should work

--- audio/headers/musicheaders3.asm
+++ audio/headers/musicheaders3.asm
@@ -114,3 +114,17 @@ Music_MeetMaleTrainer::
 	channel 1, Music_MeetMaleTrainer_Ch1
 	channel 2, Music_MeetMaleTrainer_Ch2
 	channel 3, Music_MeetMaleTrainer_Ch3
+
+; New Music
+Music_Digda01::
+	channel_count 3
+	channel 1, Music_Digda01_Ch1
+	channel 2, Music_Digda01_Ch2
+	channel 3, Music_Digda01_Ch3
+	
+Music_Field6::
+	channel_count 4
+	channel 1, Music_Field6_Ch1
+	channel 2, Music_Field6_Ch2
+	channel 3, Music_Field6_Ch3
+	channel 4, Music_Field6_Ch4
