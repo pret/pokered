445512b27e604edfee24165b3bfb9be2985555c3
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2022-08-31 16:10:36 +0100

    Add Field6 & Diglett's Cave
    Credit to zumiisawhat!

---
+++ audio/music/digda01.asm
@@ -0,0 +1,796 @@
+;
+;;	Digda01
+;
+;;	Converting on Fri Jul 28 15:35:04 1995
+;
+;;				by ver 1.02
+;
+
+
+
+
+
+
+;;----------------------------------------
+Digda01_Ch1:
+;;----------------------------------------
+
+	tempo 144
+	volume 7, 7
+    duty_cycle 1
+    toggle_perfect_pitch
+	note_type 12, 11, 3
+.loop
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+;; P1-1
+	octave 3
+	note D#, 2
+;; P1-2 --- tied
+	note D_, 2
+;; P1-3 --- tied
+	note D#, 2
+;; P1-4 --- tied
+	note F_, 2
+;; P1-5 --- tied
+	note F#, 2
+;; P1-6 --- tied
+	note F_, 2
+;; P1-7 --- tied
+	note F#, 2
+;; P1-8 --- tied
+	note G#, 2
+;; P1-9
+	note A_, 2
+;; P1-10 --- tied
+	note G#, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note F_, 2
+	note F#, 2
+	note G#, 2
+;; P1-11
+	note A_, 2
+	note G#, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note F_, 2
+	note F#, 2
+	note F_, 2
+;; P1-12
+	note A_, 2
+	note G#, 2
+	note A_, 2
+	note G#, 2
+	note F#, 2
+	note F_, 2
+	note D#, 2
+	note D_, 2
+;; P1-13 -
+	rest 16
+;; P1-14 -
+	rest 16
+;; P1-15 -
+	rest 16
+;; P1-16 -
+	rest 16
+;; P1-17 -
+	rest 16
+;; P1-18 -
+	rest 16
+;; P1-19
+	octave 4
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+;; P1-20
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	rest 2
+;; P1-21 --- tied
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+;; P1-22 --- tied
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+;; P1-23 --- tied
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+;; P1-24 --- tied
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+;; P1-25 --- tied
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note G_, 2
+	rest 2
+;; P1-26 --- tied
+	note G_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note G_, 2
+	rest 2
+;; P1-27 --- tied
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+;; P1-28 --- tied
+	note F#, 2
+	rest 2
+	note F#, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+;; P1-29 --- tied
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+;; P1-30 --- tied
+	note E_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note E_, 2
+	rest 2
+;; P1-31 --- tied
+	note E_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+;; P1-32 --- tied
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note F_, 2
+;; P1-33
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+;; P1-34
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+	note C#, 2
+;; P1-35 -
+	rest 16
+;; P1-36 -
+	rest 16
+
+	sound_loop 0, .loop
+
+;;----------------------------------------
+Digda01_Ch2:
+;;----------------------------------------
+    duty_cycle 1
+	note_type 12, 12, 3
+.loop
+;; P2-1 -
+	rest 16
+;; P2-2 -
+	rest 16
+;; P2-3 -
+	rest 16
+;; P2-4 -
+	rest 16
+;; P2-5
+	octave 3
+	note A#, 6
+	octave 4
+	note C_, 2
+	note C#, 6
+	note C_, 2
+;; P2-6
+	note C#, 6
+	note C_, 2
+	note C#, 6
+	note D#, 2
+;; P2-7
+	note E_, 6
+	note D#, 2
+	note E_, 6
+	note D#, 2
+;; P2-8
+	note C#, 6
+	note C_, 2
+	octave 3
+	note A#, 6
+	note G#, 2
+;; P2-9
+	note A#, 6
+	octave 4
+	note C_, 2
+	note C#, 6
+	note C_, 2
+;; P2-10
+	note C#, 6
+	note C_, 2
+	note C#, 6
+	note D#, 2
+;; P2-11
+	note E_, 6
+	note D#, 2
+	note E_, 6
+	note D#, 2
+;; P2-12
+	note C#, 6
+	note C_, 2
+	octave 3
+	note A#, 6
+	note G#, 2
+;; P2-13 -
+	rest 16
+;; P2-14
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note A_, 2
+;; P2-15 -
+	rest 16
+;; P2-16 -
+	rest 4
+	note C_, 2
+	note G#, 2
+	note F#, 2
+	note D_, 2
+	note E_, 2
+	note G#, 2
+;; P2-17 -
+	rest 16
+;; P2-18 -
+	rest 16
+;; P2-19 -
+	rest 4
+	octave 4
+	note_type 12, 12, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+;; P2-20
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	rest 2
+;; P2-21 --- tied
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+;; P2-22 --- tied
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+;; P2-23 --- tied
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+;; P2-24 --- tied
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+;; P2-25 --- tied
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	octave 5
+	note C_, 2
+	rest 2
+	note C_, 2
+	rest 2
+;; P2-26 --- tied
+	note C_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note C_, 2
+	rest 2
+;; P2-27 --- tied
+	octave 4
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+;; P2-28 --- tied
+	note B_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+;; P2-29 --- tied
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+;; P2-30 --- tied
+	note A_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note A_, 2
+	rest 2
+;; P2-31 --- tied
+	note A_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+;; P2-32 --- tied
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note A#, 2
+;; P2-33
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+;; P2-34
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 2
+;; P2-35 -
+	rest 16
+;; P2-36 -
+	rest 16
+
+	sound_loop 0, .loop
+
+;;----------------------------------------
+Digda01_Ch3:
+;;----------------------------------------
+
+;; P3-1
+	note_type 12, 1, 0
+.loop
+	octave 5
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-2
+	octave 4
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-3
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-4
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-5
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-6
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-7
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-8
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-9
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-10
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-11
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-12
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-13
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note A_, 2
+	rest 4
+;; P3-14
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note A_, 2
+	rest 4
+;; P3-15
+	note C_, 2
+	note G#, 2
+	note F#, 2
+	note D_, 2
+	note E_, 2
+	note G#, 2
+	rest 4
+;; P3-16
+	note C_, 2
+	note G#, 2
+	note F#, 2
+	note D_, 2
+	note E_, 2
+	note G#, 2
+	rest 4
+;; P3-17
+	octave 3
+	note B_, 2
+	octave 4
+	note G_, 2
+	note F_, 2
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	rest 4
+;; P3-18
+	octave 3
+	note B_, 2
+	octave 4
+	note G_, 2
+	note F_, 2
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	rest 4
+;; P3-19
+	octave 3
+	note B_, 2
+	octave 4
+	note G_, 2
+	note F_, 2
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	rest 4
+;; P3-20
+	octave 3
+	note B_, 2
+	octave 4
+	note G_, 2
+	note F_, 2
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	rest 4
+;; P3-21
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	note D_, 2
+	note A#, 2
+;; P3-22
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+;; P3-23
+	note F_, 2
+	note A_, 2
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note A_, 2
+;; P3-24
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	note D_, 2
+	note A#, 2
+;; P3-25
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	note D#, 2
+	note B_, 2
+	note A_, 2
+	note F_, 2
+;; P3-26
+	note G_, 2
+	note B_, 2
+	note D#, 2
+	note B_, 2
+	note A_, 2
+	note F_, 2
+	note G_, 2
+	note B_, 2
+;; P3-27
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	note D_, 2
+	note A#, 2
+;; P3-28
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+;; P3-29
+	note F_, 2
+	note A_, 2
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note A_, 2
+;; P3-30
+	note C_, 2
+	note G#, 2
+	note F#, 2
+	note D_, 2
+	note E_, 2
+	note G#, 2
+	note C_, 2
+	note G#, 2
+;; P3-31
+	note F#, 2
+	note D_, 2
+	note E_, 2
+	note G#, 2
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+;; P3-32
+	note F_, 2
+	note A_, 2
+	note C#, 2
+	note A_, 2
+	note G_, 2
+	note D#, 2
+	note F_, 2
+	note A_, 2
+;; P3-33
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-34
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-35
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+;; P3-36
+	note D_, 2
+	note A#, 2
+	note G#, 2
+	note E_, 2
+	note F#, 2
+	note A#, 2
+	rest 4
+
+	sound_loop 0, .loop
