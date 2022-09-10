e8cbdfbdc7fe9edc7a8af38c890ee4b43671bb13
Rangi <35663410+Rangi42@users.noreply.github.com>
2022-09-10 13:43:52 -0400

    Update data/events/trades.asm

--- data/events/trades.asm
+++ data/events/trades.asm
@@ -2,10 +2,10 @@ TradeMons:
 ; entries correspond to TRADE_FOR_* constants
 	table_width 3 + NAME_LENGTH, TradeMons
 	; give mon, get mon, dialog id, nickname
-	; The two instances of TRADE_DIALOGSET_EVOLUTION
-	; are a leftover from the Japanese Blue trades.
-	; To match Red and Green, replace both
-	; with TRADE_DIALOGSET_CASUAL.
+	; The two instances of TRADE_DIALOGSET_EVOLUTION are a leftover
+	; from the Japanese Blue trades, which used species that evolve.
+	; Japanese Red and Green used TRADE_DIALOGSET_CASUAL, and had
+	; the same species as English Red and Blue.
 	db NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "TERRY@@@@@@"
 	db ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@"
 	db BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU@" ; unused
