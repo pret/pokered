e617239beecdd57fc533dd3806634347b0f231a3
Vortyne <104168801+Vortyne@users.noreply.github.com>
2022-10-11 19:19:18 -0400

    New release 2.1.0

--- version_number.asm
+++ version_number.asm
@@ -1,10 +1,10 @@
 VersionText:
 IF DEF(_RED)
-	db " PureRed v2.0.9@"
+	db " PureRed v2.1.0@"
 ENDC
 IF DEF(_BLUE)
-	db " PureBlue v2.0.9@"
+	db " PureBlue v2.1.0@"
 ENDC
 IF DEF(_GREEN)
-	db " PureGreen v2.0.9@"
+	db " PureGreen v2.1.0@"
 ENDC
