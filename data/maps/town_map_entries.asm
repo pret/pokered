e79781a4098ad061454b8ec564f7fb03db80db64
Vortyne <104168801+Vortyne@users.noreply.github.com>
2022-10-13 17:29:19 -0400

    Improve ordering of town map
    and improve the location of a couple of areas

--- data/maps/town_map_entries.asm
+++ data/maps/town_map_entries.asm
@@ -24,13 +24,13 @@ ExternalMapEntries:
 	external_map  4,  3, Route3Name
 	external_map  8,  2, Route4Name
 	external_map 10,  3, Route5Name
-	external_map 10,  8, Route6Name
+	external_map 10,  7, Route6Name
 	external_map  8,  5, Route7Name
-	external_map 13,  5, Route8Name
+	external_map 12,  5, Route8Name
 	external_map 13,  2, Route9Name
 	external_map 14,  4, Route10Name
 	external_map 12,  9, Route11Name
-	external_map 14,  9, Route12Name
+	external_map 14,  8, Route12Name
 	external_map 13, 11, Route13Name
 	external_map 11, 12, Route14Name
 	external_map 10, 13, Route15Name
@@ -58,7 +58,9 @@ InternalMapEntries:
 	; maximum map id subject to this rule, x, y, name
 	internal_map OAKS_LAB,                       2, 11, PalletTownName
 	internal_map VIRIDIAN_GYM,                   2,  8, ViridianCityName
-	internal_map VIRIDIAN_FOREST_SOUTH_GATE,     2,  6, Route2Name
+	internal_map DIGLETTS_CAVE_ROUTE_2,          3,  4, DiglettsCaveName
+	internal_map VIRIDIAN_FOREST_NORTH_GATE,     2,  4, Route2Name
+	internal_map VIRIDIAN_FOREST_SOUTH_GATE,     2,  5, Route2Name
 	internal_map VIRIDIAN_FOREST,                2,  4, ViridianForestName
 	internal_map PEWTER_POKECENTER,              2,  3, PewterCityName
 	internal_map MT_MOON_B2F,                    6,  2, MountMoonName
@@ -69,8 +71,11 @@ InternalMapEntries:
 	internal_map UNDERGROUND_PATH_ROUTE_6_COPY, 10,  6, Route6Name
 	internal_map UNDERGROUND_PATH_ROUTE_7_COPY,  9,  5, Route7Name
 	internal_map UNDERGROUND_PATH_ROUTE_8,      11,  5, Route8Name
+	internal_map ROCK_TUNNEL_POKECENTER,        14,  3, Route10Name
 	internal_map ROCK_TUNNEL_1F,                14,  3, RockTunnelName
-	internal_map POWER_PLANT,                   15,  4, PowerPlantName
+	internal_map POWER_PLANT,                   15,  3, PowerPlantName
+	internal_map ROUTE_11_GATE_1F,              13,  9, Route11Name
+	internal_map DIGLETTS_CAVE_ROUTE_11,        11,  8, DiglettsCaveName
 	internal_map ROUTE_11_GATE_2F,              13,  9, Route11Name
 	internal_map ROUTE_12_GATE_1F,              14,  7, Route12Name
 	internal_map BILLS_HOUSE,                   12,  0, SeaCottageName
@@ -84,7 +89,7 @@ InternalMapEntries:
 	internal_map UNDERGROUND_PATH_WEST_EAST,    10,  5, UndergroundPathName
 	internal_map CELADON_HOTEL,                  7,  5, CeladonCityName
 	internal_map LAVENDER_POKECENTER,           14,  5, LavenderTownName
-	internal_map POKEMON_TOWER_7F,              15,  5, PokemonTowerName
+	internal_map POKEMON_TOWER_7F,              15,  4, PokemonTowerName
 	internal_map LAVENDER_CUBONE_HOUSE,         14,  5, LavenderTownName
 	internal_map WARDENS_HOUSE,                  8, 13, FuchsiaCityName
 	internal_map SAFARI_ZONE_GATE,               8, 12, SafariZoneName
@@ -105,7 +110,7 @@ InternalMapEntries:
 	internal_map VICTORY_ROAD_2F,                0,  4, VictoryRoadName
 	internal_map ROUTE_12_GATE_2F,              14,  7, Route12Name
 	internal_map VERMILION_TRADE_HOUSE,         10,  9, VermilionCityName
-	internal_map DIGLETTS_CAVE,                  3,  4, DiglettsCaveName
+	internal_map DIGLETTS_CAVE,                 11,  8, DiglettsCaveName
 	internal_map VICTORY_ROAD_3F,                0,  4, VictoryRoadName
 	internal_map ROCKET_HIDEOUT_ELEVATOR,        7,  5, RocketHQName
 	internal_map FUCHSIA_TREE_DELETER_HOUSE,     8, 13, FuchsiaCityName
