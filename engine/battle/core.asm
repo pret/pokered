171b95a6977326c6f1a2e3096c9aeb02aff4c53e
Vortyne <104168801+Vortyne@users.noreply.github.com>
2022-10-15 21:44:28 -0400

    Fix this party menu visual glitch for a third time
    should cover all ways this glitch can happen visually now

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -7686,8 +7686,8 @@ SetAISentOut:
 
 ; PureRGBnote: FIXED: function to redraw the party menu without any bottom box text, used to avoid minor graphical glitches
 EmptyPartyMenuRedraw:
-	hlcoord 13, 11
-	lb bc, 1, 7
+	hlcoord 11, 11
+	lb bc, 1, 9
 	call ClearScreenArea
 	ld a, EMPTY_PARTY_MENU ; new party menu text option - it just displays no text in the bottom box since we'll be writing there immediately
 	ld [wPartyMenuTypeOrMessageID], a
