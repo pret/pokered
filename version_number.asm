f7df2d27af5441267c1983a8d134d877a82aca5b
Vortyne <104168801+Vortyne@users.noreply.github.com>
2022-10-26 05:03:50 -0400

    Build new version 2.2.1

--- version_number.asm
+++ version_number.asm
@@ -1,10 +1,10 @@
 VersionText:
 IF DEF(_RED)
-	db " PureRed v2.2.0@"
+	db " PureRed v2.2.1@"
 ENDC
 IF DEF(_BLUE)
-	db " PureBlue v2.2.0@"
+	db " PureBlue v2.2.1@"
 ENDC
 IF DEF(_GREEN)
-	db " PureGreen v2.2.0@"
+	db " PureGreen v2.2.1@"
 ENDC
