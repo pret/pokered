cff3e7413d492aa95b5e3e8736c837d72a2875ac
Vortyne <104168801+Vortyne@users.noreply.github.com>
2022-11-22 22:53:51 -0500

    New bugfix release 2.3.1
    fixes menu selection bug in party menu I caused with the last couple of commits

--- version_number.asm
+++ version_number.asm
@@ -1,10 +1,10 @@
 VersionText:
 IF DEF(_RED)
-	db " PureRed v2.3.0@"
+	db " PureRed v2.3.1@"
 ENDC
 IF DEF(_BLUE)
-	db " PureBlue v2.3.0@"
+	db " PureBlue v2.3.1@"
 ENDC
 IF DEF(_GREEN)
-	db " PureGreen v2.3.0@"
+	db " PureGreen v2.3.1@"
 ENDC
