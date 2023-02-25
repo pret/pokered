7dd2f30645eebcee09470458fff95ef4d8952617
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-02-25 03:00:07 -0500

    add surge and erika + Tweak gym leader OG+ sprites

--- data/maps/objects/PokemonFanClub.asm
+++ data/maps/objects/PokemonFanClub.asm
@@ -16,5 +16,11 @@ PokemonFanClub_Object:
 	object_event  1,  4, SPRITE_SEEL, STAY, RIGHT, 4 ; person
 	object_event  3,  1, SPRITE_GENTLEMAN, STAY, DOWN, 5 ; person
 	object_event  5,  1, SPRITE_LINK_RECEPTIONIST, STAY, DOWN, 6 ; person
+	;object_event  6,  3, SPRITE_BROCK, STAY, LEFT, 1 ; person
+	;object_event  1,  3, SPRITE_MISTY, STAY, RIGHT, 2 ; person
+	;object_event  6,  4, SPRITE_LT_SURGE, STAY, LEFT, 3 ; person
+	;object_event  1,  4, SPRITE_ERIKA, STAY, RIGHT, 4 ; person
+	;object_event  3,  1, SPRITE_SABRINA, STAY, DOWN, 5 ; person
+	;object_event  5,  1, SPRITE_BLAINE, STAY, DOWN, 6 ; person
 
 	def_warps_to POKEMON_FAN_CLUB
--- data/maps/objects/SilphCo10F.asm
+++ data/maps/objects/SilphCo10F.asm
@@ -14,7 +14,7 @@ SilphCo10F_Object:
 	def_object_events
 	object_event  1,  9, SPRITE_ROCKET, STAY, RIGHT, 1, OPP_ROCKET, 39
 	object_event 10,  2, SPRITE_SCIENTIST, STAY, LEFT, 2, OPP_SCIENTIST, 11
-	object_event  9, 15, SPRITE_ERIKA, WALK, ANY_DIR, 3 ; person
+	object_event  9, 15, SPRITE_SILPH_WORKER_WOMAN, WALK, ANY_DIR, 3 ; person
 	object_event  2, 12, SPRITE_POKE_BALL, STAY, NONE, 4, TM_SILPH_CO_10F_BOXES_LEFT
 	object_event  4, 14, SPRITE_POKE_BALL, STAY, NONE, 5, ITEM_SILPH_CO_10F_BOXES_BOTTOM
 	object_event  5, 11, SPRITE_POKE_BALL, STAY, NONE, 6, ITEM_SILPH_CO_10F_BOXES_RIGHT
