8f809124d0bcc1ac7a588a8298b17150df1f88ee
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2023-03-06 12:23:45 +0000

    Concrete Bill's Garden wild data
    This tweaks the wild data so Aerodactyl, Omanyte, and Kabuto are in the water. Also diversified the Squirtles and added slots for the Hisui formes to use once they're in.

--- data/wild/maps/BillsGarden.asm
+++ data/wild/maps/BillsGarden.asm
@@ -1,26 +1,31 @@
-BillsGardenWildMons: ; wild mons are definitely edit-able
+; Bill's Garden is being built based on the original playground rumours.
+; Essentially, most people would claim that it was filled with all those one-time deals.
+; So here, we add those: Eevee, Starters, Hitmons, and Fossils, plus Totartle.
+; Only difference is Hisuian Formes, which otherwise are basically impossible to fit.
+; Legendary Birds were also in the mythos but there's no space and it sounds terrible.
+BillsGardenWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 46, EEVEE
+	db 46, EEVEE ; People are here for Eevee so just give them the ~40% chance...
 	db 46, EEVEE
 	db 46, CHARMANDER
 	db 49, BULBASAUR
 	db 49, HITMONTOP
 	db 52, HITMONCHAN
 	db 49, HITMONLEE
-	db 52, KABUTO
-	db 53, OMANYTE
-	db 53, AERODACTYL
+	db 52, EEVEE ; Hisuian Electrode
+	db 53, EEVEE ; Hisuian Arcanine
+	db 53, DECILLA
 	end_grass_wildmons
 
 	def_water_wildmons 10 ; encounter rate
 	db 40, SQUIRTLE
 	db 40, SQUIRTLE
 	db 40, SQUIRTLE
-	db 40, SQUIRTLE
-	db 40, SQUIRTLE
-	db 40, SQUIRTLE
-	db 40, SQUIRTLE
-	db 40, BLASTOISE
+	db 40, KABUTO
+	db 40, OMANYTE
+	db 40, AERODACTYL ; In games like DPP, you can encounter fliers on Water. Most notably, Zubat. No space on land so this is the best we got.
+	db 40, WARTORTLE
+	db 40, WARTORTLE
 	db 40, BLASTOISE
 	db 40, TOTARTLE
 	end_water_wildmons
