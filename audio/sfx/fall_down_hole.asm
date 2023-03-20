a2ca991cc60339250a38d1a13dde4a69ca2d356a
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-03-20 06:21:22 -0400

    Slight volume adjustment on this sound

--- audio/sfx/fall_down_hole.asm
+++ audio/sfx/fall_down_hole.asm
@@ -3,8 +3,8 @@
 FallDownHole::
 	duty_cycle 2
 	pitch_sweep 1, -7
-	square_note 15, 15, 0, 1900
-	square_note 15, 15, 0, 1500
+	square_note 15, 13, 0, 1900
+	square_note 15, 11, 0, 1500
 	pitch_sweep 0, 8
 	square_note 0, 0, 0, 0
 	sound_ret
