b0d6f3ee865061fb8c64b67eef51429191fb6678
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-03-28 01:33:44 -0400

    Tweak mewtwo learnset
    Fix its moveset on encountering it

--- data/pokemon/base_stats/mewtwo.asm
+++ data/pokemon/base_stats/mewtwo.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/mewtwo.pic", 0, 1 ; sprite dimensions
 	dw MewtwoPicFront, MewtwoPicBackSW
 
-	db CONFUSION, DISABLE, SWIFT, PSYCHIC_M ; level 1 learnset
+	db CONFUSION, DISABLE, SWIFT, NO_MOVE ; level 1 learnset
 	db GROWTH_SLOW ; growth rate
 
 	; tm/hm learnset
--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -2032,9 +2032,9 @@ MewtwoEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 63, BARRIER
 	db 66, PSYCHIC_M
 	db 70, RECOVER
+	db 72, BARRIER
 	db 75, MIST
 	db 81, AMNESIA
 	db 0
