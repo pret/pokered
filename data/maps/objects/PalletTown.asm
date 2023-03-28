7a8a4ef0e18e961831daa570558ce06d1f68b4b2
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-03-28 00:11:26 -0400

    Remove a test edit

--- data/maps/objects/PalletTown.asm
+++ data/maps/objects/PalletTown.asm
@@ -2,7 +2,7 @@ PalletTown_Object:
 	db $b ; border block
 
 	def_warp_events
-	warp_event  5,  5, SECRET_LAB, 1 ; TODO: REMOVE
+	warp_event  5,  5, REDS_HOUSE_1F, 1
 	warp_event 13,  5, BLUES_HOUSE, 1
 	warp_event 12, 11, OAKS_LAB, 2
 
