3d2e8a719fe4ce32036168f177e563464283af81
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2023-04-18 07:49:03 +0100

    citrine water wild data

--- data/wild/maps/CitrineCity.asm
+++ data/wild/maps/CitrineCity.asm
@@ -12,5 +12,15 @@ CitrineWildMons:
 	db 55, EXEGGUTOR
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 10 ; encounter rate
+	db 49, SEADRA
+	db 49, SEAKING
+	db 52, PENDRAKEN
+	db 50, SEADRA
+	db 50, SEAKING
+	db 53, KINGDRA
+	db 50, GOLDUCK
+	db 51, GOLDUCK
+	db 52, GYARADOS
+	db 53, GYARADOS
 	end_water_wildmons
