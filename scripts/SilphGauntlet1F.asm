993861722973dc3bf26102f6f377d13b6de50bef
MementoMartha <harleyrayquaza@gmail.com>
2023-04-25 15:57:33 +0100

    Finally fixing the Gauntlet 1F Trainers
    Adding trainers properly is hell, 1/5, would not recommend

--- scripts/SilphGauntlet1F.asm
+++ scripts/SilphGauntlet1F.asm
@@ -21,52 +21,24 @@ SilphGauntlet1F_TextPointers:
 	dw SilphGauntlet1FText6
 	
 SilphGauntlet1FTrainerHeaders:
-	def_trainers 6
+	def_trainers
 SilphGauntlet1FTrainerHeader0:
-	trainer EVENT_BEAT_GAUNTLET_YOUNGSTER, 3, SilphGauntlet1FBattleText1, SilphGauntlet1FEndBattleText1, SilphGauntlet1FAfterBattleText1
+	trainer EVENT_BEAT_GAUNTLET_YOUNGSTER, 1, SilphGauntlet1FBattleText1, SilphGauntlet1FEndBattleText1, SilphGauntlet1FAfterBattleText1
 SilphGauntlet1FTrainerHeader1:
-	trainer EVENT_BEAT_GAUNTLET_BUG_CATCHER, 2, SilphGauntlet1FBattleText2, SilphGauntlet1FEndBattleText2, SilphGauntlet1FAfterBattleText2
+	trainer EVENT_BEAT_GAUNTLET_BUG_CATCHER, 1, SilphGauntlet1FBattleText2, SilphGauntlet1FEndBattleText2, SilphGauntlet1FAfterBattleText2
 SilphGauntlet1FTrainerHeader2:
-	trainer EVENT_BEAT_GAUNTLET_STUDENT, 4, SilphGauntlet1FBattleText3, SilphGauntlet1FEndBattleText3, SilphGauntlet1FAfterBattleText3
+	trainer EVENT_BEAT_GAUNTLET_STUDENT, 1, SilphGauntlet1FBattleText3, SilphGauntlet1FEndBattleText3, SilphGauntlet1FAfterBattleText3
 SilphGauntlet1FTrainerHeader3:
-	trainer EVENT_BEAT_GAUNTLET_JR_TRAINER_M, 2, SilphGauntlet1FBattleText4, SilphGauntlet1FEndBattleText4, SilphGauntlet1FAfterBattleText4
+	trainer EVENT_BEAT_GAUNTLET_JR_TRAINER_M, 1, SilphGauntlet1FBattleText4, SilphGauntlet1FEndBattleText4, SilphGauntlet1FAfterBattleText4
 SilphGauntlet1FTrainerHeader4:
-	trainer EVENT_BEAT_GAUNTLET_JR_TRAINER_F, 2, SilphGauntlet1FBattleText5, SilphGauntlet1FEndBattleText5, SilphGauntlet1FAfterBattleText5
+	trainer EVENT_BEAT_GAUNTLET_JR_TRAINER_F, 1, SilphGauntlet1FBattleText5, SilphGauntlet1FEndBattleText5, SilphGauntlet1FAfterBattleText5
 SilphGauntlet1FTrainerHeader5:
-	trainer EVENT_BEAT_GAUNTLET_BIRD_KEEPER, 3, SilphGauntlet1FBattleText6, SilphGauntlet1FEndBattleText6, SilphGauntlet1FAfterBattleText6
+	trainer EVENT_BEAT_GAUNTLET_BIRD_KEEPER, 1, SilphGauntlet1FBattleText6, SilphGauntlet1FEndBattleText6, SilphGauntlet1FAfterBattleText6
 	db -1 ; end
 	
 SilphGauntlet1FText1:
 	text_asm
 	ld hl, SilphGauntlet1FTrainerHeader0
-	jr SilphGauntlet1FTalkToTrainer
-
-SilphGauntlet1FText2:
-	text_asm
-	ld hl, SilphGauntlet1FTrainerHeader1
-	jr SilphGauntlet1FTalkToTrainer
-
-SilphGauntlet1FText3:
-	text_asm
-	ld hl, SilphGauntlet1FTrainerHeader2
-	jr SilphGauntlet1FTalkToTrainer
-
-SilphGauntlet1FText4:
-	text_asm
-	ld hl, SilphGauntlet1FTrainerHeader3
-	jr SilphGauntlet1FTalkToTrainer
-
-SilphGauntlet1FText5:
-	text_asm
-	ld hl, SilphGauntlet1FTrainerHeader4
-	jr SilphGauntlet1FTalkToTrainer
-
-SilphGauntlet1FText6:
-	text_asm
-	ld hl, SilphGauntlet1FTrainerHeader5
-	jr SilphGauntlet1FTalkToTrainer
-	
-SilphGauntlet1FTalkToTrainer:
 	call TalkToTrainer
 	jp TextScriptEnd
 
@@ -82,6 +54,12 @@ SilphGauntlet1FAfterBattleText1:
 	text_far _SilphGauntlet1FAfterBattleText1
 	text_end
 
+SilphGauntlet1FText2:
+	text_asm
+	ld hl, SilphGauntlet1FTrainerHeader1
+	call TalkToTrainer
+	jp TextScriptEnd
+
 SilphGauntlet1FBattleText2:
 	text_far _SilphGauntlet1FBattleText2
 	text_end
@@ -93,6 +71,12 @@ SilphGauntlet1FEndBattleText2:
 SilphGauntlet1FAfterBattleText2:
 	text_far _SilphGauntlet1FAfterBattleText2
 	text_end
+	
+SilphGauntlet1FText3:
+	text_asm
+	ld hl, SilphGauntlet1FTrainerHeader2
+	call TalkToTrainer
+	jp TextScriptEnd
 
 SilphGauntlet1FBattleText3:
 	text_far _SilphGauntlet1FBattleText3
@@ -106,6 +90,12 @@ SilphGauntlet1FAfterBattleText3:
 	text_far _SilphGauntlet1FAfterBattleText3
 	text_end
 
+SilphGauntlet1FText4:
+	text_asm
+	ld hl, SilphGauntlet1FTrainerHeader3
+	call TalkToTrainer
+	jp TextScriptEnd
+
 SilphGauntlet1FBattleText4:
 	text_far _SilphGauntlet1FBattleText4
 	text_end
@@ -117,6 +107,12 @@ SilphGauntlet1FEndBattleText4:
 SilphGauntlet1FAfterBattleText4:
 	text_far _SilphGauntlet1FAfterBattleText4
 	text_end
+	
+SilphGauntlet1FText5:
+	text_asm
+	ld hl, SilphGauntlet1FTrainerHeader4
+	call TalkToTrainer
+	jp TextScriptEnd
 
 SilphGauntlet1FBattleText5:
 	text_far _SilphGauntlet1FBattleText5
@@ -129,6 +125,12 @@ SilphGauntlet1FEndBattleText5:
 SilphGauntlet1FAfterBattleText5:
 	text_far _SilphGauntlet1FAfterBattleText5
 	text_end
+
+SilphGauntlet1FText6:
+	text_asm
+	ld hl, SilphGauntlet1FTrainerHeader5
+	call TalkToTrainer
+	jp TextScriptEnd
 
 SilphGauntlet1FBattleText6:
 	text_far _SilphGauntlet1FBattleText6
