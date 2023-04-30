4da1a1235d7bafec8eaf1ca9dc96f6d30896ceac
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2023-04-30 18:38:59 +0100

    Prototype wild data for Garnet Cavern
    It should contain a mix of Rock, Steel, Ground, and Flying-type Pokemon. Feels like a cave, but has fliers to reference Galarian Articuno. Could change some stuff out for more Psychic-types given Galarian Articuno lives there. Needs more level distribution - namely, buffing near the end a la other caves.

--- data/wild/maps/GarnetCavern1F.asm
+++ data/wild/maps/GarnetCavern1F.asm
@@ -1,26 +1,27 @@
+; Garnet Cavern will contain a mix of Rock, Ground, Steel, and Flying-type Pokemon.
 GarnetCavern1FWildMons:
 	def_grass_wildmons 5 ; encounter rate
-	db 46, GOLBAT
-	db 46, HYPNO
-	db 46, MAGNETON
-	db 49, DODRIO
-	db 49, VENOMOTH
-	db 52, ARBOK
-	db 49, KADABRA
-	db 52, PARASECT
-	db 53, RAICHU
-	db 53, CLEFAIRY
+	db 62, RHYDON
+	db 62, GRAVELER
+	db 63, CROBAT
+	db 61, DODRIO
+	db 60, VENOMOTH
+	db 65, MADAAMU
+	db 64, FEAROW
+	db 65, GUARDIA
+	db 63, TRAMPEL
+	db 66, CROCKY
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
+	db 62, BAWLIGUA
+	db 62, TENTACRUEL
+	db 63, BAWLIGUA
+	db 61, TENTACRUEL
+	db 63, SLOWBRO
+	db 63, KINGLER
+	db 64, LAPRAS ; reference to the ice cavern on the sevii islands
+	db 64, GYARADOS
+	db 65, JAGG
+	db 66, CRYITHAN
 	end_water_wildmons
--- data/wild/maps/GarnetCavernB2F.asm
+++ data/wild/maps/GarnetCavernB2F.asm
@@ -1,26 +1,27 @@
+; Garnet Cavern will contain a mix of Rock, Ground, Steel, and Flying-type Pokemon.
 GarnetCavernB2FWildMons:
 	def_grass_wildmons 5 ; encounter rate
-	db 46, GOLBAT
-	db 46, HYPNO
-	db 46, MAGNETON
-	db 49, DODRIO
-	db 49, VENOMOTH
-	db 52, ARBOK
-	db 49, KADABRA
-	db 52, PARASECT
-	db 53, RAICHU
-	db 53, CLEFAIRY
+	db 62, RHYDON
+	db 62, GRAVELER
+	db 63, CROBAT
+	db 61, DODRIO
+	db 60, VENOMOTH
+	db 65, MADAAMU
+	db 64, FEAROW
+	db 65, GUARDIA
+	db 63, TRAMPEL
+	db 66, CROCKY
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
-	db 60, SKIMPER
+	db 60, BAWLIGUA
+	db 60, TENTACRUEL
+	db 60, BAWLIGUA
+	db 60, TENTACRUEL
+	db 60, SLOWBRO
+	db 60, KINGLER
+	db 60, LAPRAS ; reference to the ice cavern on the sevii islands
+	db 60, GYARADOS
+	db 60, JAGG
+	db 60, CRYITHAN
 	end_water_wildmons
