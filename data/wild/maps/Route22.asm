3f2a0659929d40a163b67ed4a382f171d0b63a1b
Martha Schilling <112545117+MementoMartha@users.noreply.github.com>
2023-06-05 10:53:14 +0100

    Update Route22.asm
    Removing Mikon to make the spawns less cluttered, and making NidoF and Mankey more common.

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,15 +1,15 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  5, RATTATA
+	db  3, RATTATA
 	db  4, NIDORAN_M
+	db  4, NIDORAN_F
+	db  4, RATTATA
+	db  4, MANKEY
+	db  3, SPEAROW
 	db  4, SPEAROW
 	db  4, NIDORAN_F
-	db  3, RATTATA
 	db  4, MANKEY
-	db  4, MIKON
-	db  5, MIKON
-	db  5, NIDORAN_F
-	db  5, NIDORAN_M
+	db  5, MANKEY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
