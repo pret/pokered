647dd73e6264e64513974164e41a510d58454551
vulcandth <vulcandth@gmail.com>
2023-06-12 20:39:30 +0000

    EXPORT SCRIPT_SAFARIZONEGATE_LEAVING_SAFARI

--- scripts/SafariZoneGate.asm
+++ scripts/SafariZoneGate.asm
@@ -13,6 +13,7 @@ SafariZoneGate_ScriptPointers:
 	dw_const SafariZoneGatePlayerMovingDownScript,       SCRIPT_SAFARIZONEGATE_PLAYER_MOVING_DOWN
 	dw_const SafariZoneGateLeavingSafariScript,          SCRIPT_SAFARIZONEGATE_LEAVING_SAFARI
 	dw_const SafariZoneGateSetScriptAfterMoveScript,     SCRIPT_SAFARIZONEGATE_SET_SCRIPT_AFTER_MOVE
+	EXPORT SCRIPT_SAFARIZONEGATE_LEAVING_SAFARI
 
 SafariZoneGateDefaultScript:
 	ld hl, .PlayerNextToSafariZoneWorker1CoordsArray
