0640f66066f89693697bff7331412c669807e503
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-07-04 19:09:09 -0400

    Seismic toss sonic boom shouldn't do damage calcs
    I accidentally removed their effect from SetDamageEffects

--- data/battle/set_damage_effects.asm
+++ data/battle/set_damage_effects.asm
@@ -1,5 +1,6 @@
 SetDamageEffects:
 ; moves that do damage but not through normal calculations
-; e.g., Super Fang
+; e.g., Super Fang, Sonicboom, Seismic Toss
 	db SUPER_FANG_EFFECT
+	db SPECIAL_DAMAGE_EFFECT
 	db -1 ; end
