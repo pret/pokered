eb96a0216ca2a3763e52b48a7b1a04b417175a63
SatoMew <SatoMew@users.noreply.github.com>
2023-07-16 01:08:05 +0100

    Remove emulator bug comment

--- engine/menus/naming_screen.asm
+++ engine/menus/naming_screen.asm
@@ -327,8 +327,6 @@ LoadEDTile:
 	ld de, ED_Tile
 	ld hl, vFont tile $70
 	ld bc, (ED_TileEnd - ED_Tile) / $8
-	; to fix the graphical bug on poor emulators
-	;lb bc, BANK(ED_Tile), (ED_TileEnd - ED_Tile) / $8
 	jp CopyVideoDataDouble
 
 ED_Tile:
