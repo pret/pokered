09ba282cdd58859a7ce974a4df8a7dbdd5ba184b
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-10-18 07:27:55 -0400

    Fix text entry

--- text/SafariZoneCenter.asm
+++ text/SafariZoneCenter.asm
@@ -82,7 +82,7 @@ _SafariZoneCenterJugglerText::
 _SafariZoneCenterJugglerEndBattleText::
 	text "Just"
 	line "call me"
-	cont "“The Matador”!"
+	cont "-The Matador-!"
 	prompt
 
 _SafariZoneCenterJugglerAfterBattleText::
