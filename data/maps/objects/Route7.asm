89c8e3f06138b10292c74765d86d9b77f7fc893c
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-10-18 17:39:14 -0400

    remove pointless warp events on route 7
    they are blocked by unwalkable tiles anyway

--- data/maps/objects/Route7.asm
+++ data/maps/objects/Route7.asm
@@ -6,9 +6,7 @@ Route7_Object:
 	db $f ; border block
 
 	def_warp_events
-	warp_event 18,  9, ROUTE_7_GATE, 3
 	warp_event 18, 10, ROUTE_7_GATE, 4
-	warp_event 11,  9, ROUTE_7_GATE, 1
 	warp_event 11, 10, ROUTE_7_GATE, 2
 	warp_event  5, 13, UNDERGROUND_PATH_ROUTE_7, 1
 
--- data/maps/objects/Route7Gate.asm
+++ data/maps/objects/Route7Gate.asm
@@ -5,10 +5,10 @@ Route7Gate_Object:
 	db $a ; border block
 
 	def_warp_events
-	warp_event  0,  3, LAST_MAP, 4
-	warp_event  0,  4, LAST_MAP, 4
+	warp_event  0,  3, LAST_MAP, 2
+	warp_event  0,  4, LAST_MAP, 2
 	warp_event  5,  3, LAST_MAP, 1
-	warp_event  5,  4, LAST_MAP, 2
+	warp_event  5,  4, LAST_MAP, 1
 
 	def_bg_events
 
