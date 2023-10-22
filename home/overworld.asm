3e12dfd00141652399a4dd32c086a980330833b9
Alchav <59858495+Alchav@users.noreply.github.com>
2023-10-21 21:42:13 -0400

    Remove incorrect comment

--- home/overworld.asm
+++ home/overworld.asm
@@ -104,7 +104,7 @@ OverworldLoopLessDelay::
 	bit 0, a
 	jr nz, .checkForOpponent
 	lda_coord 8, 9
-	ld [wTilePlayerStandingOn], a ; unused?
+	ld [wTilePlayerStandingOn]
 	call DisplayTextID ; display either the start menu or the NPC/sign text
 	ld a, [wEnteringCableClub]
 	and a
