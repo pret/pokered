3d097073bc7f9b29992cf30b3da78c474d2933c5
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-10-24 05:51:40 -0400

    New version 2.5.1
    bug when warping to route 7 from the underground path due to changing the number of warps on route 7

--- data/maps/objects/UndergroundPathRoute7.asm
+++ data/maps/objects/UndergroundPathRoute7.asm
@@ -5,8 +5,8 @@ UndergroundPathRoute7_Object:
 	db $a ; border block
 
 	def_warp_events
-	warp_event  3,  7, LAST_MAP, 5
-	warp_event  4,  7, LAST_MAP, 5
+	warp_event  3,  7, LAST_MAP, 3
+	warp_event  4,  7, LAST_MAP, 3
 	warp_event  4,  4, UNDERGROUND_PATH_WEST_EAST, 1
 
 	def_bg_events
