f6017ddbfd7e14ea39b81ce3393de9117e7310d9
colawsol <117095764+colawsol@users.noreply.github.com>
2023-12-07 01:34:37 +0000

    Correct StarterDex function comment (#440)

--- engine/events/starter_dex.asm
+++ engine/events/starter_dex.asm
@@ -1,4 +1,4 @@
-; this function temporarily makes the starters (and Ivysaur) seen
+; this function temporarily makes the starters (and Ivysaur) owned
 ; so that the full Pokedex information gets displayed in Oak's lab
 StarterDex:
 	ld a, 1 << (DEX_BULBASAUR - 1) | 1 << (DEX_IVYSAUR - 1) | 1 << (DEX_CHARMANDER - 1) | 1 << (DEX_SQUIRTLE - 1)
