274ede97338a3fb818df49d27166e4b031bfbbec
Vortyne <104168801+Vortyne@users.noreply.github.com>
2023-12-24 09:44:43 -0500

    Temp new dragonair form
    Possibly added in next release

---
+++ data/pokemon/base_stats/powered_dragonair.asm
@@ -0,0 +1,48 @@
+	db DEX_DRAGONAIR ; pokedex id
+
+	db  91,  84,  95,  80,  130
+	;   hp  atk  def  spd  spc
+
+	db DRAGON, ICE ; type
+	db 45 ; catch rate
+	db 218 ; base exp
+
+	INCBIN "gfx/pokemon/front/dragonair.pic", 0, 1 ; sprite dimensions
+	dw DragonairPicFront, DragonairPicBackSW
+
+	db WRAP, LEER, ROAR, NO_MOVE ; level 1 learnset
+	db GROWTH_SLOW ; growth rate
+
+	; tm/hm learnset
+	tmhm \
+	TOXIC,\
+	HORN_DRILL,\
+	BODY_SLAM,\
+	DOUBLE_EDGE,\
+	BUBBLEBEAM,\
+	AURORA_BEAM,\
+	ICE_BEAM,\
+	BLIZZARD,\
+	HYPER_BEAM,\
+	DRAGON_RAGE,\
+	THUNDERBOLT,\
+	THUNDER,\
+	KINESIS,\ ; FIREWALL
+	REFLECT,\
+	BIDE,\
+	AGILITY,\
+	BARRAGE,\
+	FIRE_BLAST,\
+	FLAMETHROWER,\
+	SLAM,\ ; FILTHY SLAM
+	LIGHT_SCREEN,\
+	THUNDER_WAVE,\
+	SUBSTITUTE,\
+	CUT,\
+	SURF,\
+	FLASH
+	; end
+
+	dw BANK(DragonairPicFront), BANK(DragonairPicBack)
+
+	dw 1, DragonairPicBack
