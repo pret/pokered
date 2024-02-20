d119e4d636fff345e26bddcbbf790f6d58fdb34b
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-02-20 21:30:00 +0100

    Started working on a new palette

--- data/sgb/sgb_border.asm
+++ data/sgb/sgb_border.asm
@@ -1,62 +1,36 @@
+; Indigo palette.
+; 08,00,16
+; 09,07,16
+; 13,12,21
+; 18,17,25
+; 18,21,29
+; 21,22,28
+
 BorderPalettes:
-IF DEF(_RED)
 	INCBIN "gfx/sgb/red_border.tilemap"
-ENDC
-IF DEF(_BLUE)
-	INCBIN "gfx/sgb/blue_border.tilemap"
-ENDC
 
 	ds $100
 
-IF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB1
-	RGB 25,22,25
-	RGB 25,17,21
-	RGB 24,14,12
-ENDC
-IF DEF(_BLUE)
-	RGB 0,0,0 ; PAL_SGB1 (the first color is not defined, but if used, turns up as 30,29,29... o_O)
-	RGB 10,17,26
-	RGB 5,9,20
-	RGB 16,20,27
-ENDC
+	RGB 21,22,28
+	RGB 18,17,25
+	RGB 13,12,21
 
 	ds $18
 
-IF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB2
-	RGB 22,31,16
-	RGB 27,20,6
+	RGB 21,22,28 ; OG: Green
+	RGB 18,17,25
 	RGB 15,15,15
-ENDC
-IF DEF(_BLUE)
-	RGB 30,29,29 ; PAL_SGB2
-	RGB 27,11,6
-	RGB 5,9,20
-	RGB 28,25,15
-ENDC
 
 	ds $18
 
-IF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB3
-	RGB 31,31,17
+	RGB 21,22,28 ; OG: Yellow
 	RGB 18,21,29
 	RGB 15,15,15
-ENDC
-IF DEF(_BLUE)
-	RGB 30,29,29 ; PAL_SGB3
-	RGB 12,15,11
-	RGB 5,9,20
-	RGB 14,22,17
-ENDC
 
 	ds $18
 
 SGBBorderGraphics:
-IF DEF(_RED)
 	INCBIN "gfx/sgb/red_border.2bpp"
-ENDC
-IF DEF(_BLUE)
-	INCBIN "gfx/sgb/blue_border.2bpp"
-ENDC
--- gfx/version.asm
+++ gfx/version.asm
@@ -1,8 +1,3 @@
 Version_GFX:
-IF DEF(_RED)
-	INCBIN "gfx/title/red_version.1bpp" ; 10 tiles
-ENDC
-IF DEF(_BLUE)
-	INCBIN "gfx/title/blue_version.1bpp" ; 8 tiles
-ENDC
+	INCBIN "gfx/title/indigo_version.1bpp" ; 10 tiles
 Version_GFXEnd:
