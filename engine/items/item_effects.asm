wash231/washred

Shift status flags

engine/items/item_effects.asm
@@ -1505,7 +1505,7 @@ ItemUseEscapeRope:
 	jr z, .notUsable
 	cp b
 	jr nz, .loop
-	ld hl, wStatusFlags6
+	ld hl, wWarpFlags
 	set BIT_FLY_WARP, [hl]
 	set BIT_ESCAPE_WARP, [hl]
 	ld hl, wStatusFlags4

engine/menus/start_sub_menus.asm
@@ -142,7 +142,7 @@ StartMenu_Pokemon::
 	jp .loop
 .canFly
 	call ChooseFlyDestination
-	ld a, [wStatusFlags6]
+	ld a, [wWarpFlags]
 	bit BIT_FLY_WARP, a
 	jp nz, .goBackToMap
 	call LoadFontTilePatterns
@@ -214,7 +214,7 @@ StartMenu_Pokemon::
 .canTeleport
 	ld hl, .warpToLastPokemonCenterText
 	call PrintText
-	ld hl, wStatusFlags6
+	ld hl, wWarpFlags
 	set BIT_FLY_WARP, [hl]
 	set BIT_ESCAPE_WARP, [hl]
 	ld hl, wStatusFlags4
@@ -371,7 +371,7 @@ StartMenu_Item::
 	ld a, [wcf91]
 	cp BICYCLE
 	jr nz, .notBicycle2
-	ld a, [wStatusFlags6]
+	ld a, [wWarpFlags]
 	bit BIT_ALWAYS_ON_BIKE, a
 	jr z, .useItem_closeMenu
 	ld hl, CannotGetOffHereText

engine/overworld/hidden_objects.asm
@@ -10,7 +10,7 @@ IsPlayerOnDungeonWarp::
 	ld [wWhichDungeonWarp], a
 	ld hl, wStatusFlags3
 	set BIT_ON_DUNGEON_WARP, [hl]
-	ld hl, wStatusFlags6
+	ld hl, wWarpFlags
 	set BIT_DUNGEON_WARP, [hl]
 	ret
 

engine/overworld/player_state.asm
@@ -32,7 +32,7 @@ IsPlayerStandingOnWarp::
 	ret
 
 CheckForceBikeOrSurf::
-	ld hl, wStatusFlags6
+	ld hl, wWarpFlags
 	bit BIT_ALWAYS_ON_BIKE, [hl]
 	ret nz
 	ld hl, ForcedBikeOrSurfMaps
@@ -64,7 +64,7 @@ CheckForceBikeOrSurf::
 	ld a, SCRIPT_SEAFOAMISLANDSB4F_MOVE_OBJECT
 	ld [wSeafoamIslandsB4FCurScript], a
 	jr z, .forceSurfing
-	ld hl, wStatusFlags6
+	ld hl, wWarpFlags
 	set BIT_ALWAYS_ON_BIKE, [hl]
 	ld a, $1
 	ld [wWalkBikeSurfState], a

engine/play_time.asm
@@ -1,6 +1,6 @@
 TrackPlayTime::
 	call CountDownIgnoreInputBitReset
-	ld a, [wStatusFlags6]
+	ld a, [wWarpFlags]
 	bit BIT_GAME_TIMER_COUNTING, a
 	ret z
 	ld a, [wPlayTimeMaxed]

home/npc_movement.asm
@@ -54,7 +54,7 @@ DebugPressedOrHeldB:: ; dummy except in _DEBUG
 ; This is used to skip Trainer battles, the
 ; Safari Game step counter, and some NPC scripts.
 IF DEF(_DEBUG)
-	ld a, [wStatusFlags6]
+	ld a, [wWarpFlags]
 	bit BIT_DEBUG_MODE, a
 	ret z
 	ldh a, [hJoyHeld]

home/text_script.asm
@@ -128,7 +128,7 @@ CloseTextDisplay::
 	call InitMapSprites ; reload sprite tile pattern data (since it was partially overwritten by text tile patterns)
 	ld hl, wFontLoaded
 	res 0, [hl]
-	ld a, [wStatusFlags6]
+	ld a, [wWarpFlags]
 	bit BIT_FLY_WARP, a
 	call z, LoadPlayerSpriteGraphics
 	call LoadCurrentMapView
@@ -196,9 +196,9 @@ PokemonFaintedText::
 DisplayPlayerBlackedOutText::
 	ld hl, PlayerBlackedOutText
 	call PrintText
-	ld a, [wStatusFlags6]
+	ld a, [wWarpFlags]
 	res BIT_ALWAYS_ON_BIKE, a
-	ld [wStatusFlags6], a
+	ld [wWarpFlags], a
 	jp HoldTextDisplayOpen
 
 PlayerBlackedOutText::

scripts/PokemonMansion3F.asm
@@ -69,7 +69,7 @@ PokemonMansion3FDefaultScript:
 	ld [wWhichDungeonWarp], a
 	ld hl, wStatusFlags3
 	set BIT_ON_DUNGEON_WARP, [hl]
-	ld hl, wStatusFlags6
+	ld hl, wWarpFlags
 	set BIT_DUNGEON_WARP, [hl]
 	ret
 

pret/pokered

engine/items/item_effects.asm
@@ -1505,7 +1505,7 @@ ItemUseEscapeRope:
 	jr z, .notUsable
 	cp b
 	jr nz, .loop
-	ld hl, wStatusFlags6
+	ld hl, wStatusFlags5
 	set BIT_FLY_WARP, [hl]
 	set BIT_ESCAPE_WARP, [hl]
 	ld hl, wStatusFlags4

engine/menus/start_sub_menus.asm
@@ -142,7 +142,7 @@ StartMenu_Pokemon::
 	jp .loop
 .canFly
 	call ChooseFlyDestination
-	ld a, [wStatusFlags6]
+	ld a, [wStatusFlags5]
 	bit BIT_FLY_WARP, a
 	jp nz, .goBackToMap
 	call LoadFontTilePatterns
@@ -214,7 +214,7 @@ StartMenu_Pokemon::
 .canTeleport
 	ld hl, .warpToLastPokemonCenterText
 	call PrintText
-	ld hl, wStatusFlags6
+	ld hl, wStatusFlags5
 	set BIT_FLY_WARP, [hl]
 	set BIT_ESCAPE_WARP, [hl]
 	ld hl, wStatusFlags4
@@ -371,7 +371,7 @@ StartMenu_Item::
 	ld a, [wcf91]
 	cp BICYCLE
 	jr nz, .notBicycle2
-	ld a, [wStatusFlags6]
+	ld a, [wStatusFlags5]
 	bit BIT_ALWAYS_ON_BIKE, a
 	jr z, .useItem_closeMenu
 	ld hl, CannotGetOffHereText

engine/overworld/hidden_objects.asm
@@ -10,7 +10,7 @@ IsPlayerOnDungeonWarp::
 	ld [wWhichDungeonWarp], a
 	ld hl, wStatusFlags3
 	set BIT_ON_DUNGEON_WARP, [hl]
-	ld hl, wStatusFlags6
+	ld hl, wStatusFlags5
 	set BIT_DUNGEON_WARP, [hl]
 	ret
 

engine/overworld/player_state.asm
@@ -32,7 +32,7 @@ IsPlayerStandingOnWarp::
 	ret
 
 CheckForceBikeOrSurf::
-	ld hl, wStatusFlags6
+	ld hl, wStatusFlags5
 	bit BIT_ALWAYS_ON_BIKE, [hl]
 	ret nz
 	ld hl, ForcedBikeOrSurfMaps
@@ -64,7 +64,7 @@ CheckForceBikeOrSurf::
 	ld a, SCRIPT_SEAFOAMISLANDSB4F_MOVE_OBJECT
 	ld [wSeafoamIslandsB4FCurScript], a
 	jr z, .forceSurfing
-	ld hl, wStatusFlags6
+	ld hl, wStatusFlags5
 	set BIT_ALWAYS_ON_BIKE, [hl]
 	ld a, $1
 	ld [wWalkBikeSurfState], a

engine/play_time.asm
@@ -1,6 +1,6 @@
 TrackPlayTime::
 	call CountDownIgnoreInputBitReset
-	ld a, [wStatusFlags6]
+	ld a, [wStatusFlags5]
 	bit BIT_GAME_TIMER_COUNTING, a
 	ret z
 	ld a, [wPlayTimeMaxed]

home/npc_movement.asm
@@ -54,7 +54,7 @@ DebugPressedOrHeldB:: ; dummy except in _DEBUG
 ; This is used to skip Trainer battles, the
 ; Safari Game step counter, and some NPC scripts.
 IF DEF(_DEBUG)
-	ld a, [wStatusFlags6]
+	ld a, [wStatusFlags5]
 	bit BIT_DEBUG_MODE, a
 	ret z
 	ldh a, [hJoyHeld]

home/text_script.asm
@@ -128,7 +128,7 @@ CloseTextDisplay::
 	call InitMapSprites ; reload sprite tile pattern data (since it was partially overwritten by text tile patterns)
 	ld hl, wFontLoaded
 	res 0, [hl]
-	ld a, [wStatusFlags6]
+	ld a, [wStatusFlags5]
 	bit BIT_FLY_WARP, a
 	call z, LoadPlayerSpriteGraphics
 	call LoadCurrentMapView
@@ -196,9 +196,9 @@ PokemonFaintedText::
 DisplayPlayerBlackedOutText::
 	ld hl, PlayerBlackedOutText
 	call PrintText
-	ld a, [wStatusFlags6]
+	ld a, [wStatusFlags5]
 	res BIT_ALWAYS_ON_BIKE, a
-	ld [wStatusFlags6], a
+	ld [wStatusFlags5], a
 	jp HoldTextDisplayOpen
 
 PlayerBlackedOutText::

scripts/PokemonMansion3F.asm
@@ -69,7 +69,7 @@ PokemonMansion3FDefaultScript:
 	ld [wWhichDungeonWarp], a
 	ld hl, wStatusFlags3
 	set BIT_ON_DUNGEON_WARP, [hl]
-	ld hl, wStatusFlags6
+	ld hl, wStatusFlags5
 	set BIT_DUNGEON_WARP, [hl]
 	ret
 