wash231/washred

Identify various flag labels and bit constants

engine/menus/pc.asm
@@ -5,14 +5,14 @@ ActivatePC::
 	ld hl, TurnedOnPC1Text
 	call PrintText
 	call WaitForSoundToFinish
-	ld hl, wFlags_0xcd60
-	set 3, [hl]
+	ld hl, wStrengthFlags
+	set BIT_USING_GENERIC_PC, [hl]
 	call LoadScreenTilesFromBuffer2
 	call Delay3
 PCMainMenu:
 	farcall DisplayPCMainMenu
-	ld hl, wFlags_0xcd60
-	set 5, [hl]
+	ld hl, wStrengthFlags
+	set BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call HandleMenuInput
 	bit BIT_B_BUTTON, a
 	jp nz, LogOff
@@ -48,9 +48,9 @@ PCMainMenu:
 	jp z, PKMNLeague ;if current menu item id is 3, it's pkmnleague
 	jp LogOff        ;otherwise, it's 4, and you're logging off
 .playersPC
-	ld hl, wFlags_0xcd60
-	res 5, [hl]
-	set 3, [hl]
+	ld hl, wStrengthFlags
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
+	set BIT_USING_GENERIC_PC, [hl]
 	ld a, SFX_ENTER_PC
 	call PlaySound
 	call WaitForSoundToFinish
@@ -93,9 +93,9 @@ LogOff:
 	ld a, SFX_TURN_OFF_PC
 	call PlaySound
 	call WaitForSoundToFinish
-	ld hl, wFlags_0xcd60
-	res 3, [hl]
-	res 5, [hl]
+	ld hl, wStrengthFlags
+	res BIT_USING_GENERIC_PC, [hl]
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
 	ret
 
 TurnedOnPC1Text:

engine/menus/players_pc.asm
@@ -1,14 +1,14 @@
 PlayerPC::
-	ld hl, wd730
-	set 6, [hl]
+	ld hl, wStatusFlags5
+	set BIT_NO_TEXT_DELAY, [hl]
 	ld a, ITEM_NAME
 	ld [wNameListType], a
 	call SaveScreenTilesToBuffer1
 	xor a
 	ld [wBagSavedMenuItem], a
 	ld [wParentMenuItem], a
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing player's PC through another PC?
+	ld a, [wStrengthFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, PlayerPCMenu
 ; accessing it directly
 	ld a, SFX_TURN_ON_PC
@@ -19,8 +19,8 @@ PlayerPC::
 PlayerPCMenu:
 	ld a, [wParentMenuItem]
 	ld [wCurrentMenuItem], a
-	ld hl, wFlags_0xcd60
-	set 5, [hl]
+	ld hl, wStrengthFlags
+	set BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call LoadScreenTilesFromBuffer2
 	hlcoord 0, 0
 	ld b, $8
@@ -63,22 +63,22 @@ PlayerPCMenu:
 	jp z, PlayerPCToss
 
 ExitPlayerPC:
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing player's PC through another PC?
+	ld a, [wStrengthFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, .next
 ; accessing it directly
 	ld a, SFX_TURN_OFF_PC
 	call PlaySound
 	call WaitForSoundToFinish
 .next
-	ld hl, wFlags_0xcd60
-	res 5, [hl]
+	ld hl, wStrengthFlags
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call LoadScreenTilesFromBuffer2
 	xor a
 	ld [wListScrollOffset], a
 	ld [wBagSavedMenuItem], a
-	ld hl, wd730
-	res 6, [hl]
+	ld hl, wStatusFlags5
+	res BIT_NO_TEXT_DELAY, [hl]
 	xor a
 	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
 	ret

engine/pokemon/bills_pc.asm
@@ -93,8 +93,8 @@ PKMNLeaguePCText: db "<PKMN>LEAGUE@"
 LogOffPCText:     db "LOG OFF@"
 
 BillsPC_::
-	ld hl, wd730
-	set 6, [hl]
+	ld hl, wStatusFlags5
+	set BIT_NO_TEXT_DELAY, [hl]
 	xor a
 	ld [wParentMenuItem], a
 	inc a               ; MONSTER_NAME
@@ -102,8 +102,8 @@ BillsPC_::
 	call LoadHpBarAndStatusTilePatterns
 	ld a, [wListScrollOffset]
 	push af
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing Bill's PC through another PC?
+	ld a, [wStrengthFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, BillsPCMenu
 ; accessing it directly
 	ld a, SFX_TURN_ON_PC
@@ -186,8 +186,8 @@ BillsPCMenu:
 	jp z, BillsPCChangeBox ; change box
 
 ExitBillsPC:
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing Bill's PC through another PC?
+	ld a, [wStrengthFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, .next
 ; accessing it directly
 	call LoadTextBoxTilePatterns
@@ -195,13 +195,13 @@ ExitBillsPC:
 	call PlaySound
 	call WaitForSoundToFinish
 .next
-	ld hl, wFlags_0xcd60
-	res 5, [hl]
+	ld hl, wStrengthFlags
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call LoadScreenTilesFromBuffer2
 	pop af
 	ld [wListScrollOffset], a
-	ld hl, wd730
-	res 6, [hl]
+	ld hl, wStatusFlags5
+	res BIT_NO_TEXT_DELAY, [hl]
 	ret
 
 BillsPCDeposit:

scripts/CeladonGym.asm
@@ -119,9 +119,9 @@ CeladonGymErikaText:
 .beforeBeat
 	ld hl, .PreBattleText
 	call PrintText
-	ld hl, wd72d
-	set 6, [hl]
-	set 7, [hl]
+	ld hl, wStatusFlags3
+	set BIT_UNKNOWN_3_6, [hl]
+	set BIT_UNKNOWN_3_7, [hl]
 	ld hl, .ReceivedRainbowBadgeText
 	ld de, .ReceivedRainbowBadgeText
 	call SaveEndBattleTextPointers

scripts/SaffronGym.asm
@@ -120,9 +120,9 @@ SaffronGymSabrinaText:
 .beforeBeat
 	ld hl, .Text
 	call PrintText
-	ld hl, wd72d
-	set 6, [hl]
-	set 7, [hl]
+	ld hl, wStatusFlags3
+	set BIT_UNKNOWN_3_6, [hl]
+	set BIT_UNKNOWN_3_7, [hl]
 	ld hl, .ReceivedMarshBadgeText
 	ld de, .ReceivedMarshBadgeText
 	call SaveEndBattleTextPointers

scripts/SeafoamIslandsB3F.asm
@@ -1,8 +1,8 @@
 SeafoamIslandsB3F_Script:
 	call EnableAutoTextBoxDrawing
-	ld hl, wFlags_0xcd60
-	bit 7, [hl]
-	res 7, [hl]
+	ld hl, wStrengthFlags
+	bit BIT_PUSHED_BOULDER, [hl]
+	res BIT_PUSHED_BOULDER, [hl]
 	jr z, .noBoulderWasPushed
 	ld hl, Seafoam4HolesCoords
 	call CheckBoulderCoords
@@ -36,8 +36,8 @@ SeafoamIslandsB3F_Script:
 	ld [wDungeonWarpDestinationMap], a
 	ld hl, Seafoam4HolesCoords
 	call IsPlayerOnDungeonWarp
-	ld a, [wd732]
-	bit 4, a
+	ld a, [wStatusFlags6]
+	bit BIT_DUNGEON_WARP, a
 	ret nz
 .runCurrentMapScript
 	ld hl, SeafoamIslandsB3F_ScriptPointers
@@ -72,8 +72,8 @@ SeafoamIslandsB3FDefaultScript:
 	dec a
 	ld [wSimulatedJoypadStatesIndex], a
 	call StartSimulatingJoypadStates
-	ld hl, wFlags_D733
-	set 2, [hl]
+	ld hl, wStatusFlags7
+	set BIT_FORCED_WARP, [hl]
 	ld a, SCRIPT_SEAFOAMISLANDSB3F_OBJECT_MOVING1
 	ld [wSeafoamIslandsB3FCurScript], a
 	ret
@@ -112,10 +112,10 @@ SeafoamIslandsB3FMoveObjectScript:
 	ld [wSimulatedJoypadStatesIndex], a
 	xor a
 	ld [wSpritePlayerStateData2MovementByte1], a
-	ld hl, wd730
-	set 7, [hl]
-	ld hl, wFlags_D733
-	set 2, [hl]
+	ld hl, wStatusFlags5
+	set BIT_SCRIPTED_JOYPAD, [hl]
+	ld hl, wStatusFlags7
+	set BIT_FORCED_WARP, [hl]
 	ld a, SCRIPT_SEAFOAMISLANDSB3F_OBJECT_MOVING2
 .playerNotInStrongCurrent
 	ld [wSeafoamIslandsB3FCurScript], a

pret/pokered

engine/menus/pc.asm
@@ -5,14 +5,14 @@ ActivatePC::
 	ld hl, TurnedOnPC1Text
 	call PrintText
 	call WaitForSoundToFinish
-	ld hl, wFlags_0xcd60
-	set 3, [hl]
+	ld hl, wMiscFlags
+	set BIT_USING_GENERIC_PC, [hl]
 	call LoadScreenTilesFromBuffer2
 	call Delay3
 PCMainMenu:
 	farcall DisplayPCMainMenu
-	ld hl, wFlags_0xcd60
-	set 5, [hl]
+	ld hl, wMiscFlags
+	set BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call HandleMenuInput
 	bit BIT_B_BUTTON, a
 	jp nz, LogOff
@@ -48,9 +48,9 @@ PCMainMenu:
 	jp z, PKMNLeague ;if current menu item id is 3, it's pkmnleague
 	jp LogOff        ;otherwise, it's 4, and you're logging off
 .playersPC
-	ld hl, wFlags_0xcd60
-	res 5, [hl]
-	set 3, [hl]
+	ld hl, wMiscFlags
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
+	set BIT_USING_GENERIC_PC, [hl]
 	ld a, SFX_ENTER_PC
 	call PlaySound
 	call WaitForSoundToFinish
@@ -93,9 +93,9 @@ LogOff:
 	ld a, SFX_TURN_OFF_PC
 	call PlaySound
 	call WaitForSoundToFinish
-	ld hl, wFlags_0xcd60
-	res 3, [hl]
-	res 5, [hl]
+	ld hl, wMiscFlags
+	res BIT_USING_GENERIC_PC, [hl]
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
 	ret
 
 TurnedOnPC1Text:

engine/menus/players_pc.asm
@@ -1,14 +1,14 @@
 PlayerPC::
-	ld hl, wd730
-	set 6, [hl]
+	ld hl, wStatusFlags5
+	set BIT_NO_TEXT_DELAY, [hl]
 	ld a, ITEM_NAME
 	ld [wNameListType], a
 	call SaveScreenTilesToBuffer1
 	xor a
 	ld [wBagSavedMenuItem], a
 	ld [wParentMenuItem], a
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing player's PC through another PC?
+	ld a, [wMiscFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, PlayerPCMenu
 ; accessing it directly
 	ld a, SFX_TURN_ON_PC
@@ -19,8 +19,8 @@ PlayerPC::
 PlayerPCMenu:
 	ld a, [wParentMenuItem]
 	ld [wCurrentMenuItem], a
-	ld hl, wFlags_0xcd60
-	set 5, [hl]
+	ld hl, wMiscFlags
+	set BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call LoadScreenTilesFromBuffer2
 	hlcoord 0, 0
 	ld b, $8
@@ -63,22 +63,22 @@ PlayerPCMenu:
 	jp z, PlayerPCToss
 
 ExitPlayerPC:
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing player's PC through another PC?
+	ld a, [wMiscFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, .next
 ; accessing it directly
 	ld a, SFX_TURN_OFF_PC
 	call PlaySound
 	call WaitForSoundToFinish
 .next
-	ld hl, wFlags_0xcd60
-	res 5, [hl]
+	ld hl, wMiscFlags
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call LoadScreenTilesFromBuffer2
 	xor a
 	ld [wListScrollOffset], a
 	ld [wBagSavedMenuItem], a
-	ld hl, wd730
-	res 6, [hl]
+	ld hl, wStatusFlags5
+	res BIT_NO_TEXT_DELAY, [hl]
 	xor a
 	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
 	ret

engine/pokemon/bills_pc.asm
@@ -93,8 +93,8 @@ PKMNLeaguePCText: db "<PKMN>LEAGUE@"
 LogOffPCText:     db "LOG OFF@"
 
 BillsPC_::
-	ld hl, wd730
-	set 6, [hl]
+	ld hl, wStatusFlags5
+	set BIT_NO_TEXT_DELAY, [hl]
 	xor a
 	ld [wParentMenuItem], a
 	inc a               ; MONSTER_NAME
@@ -102,8 +102,8 @@ BillsPC_::
 	call LoadHpBarAndStatusTilePatterns
 	ld a, [wListScrollOffset]
 	push af
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing Bill's PC through another PC?
+	ld a, [wMiscFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, BillsPCMenu
 ; accessing it directly
 	ld a, SFX_TURN_ON_PC
@@ -186,8 +186,8 @@ BillsPCMenu:
 	jp z, BillsPCChangeBox ; change box
 
 ExitBillsPC:
-	ld a, [wFlags_0xcd60]
-	bit 3, a ; accessing Bill's PC through another PC?
+	ld a, [wMiscFlags]
+	bit BIT_USING_GENERIC_PC, a
 	jr nz, .next
 ; accessing it directly
 	call LoadTextBoxTilePatterns
@@ -195,13 +195,13 @@ ExitBillsPC:
 	call PlaySound
 	call WaitForSoundToFinish
 .next
-	ld hl, wFlags_0xcd60
-	res 5, [hl]
+	ld hl, wMiscFlags
+	res BIT_NO_MENU_BUTTON_SOUND, [hl]
 	call LoadScreenTilesFromBuffer2
 	pop af
 	ld [wListScrollOffset], a
-	ld hl, wd730
-	res 6, [hl]
+	ld hl, wStatusFlags5
+	res BIT_NO_TEXT_DELAY, [hl]
 	ret
 
 BillsPCDeposit:

scripts/CeladonGym.asm
@@ -119,9 +119,9 @@ CeladonGymErikaText:
 .beforeBeat
 	ld hl, .PreBattleText
 	call PrintText
-	ld hl, wd72d
-	set 6, [hl]
-	set 7, [hl]
+	ld hl, wStatusFlags3
+	set BIT_TALKED_TO_TRAINER, [hl]
+	set BIT_PRINT_END_BATTLE_TEXT, [hl]
 	ld hl, .ReceivedRainbowBadgeText
 	ld de, .ReceivedRainbowBadgeText
 	call SaveEndBattleTextPointers

scripts/SaffronGym.asm
@@ -120,9 +120,9 @@ SaffronGymSabrinaText:
 .beforeBeat
 	ld hl, .Text
 	call PrintText
-	ld hl, wd72d
-	set 6, [hl]
-	set 7, [hl]
+	ld hl, wStatusFlags3
+	set BIT_TALKED_TO_TRAINER, [hl]
+	set BIT_PRINT_END_BATTLE_TEXT, [hl]
 	ld hl, .ReceivedMarshBadgeText
 	ld de, .ReceivedMarshBadgeText
 	call SaveEndBattleTextPointers

scripts/SeafoamIslandsB3F.asm
@@ -1,8 +1,8 @@
 SeafoamIslandsB3F_Script:
 	call EnableAutoTextBoxDrawing
-	ld hl, wFlags_0xcd60
-	bit 7, [hl]
-	res 7, [hl]
+	ld hl, wMiscFlags
+	bit BIT_PUSHED_BOULDER, [hl]
+	res BIT_PUSHED_BOULDER, [hl]
 	jr z, .noBoulderWasPushed
 	ld hl, Seafoam4HolesCoords
 	call CheckBoulderCoords
@@ -36,8 +36,8 @@ SeafoamIslandsB3F_Script:
 	ld [wDungeonWarpDestinationMap], a
 	ld hl, Seafoam4HolesCoords
 	call IsPlayerOnDungeonWarp
-	ld a, [wd732]
-	bit 4, a
+	ld a, [wStatusFlags6]
+	bit BIT_DUNGEON_WARP, a
 	ret nz
 .runCurrentMapScript
 	ld hl, SeafoamIslandsB3F_ScriptPointers
@@ -72,8 +72,8 @@ SeafoamIslandsB3FDefaultScript:
 	dec a
 	ld [wSimulatedJoypadStatesIndex], a
 	call StartSimulatingJoypadStates
-	ld hl, wFlags_D733
-	set 2, [hl]
+	ld hl, wStatusFlags7
+	set BIT_FORCED_WARP, [hl]
 	ld a, SCRIPT_SEAFOAMISLANDSB3F_OBJECT_MOVING1
 	ld [wSeafoamIslandsB3FCurScript], a
 	ret
@@ -112,10 +112,10 @@ SeafoamIslandsB3FMoveObjectScript:
 	ld [wSimulatedJoypadStatesIndex], a
 	xor a
 	ld [wSpritePlayerStateData2MovementByte1], a
-	ld hl, wd730
-	set 7, [hl]
-	ld hl, wFlags_D733
-	set 2, [hl]
+	ld hl, wStatusFlags5
+	set BIT_SCRIPTED_MOVEMENT_STATE, [hl]
+	ld hl, wStatusFlags7
+	set BIT_FORCED_WARP, [hl]
 	ld a, SCRIPT_SEAFOAMISLANDSB3F_OBJECT_MOVING2
 .playerNotInStrongCurrent
 	ld [wSeafoamIslandsB3FCurScript], a