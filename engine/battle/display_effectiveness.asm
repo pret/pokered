4e908d93cedfced526138d8ad574270de79609d4
Vortyne <104168801+Vortyne@users.noreply.github.com>
2024-08-19 12:48:12 -0400

    Don't show effectiveness many times multi hit move
    Game originally displays "It's super effective!" or "It's not very effective..." for every hit of a multi hit move. Now it only displays on the first hit.

--- engine/battle/display_effectiveness.asm
+++ engine/battle/display_effectiveness.asm
@@ -1,4 +1,14 @@
 DisplayEffectiveness:
+;;;;;; PureRGBnote: FIXED: don't display effectiveness again after the first hit of a multiple hit move.
+	ldh a, [hWhoseTurn]
+	and a
+	ld hl, wPlayerBattleStatus1
+	jr z, .gotTurn
+	ld hl, wEnemyBattleStatus1
+.gotTurn
+	bit ATTACKING_MULTIPLE_TIMES, [hl]
+	ret nz
+;;;;;;
 	ld a, [wDamageMultipliers]
 	and $7F
 	cp EFFECTIVE
