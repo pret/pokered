d5ee4fd652417124afc8dca0d40d36fd37bf8e4d
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-09-24 11:41:14 -0400

    Use `const_skip`

--- constants/icon_constants.asm
+++ constants/icon_constants.asm
@@ -1,17 +1,17 @@
 ; party menu icons
 ; used in MonPartySpritePointers (see data/icon_pointers.asm)
 	const_def
-	const ICON_MON       ; $0
-	const ICON_BALL      ; $1
-	const ICON_HELIX     ; $2
-	const ICON_FAIRY     ; $3
-	const ICON_BIRD      ; $4
-	const ICON_WATER     ; $5
-	const ICON_BUG       ; $6
-	const ICON_GRASS     ; $7
-	const ICON_SNAKE     ; $8
-	const ICON_QUADRUPED ; $9
-
-DEF ICON_TRADEBUBBLE EQU $e
+	const ICON_MON         ; $0
+	const ICON_BALL        ; $1
+	const ICON_HELIX       ; $2
+	const ICON_FAIRY       ; $3
+	const ICON_BIRD        ; $4
+	const ICON_WATER       ; $5
+	const ICON_BUG         ; $6
+	const ICON_GRASS       ; $7
+	const ICON_SNAKE       ; $8
+	const ICON_QUADRUPED   ; $9
+	const_skip 4
+	const ICON_TRADEBUBBLE ; $e
 
 DEF ICONOFFSET EQU $40 ; difference between alternating icon frames' tile IDs
