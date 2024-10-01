1b5e6319cd112f73c7474dbe1aa3947fb6625317
Vortyne <104168801+Vortyne@users.noreply.github.com>
2024-09-30 20:50:18 -0400

    This function sets the trainer just fought to face any direction as well

--- engine/overworld/auto_movement.asm
+++ engine/overworld/auto_movement.asm
@@ -266,10 +266,10 @@ RLEList_PewterGymGuy:
 	db NPC_MOVEMENT_RIGHT, 3
 	db -1 ; end
 
-FreezeEnemyTrainerSprite::
+SetEnemyTrainerToStayAndFaceAnyDirection::
 	ld a, [wCurMap]
 	cp POKEMON_TOWER_7F
-	ret z ; the Rockets on Pokemon Tower 7F leave after battling, so don't freeze them
+	ret z ; the Rockets on Pokemon Tower 7F leave after battling, so don't set them
 	ld hl, RivalIDs
 	ld a, [wEngagedTrainerClass]
 	ld b, a
@@ -278,7 +278,7 @@ FreezeEnemyTrainerSprite::
 	cp -1
 	jr z, .notRival
 	cp b
-	ret z ; the rival leaves after battling, so don't freeze him
+	ret z ; the rival leaves after battling, so don't set him
 	jr .loop
 .notRival
 	ld a, [wSpriteIndex]
