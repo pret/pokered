8232655d943004a525b036a09e9433b64f25ca37
Vortyne <104168801+Vortyne@users.noreply.github.com>
2024-12-06 12:16:05 -0500

    New version 2.6.3
    potential crash fix

--- version_number.asm
+++ version_number.asm
@@ -1,9 +1,9 @@
-db "2.6.2"
+db "2.6.3"
 
 ; wGameInternalVersion values
 	const_def
 	const INTERNAL_VERSION_ORIGINAL_GAME ; also any versions prior to 2.6.0
 	const INTERNAL_VERSION_2_6_0 ; also 2.6.1
-	const INTERNAL_VERSION_2_6_2
+	const INTERNAL_VERSION_2_6_2 ; also 2.6.3
 
 DEF CURRENT_INTERNAL_VERSION EQU INTERNAL_VERSION_2_6_2
