a59c2bbaf92d381acf45fd6313dc23329a8d08ce
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2024-12-21 21:04:32 -0500

    Use `SERIAL_RNS_LENGTH` in `BattleRandom`

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6681,7 +6681,7 @@ BattleRandom:
 	add hl, bc
 	inc a
 	ld [wLinkBattleRandomNumberListIndex], a
-	cp 9
+	cp SERIAL_RNS_LENGTH - 1
 	ld a, [hl]
 	pop bc
 	pop hl
@@ -6704,7 +6704,7 @@ ENDC
 	ld [wLinkBattleRandomNumberListIndex], a
 
 	ld hl, wLinkBattleRandomNumberList
-	ld b, 9
+	ld b, SERIAL_RNS_LENGTH - 1
 .loop
 	ld a, [hl]
 	ld c, a
