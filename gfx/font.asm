459616f33de1ccca695a6d838c81752d67e007d0
Masaru2 <dmasaru3@gmail.com>
2025-02-07 14:48:24 -0300

    R/G Textbox

--- gfx/font.asm
+++ gfx/font.asm
@@ -4,7 +4,12 @@ FontGraphicsEnd::
 
 ABTiles: INCBIN "gfx/font/AB.2bpp"
 
-HpBarAndStatusGraphics:: INCBIN "gfx/font/font_battle_extra.2bpp"
+HpBarAndStatusGraphics::
+IF DEF(_BLUE)
+INCBIN "gfx/font/font_battle_extra_b.2bpp"
+ELSE
+INCBIN "gfx/font/font_battle_extra.2bpp"
+ENDC
 HpBarAndStatusGraphicsEnd::
 
 BattleHudTiles1: INCBIN "gfx/battle/battle_hud_1.1bpp"
@@ -23,7 +28,12 @@ ENDC
 GameFreakLogoGraphics: INCBIN "gfx/title/gamefreak_inc.2bpp"
 GameFreakLogoGraphicsEnd:
 
-TextBoxGraphics:: INCBIN "gfx/font/font_extra.2bpp"
+TextBoxGraphics::
+IF DEF(_BLUE)
+INCBIN "gfx/font/font_extra_b.2bpp"
+ELSE
+INCBIN "gfx/font/font_extra.2bpp"
+ENDC
 TextBoxGraphicsEnd::
 
 PokedexTileGraphics: INCBIN "gfx/pokedex/pokedex.2bpp"
