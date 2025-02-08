f06f44626dc33feeade40024ef268f2c8a96ab52
Masaru2 <dmasaru3@gmail.com>
2025-02-08 12:52:39 -0300

    Forgot the Slot pictures for Green

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -395,6 +395,9 @@ SlotMachineTiles2:
 IF DEF(_RED)
 	INCBIN "gfx/slots/red_slots_2.2bpp"
 ENDC
+IF DEF(_GREEN)
+	INCBIN "gfx/slots/green_slots_2.2bpp"
+ENDC
 IF DEF(_BLUE)
 	INCBIN "gfx/slots/blue_slots_2.2bpp"
 ENDC
