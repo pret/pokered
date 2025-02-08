aa07ae4cadcec22fe235c9865187b921cd63b40a
Masaru2 <dmasaru3@gmail.com>
2025-02-08 16:35:32 -0300

    Fix the 2x2 block encounter glitch introduced in the English versions

--- engine/battle/wild_encounters.asm
+++ engine/battle/wild_encounters.asm
@@ -25,7 +25,7 @@ TryDoWildEncounter:
 .next
 ; determine if wild pokemon can appear in the half-block we're standing in
 ; is the bottom right tile (9,9) of the half-block we're standing in a grass/water tile?
-	hlcoord 9, 9
+	hlcoord 8, 9
 	ld c, [hl]
 	ld a, [wGrassTile]
 	cp c
