b460637b812f9829b69203f32e9a4f42a43070b6
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-02-27 13:07:17 -0500

    Avoid using `EQUS` when `EQU` or `MACRO` will do (#496)

--- macros/coords.asm
+++ macros/coords.asm
@@ -11,9 +11,17 @@ MACRO validate_coords
 	ENDC
 ENDM
 
-DEF hlcoord EQUS "coord hl,"
-DEF bccoord EQUS "coord bc,"
-DEF decoord EQUS "coord de,"
+MACRO hlcoord
+	coord hl, \#
+ENDM
+
+MACRO bccoord
+	coord bc, \#
+ENDM
+
+MACRO decoord
+	coord de, \#
+ENDM
 
 MACRO coord
 ; register, x, y[, origin]
@@ -25,9 +33,17 @@ MACRO coord
 	ENDC
 ENDM
 
-DEF hlbgcoord EQUS "bgcoord hl,"
-DEF bcbgcoord EQUS "bgcoord bc,"
-DEF debgcoord EQUS "bgcoord de,"
+MACRO hlbgcoord
+	bgcoord hl, \#
+ENDM
+
+MACRO bcbgcoord
+	bgcoord bc, \#
+ENDM
+
+MACRO debgcoord
+	bgcoord de, \#
+ENDM
 
 MACRO bgcoord
 ; register, x, y[, origin]
@@ -39,9 +55,17 @@ MACRO bgcoord
 	ENDC
 ENDM
 
-DEF hlowcoord EQUS "owcoord hl,"
-DEF bcowcoord EQUS "owcoord bc,"
-DEF deowcoord EQUS "owcoord de,"
+MACRO hlowcoord
+	owcoord hl, \#
+ENDM
+
+MACRO bcowcoord
+	owcoord bc, \#
+ENDM
+
+MACRO deowcoord
+	owcoord de, \#
+ENDM
 
 MACRO owcoord
 ; register, x, y, map width
