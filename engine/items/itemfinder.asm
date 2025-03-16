8f94dc2c9b761b45520485c57f91402439e76125
Masaru2 <dmasaru3@gmail.com>
2025-03-16 13:04:54 -0300

    Fix itemfinder not detecting hidden items if they are in the coordinate 0

--- engine/items/itemfinder.asm
+++ engine/items/itemfinder.asm
@@ -24,10 +24,15 @@ HiddenItemNear:
 	inc hl
 	jr nz, .loop ; if the item has already been obtained
 ; check if the item is within 4-5 tiles (depending on the direction of item)
+; Note by jojobear13: There is a bug here. if an item has an x or y coord at 0, the carry bit will not be set and it will not be detected
+; This bug wasn't fixed in either Blue or Yellow (Pikachu), they made workarounds by moving items out of the 0 coord
+; Adding z-flag checks check to account for this
 	ld a, [wYCoord]
 	call Sub5ClampTo0
 	cp d
+	jr z, .y_zflag
 	jr nc, .loop
+.y_zflag
 	ld a, [wYCoord]
 	add 4
 	cp d
@@ -35,7 +40,9 @@ HiddenItemNear:
 	ld a, [wXCoord]
 	call Sub5ClampTo0
 	cp e
+	jr z, .x_zflag
 	jr nc, .loop
+.x_zflag
 	ld a, [wXCoord]
 	add 5
 	cp e
@@ -45,7 +52,8 @@ HiddenItemNear:
 
 Sub5ClampTo0:
 ; subtract 5 but clamp to 0
-	sub 5
+; Note by jojobear13: Zeros are now included so substract 4 instead
+	sub 4
 	cp $f0
 	ret c
 	xor a
