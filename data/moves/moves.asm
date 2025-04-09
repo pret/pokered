226faff0ba633b2daef1dbb4b4a859e57738686b
Masaru2 <dmasaru3@gmail.com>
2025-04-09 15:46:35 -0400

    Restored Blizzard having 30% chance of freezing like R/B and JP Blue

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -69,7 +69,7 @@ Moves:
 	move HYDRO_PUMP,   NO_ADDITIONAL_EFFECT,       120, WATER,         80,  5
 	move SURF,         NO_ADDITIONAL_EFFECT,        95, WATER,        100, 15
 	move ICE_BEAM,     FREEZE_SIDE_EFFECT1,         95, ICE,          100, 10
-	move BLIZZARD,     FREEZE_SIDE_EFFECT1,        120, ICE,           90,  5
+	move BLIZZARD,     FREEZE_SIDE_EFFECT2,        120, ICE,           90,  5
 	move PSYBEAM,      CONFUSION_SIDE_EFFECT,       65, PSYCHIC_TYPE, 100, 20
 	move BUBBLEBEAM,   SPEED_DOWN_SIDE_EFFECT,      65, WATER,        100, 20
 	move AURORA_BEAM,  ATTACK_DOWN_SIDE_EFFECT,     65, ICE,          100, 20
