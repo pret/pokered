7463a7ece0651c4aa5bc905ce283fc5f7d759852
Vortyne <104168801+Vortyne@users.noreply.github.com>
2025-06-09 17:21:13 -0400

    Minor optimization

--- scripts/MtMoonB2F.asm
+++ scripts/MtMoonB2F.asm
@@ -11,17 +11,16 @@ MtMoonB2F_Script:
 	ret z
 	ld hl, MtMoonB2FFossilAreaCoords
 	call ArePlayerCoordsInArray
+	ld hl, wStatusFlags4
 	jr nc, .enable_battles
 ;;;;; PureRGBnote: FIXED: using pocket abra/teleport/dig near the super nerd can get battles stuck disabled, so enable battles in that specific case
-	ld hl, wStatusFlags6
-	bit BIT_ESCAPE_WARP, [hl]
+	ld a, [wStatusFlags6]
+	bit BIT_ESCAPE_WARP, a
 	jr nz, .enable_battles
 ;;;;;
-	ld hl, wStatusFlags4
 	set BIT_NO_BATTLES, [hl]
 	ret
 .enable_battles
-	ld hl, wStatusFlags4
 	res BIT_NO_BATTLES, [hl]
 	ret
 
