c51d1b7a1d03934f73ab1d8d6463581f671bf0a1
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-08-13 15:09:12 -0400

    List end markers (-1 aka $FF) are not part of table data

--- data/maps/hide_show_data.asm
+++ data/maps/hide_show_data.asm
@@ -255,7 +255,7 @@ MapHSPointers:
 	dw -1 ; end
 
 NoHS:
-	db $FF, $FF, $FF
+	db -1, -1, -1 ; end
 
 MissableObjects:
 ; entries correspond to HS_* constants (see constants/hide_show_constants)
@@ -564,5 +564,5 @@ SeafoamIslandsB4FHS:
 	db SEAFOAM_ISLANDS_B4F, SEAFOAMISLANDSB4F_BOULDER1, HIDE
 	db SEAFOAM_ISLANDS_B4F, SEAFOAMISLANDSB4F_BOULDER2, HIDE
 	db SEAFOAM_ISLANDS_B4F, SEAFOAMISLANDSB4F_ARTICUNO, SHOW
-	db $FF, $01, SHOW ; end
-	assert_table_length NUM_HS_OBJECTS + 1
+	assert_table_length NUM_HS_OBJECTS
+	db -1, 1, SHOW ; end
