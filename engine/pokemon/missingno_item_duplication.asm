3790c35a27e663cb76cf6c728f1c1c89dc5aefed
Vortyne <104168801+Vortyne@users.noreply.github.com>
2025-09-02 17:32:55 -0400

    Prevent missingno item dupes from making 255 of something
    original game can make 255 of an item, which causes bugs in some situations. instead only allow up to 254.

--- engine/pokemon/missingno_item_duplication.asm
+++ engine/pokemon/missingno_item_duplication.asm
@@ -7,5 +7,10 @@ MissingNoBattleStart::
 	ret z
 	ld hl, wBagItems + 11 ; sixth item in bag's quantity
 	set 7, [hl] ; adds 128 to the quantity, if you don't already have 128.
+	; PureRGBnote: FIXED: Now we will make sure the player can't have 255 of an item, because it can cause glitches with the item list.
+	ld a, [hl]
+	cp $FF
+	ret nz
+	dec [hl] ; make it 254 if it was at 255
 	ret
 	
