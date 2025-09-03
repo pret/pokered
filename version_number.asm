8c45fbc06180f25b8a00ea119c8f728eaf52a6b0
Vortyne <104168801+Vortyne@users.noreply.github.com>
2025-09-03 09:59:45 -0400

    New bugfix version 2.6.4

--- version_number.asm
+++ version_number.asm
@@ -1,9 +1,10 @@
-db "2.6.3"
+db "2.6.4"
 
 ; wGameInternalVersion values
 	const_def
 	const INTERNAL_VERSION_ORIGINAL_GAME ; also any versions prior to 2.6.0
 	const INTERNAL_VERSION_2_6_0 ; also 2.6.1
 	const INTERNAL_VERSION_2_6_2 ; also 2.6.3
+	const INTERNAL_VERSION_2_6_4
 
-DEF CURRENT_INTERNAL_VERSION EQU INTERNAL_VERSION_2_6_2
+DEF CURRENT_INTERNAL_VERSION EQU INTERNAL_VERSION_2_6_4
