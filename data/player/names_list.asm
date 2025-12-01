9a6bb3ed0c44155a711d66df4d8996d9396ebd6a
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-12-01 11:00:56 -0500

    Define player and rival names once, to be used in two places (#545)

---
+++ data/player/names_list.asm
@@ -0,0 +1,17 @@
+; see constants/player_constants.asm
+
+DefaultNamesPlayerList:
+	db "NEW NAME@"
+	list_start
+FOR n, 1, NUM_PLAYER_NAMES + 1
+	li #PLAYERNAME{d:n}
+ENDR
+	assert_list_length NUM_PLAYER_NAMES
+
+DefaultNamesRivalList:
+	db "NEW NAME@"
+	list_start
+FOR n, 1, NUM_PLAYER_NAMES + 1
+	li #RIVALNAME{d:n}
+ENDR
+	assert_list_length NUM_PLAYER_NAMES
