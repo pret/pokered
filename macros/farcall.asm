7272d9bb8a84106c877ced52d0d85fb8287936f1
Rangi <sylvie.oukaour+rangi42@gmail.com>
2025-12-01 13:57:15 -0500

    Comment that `farcall` and `callfar` are interchangable

--- macros/farcall.asm
+++ macros/farcall.asm
@@ -1,3 +1,10 @@
+; Far calls to another bank
+
+; There is no difference between `farcall` and `callfar`, except the arbitrary
+; order in which they set `a` and `hl` before calling `FarCall`.
+; We use the more natural name "farcall" for the more common order.
+; The same goes for `farjp` and `jpfar`.
+
 MACRO farcall
 	ld b, BANK(\1)
 	ld hl, \1
