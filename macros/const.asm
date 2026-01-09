f0bfe736f69bb1887cd7989b75634dd26a994fec
Engezerstorung <154867622+Engezerstorung@users.noreply.github.com>
2026-01-09 03:13:18 +0100

    Update macro const_def, fix const_next
    Make const_def more robust for edge cases.
    NYBBLE and BIT defined constants are forced to be between 0 and $F or $7 respectively.
    Defined constants values are masked as to not be actually bigger than their intended "format" when negative. (if CONSTANT -1 is intended to be $FF, then avoid the issue where `ld bc, CONSTANT` woud result in bc = $FFFF).
    
    Fixed const_next, it previously wouldnt have been behaving properly in some situations.
    Exemple : const_value > 0 but incrementation negative   and \1 < const_value would have resulted in a fail situation when it shouldnt have.

--- macros/const.asm
+++ macros/const.asm
@@ -13,31 +13,51 @@ MACRO? const_def
 	ENDC
 	IF const_inc > 0
 		REDEF const_size_compare EQUS "bigger"
-		DEF const_format_sign = 1
+		DEF const_inc_sign = 1
 	ELSE
 		REDEF const_size_compare EQUS "smaller"
-		DEF const_format_sign = -1
+		DEF const_inc_sign = -1
 	ENDC
-	DEF const_format = $FF
-	DEF undefined_const_limit = 1
+	IF const_value >= 0 && const_inc > 0
+		DEF const_limit_offset = 0
+	ELIF const_value < 0 && const_inc < 0
+		DEF const_limit_offset = -1
+	ELSE
+		DEF const_limit_offset = const_value
+	ENDC
 	IF _NARG >= 3
-		IF !STRCMP("\3", "WORD")
+		IF (!STRCMP("\3", "NYBBLE") || !STRCMP("\3", "BIT"))
+			IF !STRCMP("\3", "NYBBLE")
+				DEF const_format = $F
+			ELSE
+				DEF const_format = 7
+			ENDC
+			IF const_inc < 0
+				DEF const_limit = 0
+			ELSE
+				DEF const_limit = const_format
+			ENDC
+			IF const_format < const_value || const_value < 0
+				fail "When defining \3 constants, starting value must be comprised between 0 and {const_format}"
+			ENDC
+		ELIF !STRCMP("\3", "WORD")
 			DEF const_format = $FFFF
-		ELIF !STRCMP("\3", "NYBBLE")
-			DEF const_format = $F
-		ELIF !STRCMP("\3", "BIT")
-			DEF const_format = 7
+			DEF const_limit = const_limit_offset + const_inc_sign * const_format
 		ELSE
-			DEF undefined_const_limit = 0
 			DEF const_limit = \3
+			IF -$100 <= \3 <= $FF
+				DEF const_format = $FF
+			ELSE
+				DEF const_format = $FFFF
+			ENDC
 		ENDC
-	ENDC
-	IF undefined_const_limit
-		IF (const_value >= 0 && const_inc > 0) || (const_value <= 0 && const_inc < 0)
-			DEF const_limit = const_format_sign * const_format
+	ELSE
+		IF -$100 <= const_value <= $FF
+			DEF const_format = $FF
 		ELSE
-			DEF const_limit = const_value + const_format_sign * const_format
+			DEF const_format = $FFFF
 		ENDC
+		DEF const_limit = const_limit_offset + const_inc_sign * const_format
 	ENDC
 	REDEF last_const_string EQUS " as first constant."
 ENDM
@@ -47,7 +67,7 @@ MACRO? const
 		fail "Constant value cannot be {const_size_compare} than {const_limit}. Tried to define constant \1 for value {const_value}{last_const_string}"
 	ELSE
 		REDEF last_const_string EQUS ". Last valid constant was \1 for value {const_value}."
-		DEF \1 EQU const_value
+		DEF \1 EQU const_value & const_format
 		DEF const_value += const_inc
 	ENDC
 ENDM
@@ -71,8 +91,9 @@ MACRO? const_skip
 ENDM
 
 MACRO? const_next
-	if (const_value > 0 && \1 < const_value) || (const_value < 0 && \1 > const_value)
-		fail "const_next cannot go backwards from {const_value} to \1"
+	if (const_inc > 0 && \1 < const_value) || (const_inc < 0 && \1 > const_value)
+		def failed_const_value = \1
+		fail "const_next cannot go backwards from {const_value} to {failed_const_value}"
 	else
 		DEF const_value = \1
 	endc
