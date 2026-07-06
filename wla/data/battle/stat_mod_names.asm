a58c06997dfe1e14d5125127742864f4ec6889fa
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-06 18:53:52 +0000

    added stat_mod_names.asm

---
+++ wla/data/battle/stat_mod_names.asm
@@ -0,0 +1,9 @@
+; Stats that move effects can raise or lower
+; The relevant move effect IDs correspond to the stats
+
+.DB "ATTACK"
+.DB "DEFENSE"
+.DB "SPEED"
+.DB "SPECIAL"
+.DB "ACCURACY"
+.DB "EVASION"
