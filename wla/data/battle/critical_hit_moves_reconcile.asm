ba59d547522237b80472c212f82cbd3af19dcc49
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-07 21:27:47 +0000

    Reconcile data/battle/critical_hit_moves.asm for WLA-DX

--- wla/data/battle/critical_hit_moves_reconcile.asm
+++ wla/data/battle/critical_hit_moves_reconcile.asm
@@ -1,5 +1,8 @@
-; Moves that deal critical hits
+; Reconciled from master RGBDS source: data/battle/critical_hit_moves.asm
+; RGBDS source is untouched.
+; WLA-DX representation for reconciliation only.
 
+; Moves that deal critical hits
 .DB KARATE_CHOP
 .DB RAZOR_LEAF
 .DB CRABHAMMER
