ee4f3471971c3224306869e6f79d467a1b644414
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-07 16:01:07 +0000

    Reconcile data/battle/stat_modifiers.asm for WLA-DX

---
+++ wla/data/battle/stat_modifiers_reconcile.asm
@@ -0,0 +1,18 @@
+; Reconciled from master RGBDS source: data/battle/stat_modifiers.asm
+; RGBDS source is untouched.
+; WLA-DX representation for reconciliation only.
+
+; first byte is numerator, second byte is denominator
+.DB 25, 100
+.DB 28, 100
+.DB 33, 100
+.DB 40, 100
+.DB 50, 100
+.DB 66, 100
+.DB 1, 1
+.DB 15, 10
+.DB 2, 1
+.DB 25, 10
+.DB 3, 1
+.DB 35, 10
+.DB 4, 1
