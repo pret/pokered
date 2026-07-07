9f6f4b7882bd39e7af25f2042eceb24087e8ddae
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-07 15:51:46 +0000

    Adding Damage Effects

---
+++ wla/data/battle/set_damage_effects_reconcile.asm
@@ -0,0 +1,9 @@
+; Reconciled from master RGBDS source: data/battle/set_damage_effects.asm
+; RGBDS source is untouched.
+; WLA-DX representation for reconciliation only.
+
+; moves that do damage but not through normal calculations
+; e.g., Super Fang, Psywave
+.DB SUPER_FANG_EFFECT
+.DB SPECIAL_DAMAGE_EFFECT
+.DB -1
