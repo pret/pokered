ff7ec9ebb36fcf7a8fb3aed7ecb286d14f80a963
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-07 15:57:00 +0000

    Reconcile data/battle/unused_critical_hit_moves.asm for WLA-DX

---
+++ wla/data/battle/unused_critical_hit_moves_reconcile.asm
@@ -0,0 +1,10 @@
+; Reconciled from master RGBDS source: data/battle/unused_critical_hit_moves.asm
+; RGBDS source is untouched.
+; WLA-DX representation for reconciliation only.
+
+UnusedHighCriticalMoves:
+.DB KARATE_CHOP
+.DB RAZOR_LEAF
+.DB CRABHAMMER
+.DB SLASH
+.DB -1
