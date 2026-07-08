c0641184625e2ef839224791e4750b1f26a45bdf
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-08 21:17:00 +0000

    Phase 18: data/events/card_key_maps.asm reconciliation to WLA-DX
    - Converted SilphCoMapList from RGBDS .DB syntax to WLA-DX .DB
    - Removed trailing comment on last data line
    - Maintained original data content exactly

---
+++ wla/data/events/card_key_maps_reconcile.asm
@@ -0,0 +1,16 @@
+; Reconciled from master RGBDS source: data/events/card_key_maps.asm
+; RGBDS source is untouched.
+; WLA-DX representation for reconciliation only.
+
+SilphCoMapList:
+.DB SILPH_CO_2F
+.DB SILPH_CO_3F
+.DB SILPH_CO_4F
+.DB SILPH_CO_5F
+.DB SILPH_CO_6F
+.DB SILPH_CO_7F
+.DB SILPH_CO_8F
+.DB SILPH_CO_9F
+.DB SILPH_CO_10F
+.DB SILPH_CO_11F
+.DB -1
