b0119f4ae64ce8d94faacec1f9198d666623cbaf
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-08 03:04:57 +0000

    Reconcile data/types/names.asm for WLA-DX

--- wla/data/types/names_reconcile.asm
+++ wla/data/types/names_reconcile.asm
@@ -1,6 +1,13 @@
-; Type names for battle print_type
+; Reconciled from master RGBDS source: data/types/names.asm
+; RGBDS source is untouched.
+; WLA-DX representation for reconciliation only.
 
+; Type name pointers and string labels
 TypeNames:
+; table_width is RGBDS-only; dropped.
+; REPT UNUSED_TYPES_END - UNUSED_TYPES expanded to explicit .DW entries.
+; assert_table_length is RGBDS-only; dropped. Validation by direct count.
+
 .DW .Normal
 .DW .Fighting
 .DW .Flying
@@ -11,17 +18,6 @@ TypeNames:
 .DW .Bug
 .DW .Ghost
 
-; 11 unused types (REPT loop expanded)
-.DW .Normal
-.DW .Normal
-.DW .Normal
-.DW .Normal
-.DW .Normal
-.DW .Normal
-.DW .Normal
-.DW .Normal
-.DW .Normal
-.DW .Normal
 .DW .Normal
 
 .DW .Fire
@@ -36,15 +32,15 @@ TypeNames:
 .Fighting: .DB "FIGHTING@"
 .Flying:   .DB "FLYING@"
 .Poison:   .DB "POISON@"
-.Ground:   .DB "GROUND@"
-.Rock:     .DB "ROCK@"
-.Bird:     .DB "BIRD@"
-.Bug:      .DB "BUG@"
-.Ghost:    .DB "GHOST@"
 .Fire:     .DB "FIRE@"
 .Water:    .DB "WATER@"
 .Grass:    .DB "GRASS@"
 .Electric: .DB "ELECTRIC@"
 .Psychic:  .DB "PSYCHIC@"
 .Ice:      .DB "ICE@"
+.Ground:   .DB "GROUND@"
+.Rock:     .DB "ROCK@"
+.Bird:     .DB "BIRD@"
+.Bug:      .DB "BUG@"
+.Ghost:    .DB "GHOST@"
 .Dragon:   .DB "DRAGON@"
