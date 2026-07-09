bc618c516a133dbcba44884813fa5fea3737c8ea
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-09 14:47:08 +0000

    Phase 17: Reconcile tileset files (warp_tile_ids)

---
+++ wla/data/tilesets/warp_tile_ids_reconcile.asm
@@ -0,0 +1,74 @@
+; Format: bank label, pointer target — DW pairs of (bank, offset)
+; .DB 0 ; end
+
+.OverworldWarpTileIDs::
+.DB $1B, $58
+
+.ForestGateWarpTileIDs::
+.DB $3B
+
+.MuseumWarpTileIDs::
+.DB $3B
+
+.GateWarpTileIDs::
+.DB $3B
+
+.RedsHouse1WarpTileIDs::
+.DB $1A, $1C
+
+.RedsHouse2WarpTileIDs::
+.DB $1A, $1C
+
+.MartWarpTileIDs::
+.DB $5E
+
+.PokecenterWarpTileIDs::
+.DB $5E
+
+.ForestWarpTileIDs::
+.DB $5A, $5C, $3A
+
+.DojoWarpTileIDs::
+.DB $4A
+
+.GymWarpTileIDs::
+.DB $4A
+
+.HouseWarpTileIDs::
+.DB $54, $5C, $32
+
+.ShipWarpTileIDs::
+.DB $37, $39, $1E, $4A
+
+.InteriorWarpTileIDs::
+.DB $15, $55, $04
+
+.CavernWarpTileIDs::
+.DB $18, $1A, $22
+
+.LobbyWarpTileIDs::
+.DB $1A, $1C, $38
+
+.MansionWarpTileIDs::
+.DB $1A, $1C, $53
+
+.LabWarpTileIDs::
+.DB $34
+
+.FacilityWarpTileIDs::
+.DB $43, $58, $20
+
+.CemeteryWarpTileIDs::
+.DB $1B
+
+.UndergroundWarpTileIDs::
+.DB $13
+
+.PlateauWarpTileIDs::
+.DB $1B, $3B
+
+.ShipPortWarpTileIDs::
+.DB -1 ; end
+
+.ClubWarpTileIDs::
+.DB -1 ; end
