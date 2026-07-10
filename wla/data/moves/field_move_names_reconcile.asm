1bacdf1db0c8dc146ffa9e4904f7a1ab35c15edf
Brandon M. M. Green <bmmgreen@gmail.com>
2026-07-10 02:18:42 +0000

    Fix move reconcile string tables

--- wla/data/moves/field_move_names_reconcile.asm
+++ wla/data/moves/field_move_names_reconcile.asm
@@ -2,14 +2,13 @@
 ; RGBDS source is untouched.
 ; WLA-DX representation for reconciliation only.
 
-; FieldMoveNames:
-; Names of field moves (corresponds to FieldMoveDisplayData)
-.dw "CUT@"
-.dw "FLY@"
-.dw "@"
-.dw "SURF@"
-.dw "STRENGTH@"
-.dw "FLASH@"
-.dw "DIG@"
-.dw "TELEPORT@"
-.dw "SOFTBOILED@"
+FieldMoveNames:
+.DB "CUT@"
+.DB "FLY@"
+.DB "@"
+.DB "SURF@"
+.DB "STRENGTH@"
+.DB "FLASH@"
+.DB "DIG@"
+.DB "TELEPORT@"
+.DB "SOFTBOILED@"
